111080752f5ed8a38f87be1e5ad03997b5892042
Justine George <justinegeo96@gmail.com>
2022-09-18 16:25:29 -0500

    Time: 0 ms (100.00%), Space: 42.4 MB (86.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,12 +1,23 @@
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         int[] runSum = new int[nums.length];
+        
+//         runSum[0] = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             runSum[i] = nums[i] + runSum[i-1];
+//         }
+        
+//         return runSum;
+//     }
+// }
+
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] runSum = new int[nums.length];
         
-        runSum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            runSum[i] = nums[i] + runSum[i-1];
+            nums[i] = nums[i] + nums[i-1];
         }
         
-        return runSum;
+        return nums;
     }
 }
