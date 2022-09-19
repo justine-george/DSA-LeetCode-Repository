a6709923f2903e80a1ed6a9d9ae3d9ecdeaf25bc
Justine George <justinegeo96@gmail.com>
2022-09-19 00:01:11 -0500

    Time: 79 ms (7.00%), Space: 117.5 MB (5.00%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,25 +1,40 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         Map<String, Integer> letterCount = new HashMap<>();
+        
+//         // form letter count map from magazine
+//         for (char c: magazine.toCharArray()) {
+//             if (letterCount.containsKey(c + ""))
+//                 letterCount.put(c + "", letterCount.get(c + "") + 1);
+//             else
+//                 letterCount.put(c + "", 1);
+//         }
+        
+//         // check whether ransomNote can be made using magazine letters
+//         for (char c: ransomNote.toCharArray()) {
+//             if (!letterCount.containsKey(c + ""))
+//                 return false;
+//             else {
+//                 if (letterCount.get(c + "") > 0)
+//                     letterCount.put(c + "", letterCount.get(c + "") - 1);
+//                 else
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        Map<String, Integer> letterCount = new HashMap<>();
-        
-        // form letter count map from magazine
-        for (char c: magazine.toCharArray()) {
-            if (letterCount.containsKey(c + ""))
-                letterCount.put(c + "", letterCount.get(c + "") + 1);
-            else
-                letterCount.put(c + "", 1);
-        }
-        
-        // check whether ransomNote can be made using magazine letters
+    public boolean canConstruct(String ransomNote, String magazine) {        
         for (char c: ransomNote.toCharArray()) {
-            if (!letterCount.containsKey(c + ""))
+            int index = magazine.indexOf(c);
+            
+            if (index == -1)
                 return false;
-            else {
-                if (letterCount.get(c + "") > 0)
-                    letterCount.put(c + "", letterCount.get(c + "") - 1);
-                else
-                    return false;
-            }
+            
+            magazine = magazine.substring(0, index) + magazine.substring(index + 1);
+            
         }
         return true;
     }
