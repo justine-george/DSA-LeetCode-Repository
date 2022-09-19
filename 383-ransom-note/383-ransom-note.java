79e4fbf20bf730b3f745370d6f053cad7d112c4a
Justine George <justinegeo96@gmail.com>
2022-09-18 23:53:25 -0500

    Time: 49 ms (10.10%), Space: 42.7 MB (90.70%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<String, Integer> letterCount = new HashMap<>();
+        
+        // form letter count map from magazine
+        for (char c: magazine.toCharArray()) {
+            if (letterCount.containsKey(c + ""))
+                letterCount.put(c + "", letterCount.get(c + "") + 1);
+            else
+                letterCount.put(c + "", 1);
+        }
+        
+        // check whether ransomNote can be made using magazine letters
+        for (char c: ransomNote.toCharArray()) {
+            if (!letterCount.containsKey(c + ""))
+                return false;
+            else {
+                if (letterCount.get(c + "") > 0)
+                    letterCount.put(c + "", letterCount.get(c + "") - 1);
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+}
