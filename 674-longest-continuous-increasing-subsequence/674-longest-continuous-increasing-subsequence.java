d156e35502ece91a0610c543b6be9fe04b30a5e5
Justine George <justinegeo96@gmail.com>
2022-09-20 15:13:24 -0500

    Time: 1 ms (100.00%), Space: 42.5 MB (99.39%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.java
@@ -1,3 +1,4 @@
+// Solution 1
 // class Solution {
 //     public int findLengthOfLCIS(int[] nums) {
 //         int[] cisLength = new int[nums.length];
@@ -27,33 +28,59 @@
 //     }
 // }
 
+// Solution 2
+// class Solution {
+//     public int findLengthOfLCIS(int[] nums) {
+        
+//         List<List<Integer>> subsequenceList = new ArrayList<>();
+        
+//         // store increasing subsequences in a list of lists
+//         List<Integer> subsequence = new ArrayList<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             subsequence.add(nums[i]);
+//             if (i == nums.length - 1) {
+//                 subsequenceList.add(subsequence);
+//                 break;
+//             }
+//             if (nums[i] < nums[i+1]) {
+//                 // continue
+//             } else {
+//                 subsequenceList.add(subsequence);
+//                 subsequence = new ArrayList<>();
+//             }
+//         }
+        
+//         for (List<Integer> aList: subsequenceList) {
+//             for (Integer a: aList) {
+//                 System.out.print("" + a);
+//             }
+//             System.out.print("\n");
+//         }
+        
+//         // find max length among lists
+//         int max = -1;
+//         for (List<Integer> a: subsequenceList) {
+//             if (a.size() > max)
+//                 max = a.size();
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
         
-        List<List<Integer>> subsequenceList = new ArrayList<>();
+        int answer = 1, counter = 1;
         
-        // store increasing subsequences in a list of lists
-        List<Integer> subsequence = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            subsequence.add(nums[i]);
-            if (i == nums.length - 1) {
-                subsequenceList.add(subsequence);
-                break;
-            }
-            if (nums[i] < nums[i+1]) {
-                // continue
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] < nums[i]) {
+                counter++;
+                answer = Math.max(answer, counter);
             } else {
-                subsequenceList.add(subsequence);
-                subsequence = new ArrayList<>();
+                counter = 1;
             }
         }
         
-        // find max lengtha among lists
-        int max = -1;
-        for (List<Integer> a: subsequenceList) {
-            if (a.size() > max)
-                max = a.size();
-        }
-        return max;
+        return answer;
     }
 }
