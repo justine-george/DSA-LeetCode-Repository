c6f130ce2da34a28debfe51fad1954a712002dfd
Justine George <justinegeo96@gmail.com>
2022-09-20 14:48:13 -0500

    Time: 15 ms (5.71%), Space: 48.3 MB (6.62%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.java
@@ -1,27 +1,58 @@
+// class Solution {
+//     public int findLengthOfLCIS(int[] nums) {
+//         int[] cisLength = new int[nums.length];
+        
+//         // initialize 1 to every elements of cisLength
+//         for (int i = 0; i < cisLength.length; i++) {
+//             cisLength[i] = 1;
+//         }
+        
+//         // find length of continuous increasing subsequence from each index
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i; j < nums.length - 1; j++) {
+//                 if (nums[j] < nums[j+1])
+//                     cisLength[i]++;
+//                 else
+//                     break;
+//             }
+//         }
+        
+//         // find max length of LCIS and return
+//         int max = -1;
+//         for (int a: cisLength) {
+//             if (a > max)
+//                 max = a;
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
-        int[] cisLength = new int[nums.length];
         
-        // initialize 1 to every elements of cisLength
-        for (int i = 0; i < cisLength.length; i++) {
-            cisLength[i] = 1;
-        }
+        List<List<Integer>> subsequenceList = new ArrayList<>();
         
-        // find length of continuous increasing subsequence from each index
+        // store increasing subsequences in a list of lists
+        List<Integer> subsequence = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j - 1] < nums[j])
-                    cisLength[i]++;
-                else
-                    break;
+            subsequence.add(nums[i]);
+            if (i == nums.length - 1) {
+                subsequenceList.add(subsequence);
+                break;
+            }
+            if (nums[i] < nums[i+1]) {
+                // continue
+            } else {
+                subsequenceList.add(subsequence);
+                subsequence = new ArrayList<>();
             }
         }
         
-        // find max length of LCIS and return
+        // find max lengtha among lists
         int max = -1;
-        for (int a: cisLength) {
-            if (a > max)
-                max = a;
+        for (List<Integer> a: subsequenceList) {
+            if (a.size() > max)
+                max = a.size();
         }
         return max;
     }
