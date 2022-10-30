7702868d13c3379695588cf8465c17ce3decca7d
Justine George <justinegeo96@gmail.com>
2022-10-30 13:33:41 -0500

    Time: 2 ms (79.34%), Space: 73 MB (77.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,8 +4,7 @@ class Solution {
         int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
             curr = Math.max(nums[i], curr + nums[i]);
-            if (curr > max)
-                max = curr;
+            max = Math.max(max, curr);
         }
         return max;
     }
