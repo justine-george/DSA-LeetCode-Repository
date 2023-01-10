8006bfabdbc25b1f472ddf20c74d4d8ca0165e14
Justine George <justinegeo96@gmail.com>
2023-01-09 23:11:11 -0600

    Time: 0 ms (100.00%), Space: 42 MB (67.09%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int maxWealth = 0, currentWealth;
+        int maxWealth = 0, currentWealth = 0;
         for (int i = 0; i < accounts.length; i++) {
             currentWealth = 0;
             for (int j = 0; j < accounts[0].length; j++) {
