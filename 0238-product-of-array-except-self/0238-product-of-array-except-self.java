1bbfcca36a3c95cfe11f95015b4e358e89b74856
Justine George <justinegeo96@gmail.com>
2023-02-03 17:29:48 -0600

    Time: 1 ms (100.00%), Space: 50 MB (97.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int mul = 1, zeroIndex = -1, zeroCount = 0;
         int[] answer = new int[nums.length];
-        
         // find number of zeroes
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
@@ -10,25 +9,20 @@ class Solution {
                 zeroCount++;
             }
         }
-        
         // case 0: two or more zeros
         if (zeroCount >= 2) {
             return new int[nums.length];
         }
-        
         // calculate product of array
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                mul *= nums[i];
             }
         }
-        
         // case 1: one zero
         if (zeroIndex != -1) {
             answer[zeroIndex] = mul;
-        }
-        // case 2: no zeroes
-        else { 
+        } else { // case 2: no zeroes
            for (int i = 0; i < nums.length; i++) {
                answer[i] = mul / nums[i];
            }
