0a273a719410b27a252994e8a508b6d358ab9778
Justine George <justinegeo96@gmail.com>
2023-02-05 18:56:12 -0600

    Time: 1 ms (100.00%), Space: 50.5 MB (65.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,31 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int mul = 1, zeroIndex = -1, zeroCount = 0;
         int[] answer = new int[nums.length];
-        // find number of zeroes
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroIndex = i;
-                zeroCount++;
-            }
+        answer[0] = 1;
+        // leftside values
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = nums[i-1] * answer[i-1];
         }
-        // case 0: two or more zeros
-        if (zeroCount >= 2) {
-            return new int[nums.length];
-        }
-        // calculate product of array
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-               mul *= nums[i];
-            }
-        }
-        // case 1: one zero
-        if (zeroIndex != -1) {
-            answer[zeroIndex] = mul;
-        } else { // case 2: no zeroes
-           for (int i = 0; i < nums.length; i++) {
-               answer[i] = mul / nums[i];
-           }
+        // rightside values
+        int mul = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            mul *= nums[i+1];
+            answer[i] = mul * answer[i];
         }
         return answer;
     }
