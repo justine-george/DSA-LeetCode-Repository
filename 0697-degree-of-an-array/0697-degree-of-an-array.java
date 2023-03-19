08b5fead1629145c54cff3506c56afe17610873c
Justine George <justinegeo96@gmail.com>
2023-03-19 04:03:24 -0500

    Time: 30 ms (37.85%), Space: 47.2 MB (42.30%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,43 +1,43 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer, Integer> countMap = new HashMap();
-        HashMap<Integer, Integer> firstOccurMap = new HashMap();
-        int maxCount = 0;
-        int minLen = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            firstOccurMap.putIfAbsent(nums[i], i);
-            countMap.put((nums[i]), countMap.getOrDefault(nums[i], 0) + 1);
-            if (maxCount < countMap.get(nums[i])) {
-                maxCount = countMap.get(nums[i]);
-                minLen = i - firstOccurMap.get(nums[i]) + 1;
-            } else if (maxCount == countMap.get(nums[i])) {
-                minLen = Math.min(minLen, i - firstOccurMap.get(nums[i]) + 1);
-            }
-        }
-        
-        return minLen;
-        
-//         HashMap<Integer, Integer> firstOccurMap = new HashMap<>();
-//         HashMap<Integer, Integer> latestOccurMap = new HashMap<>();
-//         HashMap<Integer, Integer> countMap = new HashMap<>();
- 
+//         HashMap<Integer, Integer> countMap = new HashMap();
+//         HashMap<Integer, Integer> firstOccurMap = new HashMap();
 //         int maxCount = 0;
-//         for (int i = 0; i < nums.length; i++) {
-//             if (!firstOccurMap.containsKey(nums[i]))
-//                 firstOccurMap.put((nums[i]), i);
-//             countMap.put((nums[i]), countMap.getOrDefault(nums[i], 0) + 1);
-//             latestOccurMap.put((nums[i]), i);
-//             maxCount = Math.max(maxCount, countMap.get(nums[i]));
-//         }
+//         int minLen = 0;
         
-//         int minLen = Integer.MAX_VALUE;
-//         for (Map.Entry<Integer, Integer> e: countMap.entrySet()) {
-//             if (e.getValue() == maxCount) {
-//                 minLen = Math.min(minLen, latestOccurMap.get(e.getKey()) - firstOccurMap.get(e.getKey()) + 1);
+//         for (int i = 0; i < nums.length; i++) {
+//             firstOccurMap.putIfAbsent(nums[i], i);
+//             countMap.put((nums[i]), countMap.getOrDefault(nums[i], 0) + 1);
+//             if (maxCount < countMap.get(nums[i])) {
+//                 maxCount = countMap.get(nums[i]);
+//                 minLen = i - firstOccurMap.get(nums[i]) + 1;
+//             } else if (maxCount == countMap.get(nums[i])) {
+//                 minLen = Math.min(minLen, i - firstOccurMap.get(nums[i]) + 1);
 //             }
 //         }
+        
 //         return minLen;
+        
+        HashMap<Integer, Integer> firstOccurMap = new HashMap<>();
+        HashMap<Integer, Integer> latestOccurMap = new HashMap<>();
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+ 
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!firstOccurMap.containsKey(nums[i]))
+                firstOccurMap.put((nums[i]), i);
+            countMap.put((nums[i]), countMap.getOrDefault(nums[i], 0) + 1);
+            latestOccurMap.put((nums[i]), i);
+            maxCount = Math.max(maxCount, countMap.get(nums[i]));
+        }
+        
+        int minLen = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> e: countMap.entrySet()) {
+            if (e.getValue() == maxCount) {
+                minLen = Math.min(minLen, latestOccurMap.get(e.getKey()) - firstOccurMap.get(e.getKey()) + 1);
+            }
+        }
+        return minLen;
         
     }
 }
