cd573d978a11ebaf6280c3b6403c0fbcdb4b572f
Justine George <justinegeo96@gmail.com>
2023-03-19 03:46:33 -0500

    Time: 32 ms (35.41%), Space: 46.3 MB (80.32%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -4,6 +4,7 @@ class Solution {
         HashMap<Integer, Integer> latestOccurMap = new HashMap<>();
         HashMap<Integer, Integer> countMap = new HashMap<>();
  
+        int maxCount = 0;
         for (int i = 0; i < nums.length; i++) {
             if (!firstOccurMap.containsKey(nums[i])) {
                 firstOccurMap.put((nums[i]), i);
@@ -12,13 +13,7 @@ class Solution {
                 countMap.put((nums[i]), countMap.get(nums[i]) + 1);
             }
             latestOccurMap.put((nums[i]), i);
-        }
-        
-        int maxCount = 0;
-        for (Map.Entry<Integer, Integer> e: countMap.entrySet()) {
-            int key = e.getKey();
-            int val = e.getValue();
-            maxCount = Math.max(maxCount, val);
+            maxCount = Math.max(maxCount, countMap.get(nums[i]));
         }
         
         int minLen = Integer.MAX_VALUE;
