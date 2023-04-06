976a5d3a1f61e440eb970640f9e867bf2949eb1f
Justine George <justinegeo96@gmail.com>
2023-04-06 18:09:23 -0500

    Time: 46 ms (6.10%), Space: 14.1 MB (9.29%) - LeetHub

---
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -0,0 +1,41 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+        
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            
+            minLen = min(len(w1), len(w2))
+            
+            # invalid order
+            if w1[:minLen] == w2[:minLen] and len(w1) > len(w2):
+                return ""
+            
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+            
+        visited = {}
+        res = []
+        def dfs(c):
+            if c in visited:
+                return visited[c]
+            
+            visited[c] = True
+            
+            for neighbor in adj[c]:
+                if dfs(neighbor): # cycle detected
+                    return True
+            
+            # post order dfs
+            visited[c] = False
+            res.append(c)
+        
+        for c in adj:
+            if dfs(c): # cycle detected
+                return ""
+        
+        reversedWord = res[::-1] # reverse list
+        
+        return "".join(reversedWord) # [a, b, c] to abc
