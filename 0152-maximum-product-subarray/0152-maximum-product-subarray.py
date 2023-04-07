2b378685b80e25a1d854314536f0e077504d957f
Justine George <justinegeo96@gmail.com>
2023-04-07 01:14:00 -0500

    Time: 81 ms (84.62%), Space: 14.3 MB (69.32%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,10 +4,6 @@ class Solution:
         
         currMin, currMax = 1, 1
         for n in nums:
-            # if n == 0:
-            #     currMin, currMax = 1, 1
-            #     continue
-                
             temp = currMax
             currMax = max(currMax * n, currMin * n, n)
             currMin = min(temp * n, currMin * n, n)
