575646baf0d61cd76a2bb07ec2ecb57b999779fe
Justine George <justinegeo96@gmail.com>
2023-04-06 23:31:26 -0500

    Time: 219 ms (96.34%), Space: 21.2 MB (66.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,11 +5,10 @@ class Solution:
         for i in range(1, len(nums)):
             res[i] = nums[i - 1] * prefix
             prefix = res[i]
-            
+
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
             res[i] = res[i] * postfix # res[i] is the prefix
             postfix = postfix * nums[i]
-
-        
+            
         return res
