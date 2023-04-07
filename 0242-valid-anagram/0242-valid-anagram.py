8577420967e3a231f5a0324332addc06d5ea011c
Justine George <justinegeo96@gmail.com>
2023-04-07 06:20:44 -0500

    Time: 55 ms (43.79%), Space: 14.6 MB (32.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,20 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        # sDict = collections.defaultdict(lambda: 0)
-        sDict = {}
-        for c in s:
-            # sDict[c] += 1
-            sDict[c] = 1 + sDict.get(c, 0)
-
-        # tDict = collections.defaultdict(lambda: 0)
-        tDict = {}
-        for c in t:
-            # tDict[c] += 1
-            tDict[c] = 1 + tDict.get(c, 0)
+        sDict, tDict = {}, {}
+        for i in range(len(s)):
+            sDict[s[i]] = 1 + sDict.get(s[i], 0)
+            tDict[t[i]] = 1 + tDict.get(t[i], 0)
         
-        for c in s:
+        for c in sDict:
             if sDict[c] != tDict.get(c, -1):
-                return False
+                return False            
         
         return True
