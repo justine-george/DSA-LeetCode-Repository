21911052c7555295a1961e2f8a1d5aaca23f9c76
Justine George <justinegeo96@gmail.com>
2023-04-07 00:14:07 -0500

    Time: 437 ms (95.22%), Space: 16.1 MB (65.57%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,12 +1,13 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # O(ELogV) time complexity
         adjList = collections.defaultdict(list)
         
         # create adjacency list
         for u, v, weight in times:
             adjList[u].append((v, weight))
         
-        # (pathlength, destNode)
+        # (pathlength, destNode), heapq will sort by the first element of the tuple
         minHeap = [(0, k)] # starting from k
         visit = set()
         t = 0 # cost to visit the last node
