076f48a7a5f322176d9482ed2f20d1616a333168
Justine George <justinegeo96@gmail.com>
2023-04-06 23:23:01 -0500

    Time: 256 ms (25.17%), Space: 25.7 MB (7.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,7 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         prefix = 1
-        
         res = [1]
         for i in range(1, len(nums)):
             res.append(nums[i - 1] * prefix)
@@ -11,17 +10,6 @@ class Solution:
         for i, el in reversed(list(enumerate(nums))):
             res[i] = res[i] * postfix
             postfix = postfix * nums[i]
-            
-        for i in range(0, len(res)):
-            print(res[i], end=" ")
+
         
         return res
-            
-            
-#             [1, 2, 3, 4]
-#             [1, 1, 2, 6]
-            
-#             post = 12
-            
-#             [1, 1, 2, 6]
-#             [      8 , 6]
