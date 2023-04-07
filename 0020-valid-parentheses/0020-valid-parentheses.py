713ff3348fe2b2a93c38fe4dad1e2f3663581461
Justine George <justinegeo96@gmail.com>
2023-04-07 08:33:00 -0500

    Time: 32 ms (66.41%), Space: 13.9 MB (16.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         st = []
-        
         map = {
             "(": ")",
             "[": "]",
             "{": "}"
         }
+        
         for el in s:
             if st and map.get(st[-1]) == el:
                 st.pop()
