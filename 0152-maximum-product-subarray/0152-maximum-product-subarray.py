e663c5cbe9b33fe2e80d91f573593d18fd0d9884
Justine George <justinegeo96@gmail.com>
2023-04-07 01:29:27 -0500

    Time: 89 ms (50.23%), Space: 14.3 MB (98.63%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)
-        
         # T: O(n), S:O(1) - (DP) We use currMin and currMax
         # of i-1th position to find out the same for ith position
-        currMin, currMax = 1, 1
-        for n in nums:
-            temp = currMax
-            currMax = max(currMax * n, currMin * n, n)
-            currMin = min(temp * n, currMin * n, n)
+        currMin = currMax = res = nums[0]
+        for n in nums[1:]:
+            currMax, currMin = max(currMax * n, currMin * n, n), min(currMax * n, currMin * n, n)
             res = max(res, currMax)
-            
         return res
