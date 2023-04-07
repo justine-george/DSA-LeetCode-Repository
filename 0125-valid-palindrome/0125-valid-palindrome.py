9fb67cdea101dac0178272a2308e26744b5fcb06
Justine George <justinegeo96@gmail.com>
2023-04-07 07:51:33 -0500

    Time: 75 ms (11.70%), Space: 14.6 MB (95.30%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
+        # trivial solution, S: O(n)
 #         str = ""
 #         for c in s:
 #             if c.isalnum():
 #                 str += c.lower()
-        
 #         return str == str[::-1]
     
+        # Two pointers - S: O(1)
+        def isalphanum(c):
+            return (ord('0') <= ord(c) <= ord('9')) or (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z'))
+        
         l, r = 0, len(s) - 1
         
         while l < r:
-            while not s[l].isalnum() and l < r:
+            while not isalphanum(s[l]) and l < r:
                 l += 1
-            while not s[r].isalnum() and l < r:
+            while not isalphanum(s[r]) and l < r:
                 r -= 1
             
             if s[l].lower() != s[r].lower():
@@ -23,5 +27,4 @@ class Solution:
             r -= 1
         
         return True
-        
         
