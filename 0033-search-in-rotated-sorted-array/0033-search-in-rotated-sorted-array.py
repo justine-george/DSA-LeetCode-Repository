d7e5683603bd6b51fb9427dccb562c4bfec03225
Justine George <justinegeo96@gmail.com>
2023-04-07 02:40:55 -0500

    Time: 50 ms (15.05%), Space: 14.2 MB (45.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -9,21 +9,31 @@ class Solution:
             if nums[mid] == target:
                 return mid
             
+            # break down into cases
+            # if mid value belongs to left sorted portion
             if nums[mid] >= nums[l]:
-                if target > nums[mid]:
+                if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
-                    if target < nums[l]:
-                        l = mid + 1
-                    else:
-                        r = mid - 1
-            else:
-                if target < nums[mid]:
+                    r = mid - 1
+                # if target > nums[mid]:
+                #     l = mid + 1
+                # else: # two options now
+                #     if target < nums[l]:
+                #         l = mid + 1
+                #     else:
+                #         r = mid - 1
+            else: # means mid value belongs to right sorted portion
+                if target < nums[mid] or target > nums[r]:
                     r = mid - 1
                 else:
-                    if target > nums[r]:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
+                    l = mid + 1
+                # if target < nums[mid]:
+                #     r = mid - 1
+                # else: # two options now
+                #     if target > nums[r]:
+                #         r = mid - 1
+                #     else:
+                #         l = mid + 1
         
         return -1
