359674a0cb1cda5907d0b520eea9f7dcf9b60a22
Justine George <justinegeo96@gmail.com>
2023-04-08 05:44:45 -0500

    Time: 896 ms (86.12%), Space: 20.5 MB (67.12%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -8,7 +8,7 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
         def revOdd(l, r, level=1):
-            if not r:
+            if not l:
                 return
             
             if level % 2 != 0:
