04931bdc77dffd4795fa7893507ead28b6b6c782
Justine George <justinegeo96@gmail.com>
2023-04-09 04:47:30 -0500

    Time: 67 ms (49.73%), Space: 14 MB (21.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,29 +1,8 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-#         res = []
-#         # backtracking - 2 decisions
-#         def dfs(i, cur, total):
-#             if total == target:
-#                 res.append(cur.copy())
-#                 return
-#             if i >= len(candidates) or total > target:
-#                 return
-            
-#             # decision 1: include candidate[i] and continue to include ith position
-#             cur.append(candidates[i])
-#             dfs(i, cur, total + candidates[i])
-            
-#             # decision 2: don't candidate[i] and don't use ith index ever
-#             cur.pop()
-#             dfs(i + 1, cur, total)
-            
-#         dfs(0, [], 0)
-#         return res
-    
         res = []
         # backtracking - 2 decisions
-        cur = []
+        cur = [] # to track current combination
         def dfs(i, total):
             if total == target:
                 res.append(cur.copy())
