54a094bc14354a873767c47985ba162e283feb60
Justine George <justinegeo96@gmail.com>
2023-04-09 06:17:57 -0500

    Time: 406 ms (16.50%), Space: 16.4 MB (79.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,65 +2,58 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def isUnseenIsland(grid, r, c, visited):
-            if (r,c) in visited:
-                return False
-            if grid[r][c] == '0':
-                return False
-            q = deque([(r,c)])
-            visited.add((r,c))
-            while q:
-                r, c = q.popleft()
-                appendIfNewAndValidLand(grid, r - 1, c, visited, q)
-                appendIfNewAndValidLand(grid, r + 1, c, visited, q)
-                appendIfNewAndValidLand(grid, r, c + 1, visited, q)
-                appendIfNewAndValidLand(grid, r, c - 1, visited, q)
-            return True
-
-        def appendIfNewAndValidLand(grid, r, c, visited, q):
-            if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-                if grid[r][c] == '1':
-                    if (r, c) not in visited:
-                        q.append((r, c))
-                        visited.add((r, c))
-        
-        visited = set()
-        count = 0
-        for r, row in enumerate(grid):
-            for c, _ in enumerate(row):
-                if isUnseenIsland(grid, r, c, visited):
-                    count = count + 1
-        return count
-        
-#         # Below solution is only valid if input grid can be mutated
-#         # when visited, change value to -1
-#         # T: O(m*n), S: O(min(m,n)) for bfs queue
-#         def isUnseenIsland(grid, r, c):
-#             if grid[r][c] == -1: # if visited
-#                 return False
-#             if grid[r][c] == '0':
-#                 return False
+#         def isUnseenIsland(r, c):
 #             q = deque([(r,c)])
+#             visited.add((r,c))
+            
 #             while q:
-#                 r, c = q.popleft()
-#                 appendIfNewAndValidLand(grid, r - 1, c, q)
-#                 appendIfNewAndValidLand(grid, r + 1, c, q)
-#                 appendIfNewAndValidLand(grid, r, c + 1, q)
-#                 appendIfNewAndValidLand(grid, r, c - 1, q)
-#             return True
+#                 curR, curC = q.popleft()
+#                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+                
+#                 for dr, dc in directions:
+#                     newR, newC = curR + dr, curC + dc
+#                     validateAndAddLand(newR, newC, q)
 
-#         def appendIfNewAndValidLand(grid, r, c, q):
-#             if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-#                 if grid[r][c] == '1':
-#                     if grid[r][c] != -1: # if not visited
+#         def validateAndAddLand(r, c, q):
+#             if (r in range(rows) and c in range(cols) and grid[r][c] == '1' and (r, c) not in visited):
 #                         q.append((r, c))
-#                         grid[r][c] = -1 # set as visited
+#                         visited.add((r, c))
         
+#         rows, cols = len(grid), len(grid[0])
+#         visited = set()
 #         count = 0
 #         for r, row in enumerate(grid):
 #             for c, _ in enumerate(row):
-#                 if isUnseenIsland(grid, r, c):
-#                     count = count + 1
+#                 if grid[r][c] == '1' and (r,c) not in visited:
+#                     isUnseenIsland(r, c)
+#                     count += 1
 #         return count
-    
-    
+        
+        # Below solution is only valid if input grid can be mutated
+        # when visited, change value to -1
+        # T: O(m*n), S: O(min(m,n)) for bfs queue
+        def isUnseenIsland(r, c):
+            q = deque([(r,c)])
+            grid[r][c] = -1
+            
+            while q:
+                curR, curC = q.popleft()
+                directions= [[1,0],[-1,0],[0,1],[0,-1]]
+                
+                for dr, dc in directions:
+                    newR, newC = curR + dr, curC + dc
+                    validateAndAddLand(newR, newC, q)
+
+        def validateAndAddLand(r, c, q):
+            if (r in range(rows) and c in range(cols) and grid[r][c] == '1' and grid[r][c] != -1):
+                        q.append((r, c))
+                        grid[r][c] = -1
+        
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+        for r, row in enumerate(grid):
+            for c, _ in enumerate(row):
+                if grid[r][c] == '1' and grid[r][c] != -1:
+                    isUnseenIsland(r, c)
+                    count += 1
+        return count
