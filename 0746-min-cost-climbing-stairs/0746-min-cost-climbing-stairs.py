25792f7b7df0ff067f20ad4a1b5d8483a3eaf328
Justine George <justinegeo96@gmail.com>
2023-04-09 18:44:29 -0500

    Time: 61 ms (56.81%), Space: 14.1 MB (32.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,19 +1,29 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-
-        memo = {}
-        # @cache
-        # either use @cache or manually memoize
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i <= 1:
-                return 0
+#         # recursive solution
+#         memo = {}
+#         # @cache
+#         # either use @cache or manually memoize
+#         def dfs(i):
+#             if i in memo:
+#                 return memo[i]
+#             if i <= 1:
+#                 return 0
             
-            downOne = cost[i - 1] + dfs(i - 1)
-            downTwo = cost[i - 2] + dfs(i - 2)
+#             downOne = cost[i - 1] + dfs(i - 1)
+#             downTwo = cost[i - 2] + dfs(i - 2)
             
-            memo[i] = min(downOne, downTwo)
-            return memo[i]
+#             memo[i] = min(downOne, downTwo)
+#             return memo[i]
         
-        return dfs(len(cost))
+#         return dfs(len(cost))
+    
+        # iterative solution
+        # solve from right to left
+        
+        dp = cost.copy()
+        dp = dp + [0]
+        for i in range(len(cost) - 2, -1, -1):
+            dp[i] = dp[i] + min(dp[i + 1], dp[i + 2])
+            
+        return min(dp[0], dp[1])
