47d3174e1d2c2f4f916b75200df00038dbf3056d
Justine George <justinegeo96@gmail.com>
2023-04-09 06:41:34 -0500

    Time: 35 ms (45.27%), Space: 13.8 MB (96.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,28 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # dict = {(m-1, n-1): 0}
+        # for i in range(m - 1, -1, -1):
+        #     for j in range(n - 1, -1, -1):
+        #         if i == m - 1 or j == n - 1:
+        #             dict[(i, j)] = 1
+        #         else:
+        #             dict[(i, j)] = dict[(i + 1, j)] + dict[(i, j + 1)]
+        # return dict[(0, 0)]
+    
+    
         
-        dict = {(m-1, n-1): 0}
+        # start from bottom row
+        row = [1] * n
         
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if i == m - 1 or j == n - 1:
-                    dict[(i, j)] = 1
-                else:
-                    dict[(i, j)] = dict[(i + 1, j)] + dict[(i, j + 1)]
+        for i in range(m - 1):# go through other rows
+            newRow = [1] * n # this is going to the row above bottom row
+            
+            # start at the second to last position, since we know the last column is all 1s, go till the beginning, in reverse order
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            
+            # this new row is now row
+            row = newRow
         
-        return dict[(0, 0)]
+        # now row is the top row
+        return row[0]
