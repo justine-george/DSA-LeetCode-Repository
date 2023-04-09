20915adda24f63f7bc35c861315d521b02118da0
Justine George <justinegeo96@gmail.com>
2023-04-09 05:49:32 -0500

    Time: 345 ms (32.55%), Space: 21.6 MB (34.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -7,21 +7,21 @@ class Solution:
                 return False
             if grid[r][c] == '0':
                 return False
-            st = deque([(r,c)])
+            q = deque([(r,c)])
             visited.add((r,c))
-            while st:
-                r, c = st.pop()
-                appendIfNewAndValidLand(grid, r - 1, c, visited, st)
-                appendIfNewAndValidLand(grid, r + 1, c, visited, st)
-                appendIfNewAndValidLand(grid, r, c + 1, visited, st)
-                appendIfNewAndValidLand(grid, r, c - 1, visited, st)
+            while q:
+                r, c = q.popleft()
+                appendIfNewAndValidLand(grid, r - 1, c, visited, q)
+                appendIfNewAndValidLand(grid, r + 1, c, visited, q)
+                appendIfNewAndValidLand(grid, r, c + 1, visited, q)
+                appendIfNewAndValidLand(grid, r, c - 1, visited, q)
             return True
 
-        def appendIfNewAndValidLand(grid, r, c, visited, st):
+        def appendIfNewAndValidLand(grid, r, c, visited, q):
             if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
                 if grid[r][c] == '1':
                     if (r, c) not in visited:
-                        st.append((r, c))
+                        q.append((r, c))
                         visited.add((r, c))
         
         visited = set()
