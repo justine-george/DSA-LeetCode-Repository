42b177f232638c4b7818c4664236fd4a4d75e885
Justine George <justinegeo96@gmail.com>
2023-04-08 19:24:55 -0500

    Time: 48 ms (29.37%), Space: 15.3 MB (81.87%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,26 +6,38 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#         # recursive dfs: T: O(n), S: O(n)
+#         # recursive dfs
+#         # T: O(n), S: O(n)
 #         # if balanced tree, then S: O(logn)
 #         if not root:
 #             return 0
             
 #         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-        # iterative bfs
-        # T: O(n), S: O(n)
-        # if it is a skewed binary tree, max queue size would be just 1
-        q = collections.deque([root]) if root else None
-        levelCount = 0
+#         # iterative bfs
+#         # T: O(n), S: O(n)
+#         # if it is a skewed binary tree, max queue size would be just 1
+#         q = collections.deque([root]) if root else None
+#         levelCount = 0
         
-        while q:
-            for i in range(len(q)): # pop all nodes in this level
-                cur = q.popleft()
-                if cur.left:
-                    q.append(cur.left)
-                if cur.right:
-                    q.append(cur.right)
-            levelCount += 1
+#         while q:
+#             for i in range(len(q)): # pop all nodes in this level
+#                 cur = q.popleft()
+#                 if cur.left:
+#                     q.append(cur.left)
+#                 if cur.right:
+#                     q.append(cur.right)
+#             levelCount += 1
                 
-        return levelCount
+#         return levelCount
+        
+        st = [[root, 1]] if root else None
+        maxLevel = 0
+        while st:
+            curNode, curLevel = st.pop()
+            maxLevel = max(maxLevel, curLevel)
+            if curNode.left:
+                st.append([curNode.left, curLevel + 1])
+            if curNode.right:
+                st.append([curNode.right, curLevel + 1])
+        return maxLevel
