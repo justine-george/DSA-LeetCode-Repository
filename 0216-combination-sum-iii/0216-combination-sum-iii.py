552cd80e9dbda7b634d5b1214b28d78b2d18cf0f
Justine George <justinegeo96@gmail.com>
2023-04-09 05:07:30 -0500

    Time: 32 ms (70.86%), Space: 13.9 MB (15.66%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+        cur = []
+        candidates = [i for i in range(1, 10)]
+        # backtracking
+        def dfs(i, total, depth):
+            if total == n and depth == k:
+                res.append(cur.copy())
+                return
+            
+            for j in range(i, len(candidates)):
+                if total + candidates[j] > n or depth >= k:
+                    break
+                cur.append(candidates[j])
+                dfs(j + 1, total + candidates[j], depth + 1)
+                cur.pop()
+        
+        dfs(0, 0, 0)
+        return res
