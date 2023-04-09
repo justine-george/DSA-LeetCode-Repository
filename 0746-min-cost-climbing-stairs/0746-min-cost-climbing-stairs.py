60c6fdab8d0d3dfee39b2bc5171c7e38cb6cc704
Justine George <justinegeo96@gmail.com>
2023-04-09 18:30:58 -0500

    Time: 66 ms (27.03%), Space: 16.2 MB (8.18%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,14 +1,18 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        @cache
+        memo = {}
+        # @cache
         def dfs(i):
+            if i in memo:
+                return memo[i]
             if i <= 1:
                 return 0
             
             downOne = cost[i - 1] + dfs(i - 1)
             downTwo = cost[i - 2] + dfs(i - 2)
             
-            return min(downOne, downTwo)
+            memo[i] = min(downOne, downTwo)
+            return memo[i]
         
         return dfs(len(cost))
