e96abec58b3e5eceece5c126547d87164d35b359
Justine George <justinegeo96@gmail.com>
2023-04-09 04:46:45 -0500

    Time: 69 ms (42.78%), Space: 14 MB (55.60%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,9 +1,30 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
+#         res = []
+#         # backtracking - 2 decisions
+#         def dfs(i, cur, total):
+#             if total == target:
+#                 res.append(cur.copy())
+#                 return
+#             if i >= len(candidates) or total > target:
+#                 return
+            
+#             # decision 1: include candidate[i] and continue to include ith position
+#             cur.append(candidates[i])
+#             dfs(i, cur, total + candidates[i])
+            
+#             # decision 2: don't candidate[i] and don't use ith index ever
+#             cur.pop()
+#             dfs(i + 1, cur, total)
+            
+#         dfs(0, [], 0)
+#         return res
+    
         res = []
         # backtracking - 2 decisions
-        def dfs(i, cur, total):
+        cur = []
+        def dfs(i, total):
             if total == target:
                 res.append(cur.copy())
                 return
@@ -12,11 +33,11 @@ class Solution:
             
             # decision 1: include candidate[i] and continue to include ith position
             cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
+            dfs(i, total + candidates[i])
             
             # decision 2: don't candidate[i] and don't use ith index ever
             cur.pop()
-            dfs(i + 1, cur, total)
+            dfs(i + 1, total)
             
-        dfs(0, [], 0)
+        dfs(0, 0)
         return res
