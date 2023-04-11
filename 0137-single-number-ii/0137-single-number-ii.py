a28a6c757742b6ea3cd56c0d468000e107887f4a
Justine George <justinegeo96@gmail.com>
2023-04-10 22:09:02 -0500

    Time: 59 ms (77.18%), Space: 15.6 MB (81.45%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # bitwise
+        # https://youtu.be/cOFAmaMBVps?t=493
         ones, twos = 0, 0
         for n in nums:
             ones = (ones ^ n) & ~twos
