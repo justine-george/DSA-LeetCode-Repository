f86867146b8cd3dc467f4406b7fbe00ccf7e50b2
Justine George <justinegeo96@gmail.com>
2023-04-10 19:47:52 -0500

    Time: 42 ms (9.36%), Space: 13.9 MB (5.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,17 +4,21 @@ class Solution:
         # numVal += 1
         # return [int(digit) for digit in str(numVal)]
     
-        res = []
-        carry = 0
-        for i in range(len(digits) - 1, -1, -1):
-            sum = digits[i] + carry
-            if i == (len(digits) - 1):
-                sum += 1
-            carry = sum // 10
-            digits[i] = sum % 10
-            
-            if carry == 0:
-                break
-        if carry != 0:
-            return [carry] + digits
-        return digits
+        # carry = 0
+        # for i in range(len(digits) - 1, -1, -1):
+        #     sum = digits[i] + carry
+        #     if i == (len(digits) - 1):
+        #         sum += 1
+        #     carry = sum // 10
+        #     digits[i] = sum % 10
+        #     if carry == 0:
+        #         break
+        # return digits if carry == 0 else [1] + digits
+    
+        # even simpler code
+        for i in range(len(digits) -1, -1, -1):
+            digits[i] += 1
+            if digits[i] != 10:
+                return digits
+            digits[i] = 0
+        return [1] + digits
