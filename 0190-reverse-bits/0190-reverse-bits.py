896efe9bf8789e452e799ef33e10430a0d0d7da8
Justine George <justinegeo96@gmail.com>
2023-04-11 01:39:04 -0500

    Time: 35 ms (55.81%), Space: 13.8 MB (36.62%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -8,12 +8,11 @@ class Solution:
         #     i += 1
         # return int("".join(str(r) for r in res), 2)
 
-        # S: O(1) approach
+        # T: O(1) and S: O(1) approach
         res = 0
         for i in  range(32):
             # start from rightmost bit, and work towards left
             bit = (n >> i) & 1
-            
             # start appending to left most position, work towards right
             res = res | (bit << (31 - i))
         return res
