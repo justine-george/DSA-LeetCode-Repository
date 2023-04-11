7d667237095e7ef4ad32e2a98f477cbd1e7c611f
Justine George <justinegeo96@gmail.com>
2023-04-10 19:55:13 -0500

    Time: 36 ms (45.44%), Space: 13.9 MB (5.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -15,10 +15,26 @@ class Solution:
         #         break
         # return digits if carry == 0 else [1] + digits
     
-        # even simpler code
-        for i in range(len(digits) -1, -1, -1):
-            digits[i] += 1
-            if digits[i] != 10:
-                return digits
-            digits[i] = 0
-        return [1] + digits
+        # # even simpler code
+        # for i in range(len(digits) -1, -1, -1):
+        #     digits[i] += 1
+        #     if digits[i] != 10:
+        #         return digits
+        #     digits[i] = 0
+        # return [1] + digits
+    
+        # different approach
+        digits = digits[::-1] # reverse list
+        one, i = 1, 0
+        while one:
+            if i < len(digits):
+                if digits[i] == 9: # carry in this case
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(one)
+                one = 0
+            i += 1
+        return digits[::-1]
