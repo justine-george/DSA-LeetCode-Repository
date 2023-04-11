b7c7b18493d6e7774e3001e2cec28922c4aa45e4
Justine George <justinegeo96@gmail.com>
2023-04-10 19:41:20 -0500

    Time: 34 ms (62.62%), Space: 13.8 MB (45.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,5 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        numVal = int("".join(str(e) for e in digits))
-        numVal += 1
-        return [int(digit) for digit in str(numVal)]
+        # numVal = int("".join(str(e) for e in digits))
+        # numVal += 1
+        # return [int(digit) for digit in str(numVal)]
+    
+        res = []
+        carry = 0
+        for i in range(len(digits) - 1, -1, -1):
+            sum = digits[i] + carry
+            if i == (len(digits) - 1):
+                sum += 1
+            carry = sum // 10
+            digits[i] = sum % 10
+            
+            if carry == 0:
+                break
+        if carry != 0:
+            return [carry] + digits
+        return digits
