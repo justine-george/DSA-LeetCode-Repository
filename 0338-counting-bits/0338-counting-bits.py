56990db7fee210967196e10d60541def1cad25de
Justine George <justinegeo96@gmail.com>
2023-04-10 22:51:00 -0500

    Time: 75 ms (93.41%), Space: 20.7 MB (65.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -15,23 +15,31 @@ class Solution:
 #             ans[i] = countBits(i)
 #         return ans
     
-        # 0 0000 
-        # 1 0001 - offset 1
-        # 2 0010 - offset 2
-        # 3 0011 - offset 2
-        # 4 0100 - offset 4
-        # 5 0101 - offset 4
-        # 6 0110 - offset 4
-        # 7 0111 - offset 4
-        # 8 1000 - offset 8
+#         # 0 0000 
+#         # 1 0001 - offset 1
+#         # 2 0010 - offset 2
+#         # 3 0011 - offset 2
+#         # 4 0100 - offset 4
+#         # 5 0101 - offset 4
+#         # 6 0110 - offset 4
+#         # 7 0111 - offset 4
+#         # 8 1000 - offset 8
         
-        # T: O(n), S: O(1)
-        dp = [0] * (n + 1)
-        offset = 1 # stores highest power of 2
+#         # T: O(n), S: O(1)
+#         dp = [0] * (n + 1)
+#         offset = 1 # stores highest power of 2
         
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i - offset]
+#         for i in range(1, n + 1):
+#             if offset * 2 == i:
+#                 offset = i
+#             dp[i] = 1 + dp[i - offset]
             
+#         return dp
+    
+        # different approach using bitshift
+        # If we shift 5 to the right by 1, and it becomes 2, and 5 & 1 is 1, so the number of 1's in 5, is actually the number of 1's in 2 plus 1, because 5&1 == 1.
+        # Similarly, if we shift 4 to the right by 1, which becomes 2 as well, and 4&1 is 0, so number of 1's in 4, is the the number of 1's in 2 plus 0, because 4&1 == 0
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i>>1] + (i&1)
         return dp
