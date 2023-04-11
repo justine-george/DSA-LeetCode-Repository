475575f06e1275f717d84447e4aeeded64c7f120
Justine George <justinegeo96@gmail.com>
2023-04-11 01:35:32 -0500

    Time: 39 ms (27.66%), Space: 13.8 MB (88.02%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,10 +1,19 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = [0] * 32
-        i = 0
-        while n and i < 32:
-            res[i] = n & 1
-            n = n >> 1
-            i += 1
+        # res = [0] * 32
+        # i = 0
+        # while n and i < 32:
+        #     res[i] = n & 1
+        #     n = n >> 1
+        #     i += 1
+        # return int("".join(str(r) for r in res), 2)
+
+        # S: O(1) approach
+        res = 0
+        for i in  range(32):
+            # start from rightmost bit
+            bit = (n >> i) & 1
             
-        return int("".join(str(r) for r in res), 2)
+            # start appending to left most position
+            res = res | (bit << (31 - i))
+        return res
