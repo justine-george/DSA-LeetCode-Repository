2006817c19d86ac93fe47b67f637d71cb8e59f5b
Justine George <justinegeo96@gmail.com>
2023-04-10 21:31:31 -0500

    Time: 127 ms (91.20%), Space: 16.8 MB (42.85%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # XOR all numbers - what's left is the single number
+        # XOR - inequality checker
+        # For ex. a ^ b ^ a = (a ^ a) ^ b = 0 ^ b = b
         res = 0
         for n in nums:
             res = n ^ res
         return res
-                
- 
