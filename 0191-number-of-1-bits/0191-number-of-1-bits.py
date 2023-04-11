a34008e567f3b2113ab66b0bb0b3517c0ec65ecb
Justine George <justinegeo96@gmail.com>
2023-04-10 22:23:19 -0500

    Time: 28 ms (87.63%), Space: 13.9 MB (37.83%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,7 +3,7 @@ class Solution:
         # count = 0
         # while n > 0:
         #     count += n % 2
-        #     n = n // 2
+        #     n = n // 2 # or n = n >> 1
         # return count
     
         # more efficient solution
