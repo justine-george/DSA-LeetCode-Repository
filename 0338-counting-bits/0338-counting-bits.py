615c8b3ab5a6d088ebe8f8d806afd0a58a7cad32
Justine George <justinegeo96@gmail.com>
2023-04-10 22:39:25 -0500

    Time: 86 ms (65.00%), Space: 20.8 MB (18.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,15 +2,28 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
 #         5 - 101
 #         [0, 1, 1, 2, 1, 2]
-        def countBits(i):
-            count = 0
-            while i:
-                i = i & (i - 1)
-                count += 1
-            return count
+#         def countBits(i):
+#             count = 0
+#             while i:
+#                 # zeroing out the least significant nonzero bit
+#                 i = i & (i - 1) 
+#                 count += 1
+#             return count
         
-        ans = [0] * (n + 1)
-        for i in range(n + 1):
-            # count number of 1 bits in i
-            ans[i] = countBits(i)
-        return ans
+#         # T: O(nlogn), S: O(1)
+#         ans = [0] * (n + 1)
+#         for i in range(n + 1):
+#             # count number of 1 bits in i
+#             ans[i] = countBits(i)
+#         return ans
+    
+        # T: O(n), S: O(1)
+        dp = [0] * (n + 1)
+        offset = 1 # stores highest power of 2
+        
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+            
+        return dp
