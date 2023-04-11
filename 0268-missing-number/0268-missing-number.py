958a50cf7a2f4491a4a8b491f987a626d7f1b901
Justine George <justinegeo96@gmail.com>
2023-04-11 04:55:11 -0500

    Time: 125 ms (94.53%), Space: 15 MB (99.90%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,7 +5,7 @@ class Solution:
         # return (n*(n + 1)//2) - sum(nums)
     
         # XOR solution - more efficient
-        # [0, 1, 2, 3] ^ [0, 1, 3] = odd value remains
+        # [0, 1, 2, 3] ^ [0, 1, 3] = missing no. remains
         # since we are iterating from 0 to end - 1, we initialize with end value and start accumulating XOR
         res = len(nums)
         for i in range(len(nums)):
