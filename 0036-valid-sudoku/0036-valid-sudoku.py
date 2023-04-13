2f2c0f9ce58de343563d6437e57eff9e5d37e95f
Justine George <justinegeo96@gmail.com>
2023-04-13 05:48:05 -0500

    Time: 94 ms (76.63%), Space: 13.8 MB (69.99%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        for i in range(len(board)):
-            setRow = set()
-            setCol = set()
-            for j in range(len(board[0])):
-                # check all rows
-                if board[i][j] != ".":
-                    if board[i][j] in setRow:
-                        return False
-                    setRow.add(board[i][j])
-                # check all columns
-                if board[j][i] != ".":
-                    if board[j][i] in setCol:
-                        return False
-                    setCol.add(board[j][i])
-        
-        # check all 3x3s
-        for m in range(0, 9, 3):
-            for n in range(0, 9, 3):
-                set3x3 = set()
-                for i in range(m, m + 3):
-                    for j in range(n, n + 3):
-                        if board[i][j] != ".":
-                            if board[i][j] in set3x3:
-                                return False
-                            set3x3.add(board[i][j])
+        # more efficient, iteration in 9^2
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        tinysquares = collections.defaultdict(set)
 
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    if (board[r][c] in rows[r] or 
+                        board[r][c] in cols[c] or
+                        board[r][c] in tinysquares[(r // 3, c // 3)]):
+                        return False
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    tinysquares[(r // 3, c // 3)].add(board[r][c])
+                    
         return True
