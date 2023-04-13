773f639cd4afca9d05fd3fd7bb8cde7be46d2307
Justine George <justinegeo96@gmail.com>
2023-04-13 05:13:50 -0500

    Time: 98 ms (89.25%), Space: 19 MB (29.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,45 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         # T: O(klogn) heap approach
-#         freq = collections.defaultdict(lambda: 0)
-#         for n in nums:
-#             freq[n] += 1
+        # T: O(klogn) heap approach
+        freq = collections.defaultdict(lambda: 0)
+        for n in nums:
+            freq[n] += 1
             
-#         minHeap = [(count, num) for num, count in freq.items()]
-#         heapq.heapify(minHeap)
+        maxHeap = [(-count, num) for num, count in freq.items()]
+        heapq.heapify(maxHeap)
+            
+        # Note: pop from the heap k times to get top k frequent elements
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(maxHeap)[1])
+        return res
+    
+    
+#         # T: O(n) using bucket sort with a twist
+#         # array indexed by count and value is a list of numbers with this count
+#         countDict = {}
         
-#         # remove least frequent elements, so that remaining elements are top k frequent
-#         while len(minHeap) > k:
-#             heapq.heappop(minHeap)
-            
-#         # Note: another approach is to make a maxHeap and pop from the heap k times to get top k frequent elements
+#         maxCount = 0
+#         for n in nums:
+#             countDict[n] = 1 + countDict.get(n, 0)
+#             maxCount = max(maxCount, countDict[n])
+        
+#         # make bucket of size max count
+#         bucket = [[] for i in range(maxCount)]
+        
+#         for num, freq in countDict.items():
+#             bucket[freq - 1].append(num)
         
 #         res = []
-#         for count, n in minHeap:
-#             res.append(n)
-#         return res
-    
-    
-        # T: O(n) using bucket sort with a twist
-        # array indexed by count and value is a list of numbers with this count
-        countDict = {}
-        
-        maxCount = 0
-        for n in nums:
-            countDict[n] = 1 + countDict.get(n, 0)
-            maxCount = max(maxCount, countDict[n])
-        
-        # make bucket of size max count
-        bucket = [[] for i in range(maxCount)]
-        
-        for num, freq in countDict.items():
-            bucket[freq - 1].append(num)
-        
-        res = []
-        for i in range(len(bucket) - 1, -1, -1):
-            for n in bucket[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-        
-        
+#         for i in range(len(bucket) - 1, -1, -1):
+#             for n in bucket[i]:
+#                 res.append(n)
+#                 if len(res) == k:
+#                     return res
