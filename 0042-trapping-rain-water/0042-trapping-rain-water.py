1dc9135cf96cee94772fcbb3a0e2e38a4d33dbda
Justine George <justinegeo96@gmail.com>
2023-04-13 21:50:06 -0500

    Time: 127 ms (61.90%), Space: 16 MB (73.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,32 +1,32 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # T: O(n), S: O(n)
-        if not height:
-            return 0
+#         # T: O(n), S: O(n)
+#         if not height:
+#             return 0
         
-        n = len(height)
-        maxLeftHeight = [0] * n
-        maxRightHeight = [0] * n
+#         n = len(height)
+#         maxLeftHeight = [0] * n
+#         maxRightHeight = [0] * n
         
-        # calculate maxLeftHeight
-        maxH = 0
-        for i in range(1, n):
-            maxH = max(maxH, height[i - 1])
-            maxLeftHeight[i] = maxH
+#         # calculate maxLeftHeight
+#         maxH = 0
+#         for i in range(1, n):
+#             maxH = max(maxH, height[i - 1])
+#             maxLeftHeight[i] = maxH
         
-        # calculate maxRightHeight
-        maxH = 0
-        for i in range(n - 2, -1, -1):
-            maxH = max(maxH, height[i + 1])
-            maxRightHeight[i] = maxH
+#         # calculate maxRightHeight
+#         maxH = 0
+#         for i in range(n - 2, -1, -1):
+#             maxH = max(maxH, height[i + 1])
+#             maxRightHeight[i] = maxH
         
-        # calculate amount of trapped water
-        totalWater = 0
-        for i in range(n):
-            curWater = min(maxLeftHeight[i], maxRightHeight[i]) - height[i]
-            curWater = 0 if curWater <= 0 else curWater
-            totalWater += curWater
-        return totalWater
+#         # calculate amount of trapped water
+#         totalWater = 0
+#         for i in range(n):
+#             curWater = min(maxLeftHeight[i], maxRightHeight[i]) - height[i]
+#             curWater = 0 if curWater <= 0 else curWater
+#             totalWater += curWater
+#         return totalWater
     
         # T: O(n), S: O(1)
         # 2 pointers
