7a65a99ff96000b00ce9e0786497d86a687eb1fe
Justine George <justinegeo96@gmail.com>
2023-04-15 01:53:34 -0500

    Time: 127 ms (60.75%), Space: 13.9 MB (88.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,14 +1,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+#         count = {}
+#         res = 0
+        
+#         l = 0
+#         for r in range(len(s)):
+#             count[s[r]] = 1 + count.get(s[r], 0)
+#             # if current window is not valid, reduce window from left
+#             while (r - l + 1) - max(count.values()) > k:
+#                 count[s[l]] -= 1
+#                 l += 1
+#             res = max(res, r - l + 1)
+        
+#         return res
+    
+        # More efficient
         count = {}
         res = 0
         
         l = 0
+        maxCount = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            
-            # check if current window is valid, if not valid, reduce window from left
-            while (r - l + 1) - max(count.values()) > k:
+            maxCount = max(maxCount, count[s[r]])
+            # if current window is not valid, reduce window from left
+            while (r - l + 1) - maxCount > k:
                 count[s[l]] -= 1
                 l += 1
             
