f125ac47708476061e020cab45b011df60eee976
Justine George <justinegeo96@gmail.com>
2023-04-15 06:59:55 -0500

    Time: 61 ms (68.19%), Space: 18.4 MB (10.76%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,27 +2,15 @@ class MinStack:
 
     def __init__(self):
         self.stack = collections.deque()
-        # self.minMap = {0: float('inf')}
-        # self.min = float('inf')
-        # self.size = 0
         
     def push(self, val: int) -> None:
         if not self.stack:
             self.stack.append((val, val))
         else:
             self.stack.append((val, min(val, self.stack[-1][1])))
-        
-        # self.stack.append(val)
-        # self.size += 1
-        # self.min = min(self.min, val)
-        # self.minMap[self.size] = self.min
-        # print(f"{self.stack}: min: {self.min}")
 
     def pop(self) -> None:
         self.stack.pop()
-        # self.size -= 1
-        # self.min = self.minMap[self.size]
-        # print(f"{self.stack}: min: {self.min}")
 
     def top(self) -> int:
         return self.stack[-1][0]
