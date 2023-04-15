d52319dea20bfcb0290f827fdafef7fbfef178b7
Justine George <justinegeo96@gmail.com>
2023-04-15 03:50:04 -0500

    Time: 103 ms (88.42%), Space: 15.3 MB (22.58%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        if len(p) > len(s):
+            return res
+        
+        pcount, scount = collections.defaultdict(lambda: 0), collections.defaultdict(lambda: 0)
+        
+        for i in range(len(p)):
+            pcount[p[i]] += 1
+            scount[s[i]] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += (1 if pcount[chr(i + ord('a'))] == scount[chr(i + ord('a'))] else 0)
+        
+        if matches == 26:
+            res.append(0)
+            
+        l = 0
+        for r in range(len(p), len(s)):
+            # current char is s[r]
+            scount[s[r]] += 1
+            if scount[s[r]] == pcount[s[r]]: # now equal
+                matches += 1
+            elif scount[s[r]] - 1 == pcount[s[r]]: # were equal
+                matches -= 1
+            
+            # char to be removed from window is s[l]
+            scount[s[l]] -= 1
+            if scount[s[l]] == pcount[s[l]]: # now equal
+                matches += 1
+            elif scount[s[l]] + 1 == pcount[s[l]]: # were equal
+                matches -= 1
+            l += 1
+        
+            if matches == 26:
+                res.append(l)
+        
+        return res
