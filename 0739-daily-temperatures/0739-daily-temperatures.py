616c8ce4921660d1050de80328d9404bed0f2095
Justine George <justinegeo96@gmail.com>
2023-04-16 02:25:39 -0500

    Time: 1326 ms (93.90%), Space: 28.1 MB (93.75%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,36 +1,30 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # T: O(n), S: O(n)
-        res = [0] * len(temperatures)
-        st = [] # keep stack monotonic non-increasing, save indexes only
-        for i in range(len(temperatures)):
-            while st and temperatures[st[-1]] < temperatures[i]:
-                index = st.pop()
-                res[index] = i - index
-            st.append(i)
-        return res
-    
-        
         # T: O(n), S: O(1) - reverse iterate, only constant space
         n = len(temperatures)
         res = [0] * n
         
-        for i in range(n - 2, -1, -1):
-            curTemp = temperatures[i]
-            
-            it = i + 1
-            while it < n and curTemp > temperatures[it]:
-                if res[it] != 0:
-                    it += res[it]
-                else:
+        for i in range(n - 1, -1, -1):
+            j = i + 1
+            while j < n:
+                if temperatures[j] > temperatures[i]:
+                    res[i] = j - i
                     break
-            res[i] = it - i
-            
+                elif res[j] == 0:
+                    break
+                else:
+                    j += res[j]
+
         return res
-#             i = 5
-            
-#             it = 6
-            
-#             n = 8
-        
+    
+        # # T: O(n), S: O(n)
+        # res = [0] * len(temperatures)
+        # st = [] # keep stack monotonic non-increasing, save indexes only
+        # for i in range(len(temperatures)):
+        #     while st and temperatures[st[-1]] < temperatures[i]:
+        #         index = st.pop()
+        #         res[index] = i - index
+        #     st.append(i)
+        # return res
+    
         
