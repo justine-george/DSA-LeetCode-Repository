54a0eff123b3e301ebab2a1b685be043c907dc75
Justine George <justinegeo96@gmail.com>
2023-04-16 02:17:57 -0500

    Time: 1381 ms (61.95%), Space: 28.7 MB (61.23%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # T: O(n)
+        # T: O(n), S: O(n)
         res = [0] * len(temperatures)
         st = [] # keep stack monotonic non-increasing, save indexes only
         for i in range(len(temperatures)):
@@ -9,3 +9,28 @@ class Solution:
                 res[index] = i - index
             st.append(i)
         return res
+    
+        
+        # T: O(n), S: O(1) - reverse iterate, only constant space
+        n = len(temperatures)
+        res = [0] * n
+        
+        for i in range(n - 2, -1, -1):
+            curTemp = temperatures[i]
+            
+            it = i + 1
+            while it < n and curTemp > temperatures[it]:
+                if res[it] != 0:
+                    it += res[it]
+                else:
+                    break
+            res[i] = it - i
+            
+        return res
+#             i = 5
+            
+#             it = 6
+            
+#             n = 8
+        
+        
