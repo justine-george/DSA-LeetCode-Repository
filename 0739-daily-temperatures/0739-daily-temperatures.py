4e15c1f228e4da1c7ef6cf3e67a3859d3083c173
Justine George <justinegeo96@gmail.com>
2023-04-16 01:52:28 -0500

    Time: 1368 ms (70.91%), Space: 28.5 MB (67.28%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,10 +2,10 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # T: O(n)
         res = [0] * len(temperatures)
-        st = [] # keep stack monotonic non-increasing
+        st = [] # keep stack monotonic non-increasing, save indexes only
         for i in range(len(temperatures)):
-            while st and st[-1][0] < temperatures[i]:
-                val, index = st.pop()
+            while st and temperatures[st[-1]] < temperatures[i]:
+                index = st.pop()
                 res[index] = i - index
-            st.append((temperatures[i], i))
+            st.append(i)
         return res
