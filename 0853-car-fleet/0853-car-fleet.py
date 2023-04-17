22bd052411da39a7817aea86562728c8bb317f45
Justine George <justinegeo96@gmail.com>
2023-04-17 00:46:45 -0500

    Time: 918 ms (81.21%), Space: 36.2 MB (52.78%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,7 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # visualize in a cartesian plane, speed is the slope, position is the y axis, and time is x axis.
-        # make a pair. then sort and iterate in reverse
+        # Visualize the problem in a Cartesian plane:
+        # - Speed is the slope
+        # - Position is the y-axis
+        # - Time is the x-axis
+        # Create a pair of position and speed, sort the pairs, and iterate through them in reverse order
         # [
         #     (0, 1),
         #     (3, 3),
@@ -13,12 +16,12 @@ class Solution:
         
         pair = [(p, s) for p, s in zip(position, speed)]
         pair.sort()
-        st = [] # stack stores time taken to reach target
+        st = [] # stack stores the time taken to reach the target
         
-        for p, s in pair[::-1]: # iterate in reverse
-            # add to the stack only if p,s won't meet with top of the stack before target
-            # this is not a while loop since only one check is needed, no need to check values deep in stack since they are already checked (the reason we iterate in reverse order.)
-            if not st or st and (target - p)/s > st[-1]:
+        for p, s in pair[::-1]: # iterate in reverse order
+            # Add the pair to the stack only if it won't meet the top of the stack before reaching the target
+            # A single check is sufficient, as there is no need to check values deeper in the stack since they have already been checked (the reason we iterate in reverse order)
+            if not st or st and (target - p)/s > st[-1]: # time taken for the preceding car is longer than the one in front => they won't meet.
                     st.append((target - p)/s)
         
         return len(st)
