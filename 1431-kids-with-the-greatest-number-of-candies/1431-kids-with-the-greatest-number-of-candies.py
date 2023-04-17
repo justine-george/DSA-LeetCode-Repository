722898361d7bde05242067c25d016330d611600b
Justine George <justinegeo96@gmail.com>
2023-04-17 06:35:36 -0500

    Time: 34 ms (90.72%), Space: 13.8 MB (50.96%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,4 +1,4 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        maxCandy = max(candies)
-        return [True if c + extraCandies >= maxCandy else False for c in candies]
+        val = max(candies) - extraCandies
+        return [True if c >= val else False for c in candies]
