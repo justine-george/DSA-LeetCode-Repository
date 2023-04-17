33e8c7e4f4d891624c60f1f74bfdf8b24ac94fd7
Justine George <justinegeo96@gmail.com>
2023-04-17 01:00:40 -0500

    Time: 883 ms (94.31%), Space: 36 MB (64.36%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,17 +1,7 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # Visualize the problem in a Cartesian plane:
-        # - Speed is the slope
-        # - Position is the y-axis
-        # - Time is the x-axis
         # Create a pair of position and speed, sort the pairs, and iterate through them in reverse order
-        # [
-        #     (0, 1),
-        #     (3, 3),
-        #     (5, 1),
-        #     (8, 4),
-        #     (10, 2)
-        # ]
+        # [(0, 1),(3, 3),(5, 1),(8, 4),(10, 2)]
         # T: O(nlogn)
         
         pair = [(p, s) for p, s in zip(position, speed)]
@@ -23,5 +13,17 @@ class Solution:
             # A single check is sufficient, as there is no need to check values deeper in the stack since they have already been checked (the reason we iterate in reverse order)
             if not st or st and (target - p)/s > st[-1]: # time taken for the preceding car is longer than the one in front => they won't meet.
                     st.append((target - p)/s)
-        
         return len(st)
+    
+    
+        # slightly different code, same method
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        st = []
+        
+        for p, s in pair:
+            st.append((target - p)/s)
+            if len(st) >= 2 and st[-1] <= st[-2]: # top of the stack would be the car just behind st[-2]. If the car behind takes less or equal time than the car in the front to reach the target, then it is a fleet. so delete the faster car at the back from the stack,. ie pop.
+                st.pop()
+        return len(st)
+                
