c56e35cf8fa8c884ad20e3965da615fff4f66abe
Justine George <justinegeo96@gmail.com>
2023-04-17 01:01:07 -0500

    Time: 945 ms (67.40%), Space: 36.4 MB (45.73%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -4,16 +4,16 @@ class Solution:
         # [(0, 1),(3, 3),(5, 1),(8, 4),(10, 2)]
         # T: O(nlogn)
         
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort()
-        st = [] # stack stores the time taken to reach the target
+#         pair = [(p, s) for p, s in zip(position, speed)]
+#         pair.sort()
+#         st = [] # stack stores the time taken to reach the target
         
-        for p, s in pair[::-1]: # iterate in reverse order
-            # Add the pair to the stack only if it won't meet the top of the stack before reaching the target
-            # A single check is sufficient, as there is no need to check values deeper in the stack since they have already been checked (the reason we iterate in reverse order)
-            if not st or st and (target - p)/s > st[-1]: # time taken for the preceding car is longer than the one in front => they won't meet.
-                    st.append((target - p)/s)
-        return len(st)
+#         for p, s in pair[::-1]: # iterate in reverse order
+#             # Add the pair to the stack only if it won't meet the top of the stack before reaching the target
+#             # A single check is sufficient, as there is no need to check values deeper in the stack since they have already been checked (the reason we iterate in reverse order)
+#             if not st or st and (target - p)/s > st[-1]: # time taken for the preceding car is longer than the one in front => they won't meet.
+#                     st.append((target - p)/s)
+#         return len(st)
     
     
         # slightly different code, same method
