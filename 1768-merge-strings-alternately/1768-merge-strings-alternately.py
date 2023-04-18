919715d52a12a324ecf3ead04487978589105364
Justine George <justinegeo96@gmail.com>
2023-04-17 23:48:30 -0500

    Time: 28 ms (85.44%), Space: 13.9 MB (55.13%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,9 +1,3 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ""
-        for a, b in zip_longest(word1, word2):
-            if a:
-                res += a
-            if b:
-                res += b
-        return res
+        return "".join(a + b for a, b in itertools.zip_longest(word1, word2, fillvalue=""))
