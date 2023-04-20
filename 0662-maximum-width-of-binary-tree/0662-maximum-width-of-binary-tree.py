777d3a94305b298673642dd2bb425e3934d87b9d
Justine George <justinegeo96@gmail.com>
2023-04-20 00:13:57 -0500

    Time: 49 ms (48.29%), Space: 14.8 MB (39.48%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -6,54 +6,54 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-#         # BFS solution
-#         # T: O(n), S: O(n)
-#         if not root:
-#             return 0
-        
-#         maxWidth = 0
-#         # queue -> [(node, colIndex)] colIndex starting from 0
-#         q = collections.deque([(root, 0)])
-        
-#         while q:
-#             levelLength = len(q)
-#             _, levelHeadIndex = q[0]
-            
-#             # iterate through the current level
-#             for _ in range(levelLength):
-#                 cur, colIndex = q.popleft()
-#                 if cur.left:
-#                     q.append((cur.left, 2 * colIndex))
-#                 if cur.right:
-#                     q.append((cur.right, 2 * colIndex + 1))
-            
-#             maxWidth = max(maxWidth, colIndex - levelHeadIndex + 1)
-        
-#         return maxWidth
-    
-        
-        # DFS solution
+        # BFS solution
         # T: O(n), S: O(n)
         if not root:
             return 0
-
-        # save first visit of a node in a particular depth
-        firstNodeDepth = {}
         
         maxWidth = 0
-        def dfs(node, depth, colIndex):
-            nonlocal maxWidth
-            if not node:
-                return
-            
-            if depth not in firstNodeDepth:
-                firstNodeDepth[depth] = colIndex
-            
-            maxWidth = max(maxWidth, colIndex - firstNodeDepth[depth] + 1)
-            
-            dfs(node.left, depth + 1, 2 * colIndex)
-            dfs(node.right, depth + 1, 2 * colIndex + 1)
-            
-            return maxWidth
+        # queue -> [(node, colIndex)] colIndex starting from 0
+        q = collections.deque([(root, 0)])
         
-        return dfs(root, 0, 0)
+        while q:
+            levelLength = len(q)
+            _, levelHeadIndex = q[0]
+            
+            # iterate through the current level
+            for _ in range(levelLength):
+                cur, colIndex = q.popleft()
+                if cur.left:
+                    q.append((cur.left, 2 * colIndex))
+                if cur.right:
+                    q.append((cur.right, 2 * colIndex + 1))
+            
+            maxWidth = max(maxWidth, colIndex - levelHeadIndex + 1)
+        
+        return maxWidth
+    
+        
+#         # DFS solution
+#         # T: O(n), S: O(n)
+#         if not root:
+#             return 0
+
+#         # save first visit of a node in a particular depth
+#         firstNodeDepth = {}
+        
+#         maxWidth = 0
+#         def dfs(node, depth, colIndex):
+#             nonlocal maxWidth
+#             if not node:
+#                 return
+            
+#             if depth not in firstNodeDepth:
+#                 firstNodeDepth[depth] = colIndex
+            
+#             maxWidth = max(maxWidth, colIndex - firstNodeDepth[depth] + 1)
+            
+#             dfs(node.left, depth + 1, 2 * colIndex)
+#             dfs(node.right, depth + 1, 2 * colIndex + 1)
+            
+#             return maxWidth
+        
+#         return dfs(root, 0, 0)
