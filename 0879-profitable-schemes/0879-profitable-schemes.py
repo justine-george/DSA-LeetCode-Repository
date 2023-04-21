82d2d2eac71404c5167967efb7cb2ca2df92b324
Justine George <justinegeo96@gmail.com>
2023-04-21 09:06:16 -0500

    Time: 3933 ms (25.71%), Space: 654 MB (8.57%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -7,28 +7,30 @@ class Solution:
         #              /     \
         # (n=3, p=2, i=1)    (n=5, p=0, i=1)
         #    chose i=0         didn't choose i=0
-        mod = 10**9 + 7
         
-        # T: O(n * m * p) -> m = length of group
-        # (i, m, p) -> number of ways we can generate profitable schemes with these params
-        dp = collections.defaultdict(int)
         
-        # setting base case
-        for m in range(n + 1):
-            dp[(len(group), m, minProfit)] = 1 # this is the case when i == len(group) in recursive solution
+#         # T: O(n * m * p) -> m = length of group
+#         # (i, m, p) -> number of ways we can generate profitable schemes with these params
+#         dp = collections.defaultdict(int)
+#         mod = 10**9 + 7
+#         # setting base case
+#         for m in range(n + 1):
+#             dp[(len(group), m, minProfit)] = 1 # this is the case when i == len(group) in recursive solution
         
-        for i in range(len(group) -1, -1, -1): # iterate in reverse
-            for m in range(n + 1): # include n too
-                for p in range(minProfit + 1): # include minProfit too
-                    dp[(i, m, p)] = dp[(i + 1, m, p)]
-                    if m + group[i] <= n:
-                        dp[(i, m, p)] += dp[(i + 1, m + group[i], min(minProfit, p + profit[i]))] % mod
+#         for i in range(len(group) -1, -1, -1): # iterate in reverse
+#             for m in range(n + 1): # include n too
+#                 for p in range(minProfit + 1): # include minProfit too
+#                     dp[(i, m, p)] = dp[(i + 1, m, p)]
+#                     if m + group[i] <= n:
+#                         dp[(i, m, p)] += dp[(i + 1, m + group[i], min(minProfit, p + profit[i]))] % mod
         
-        return dp[(0, 0, 0)] % mod
+#         return dp[(0, 0, 0)] % mod
+    
         
         
 #         # backtracking with memoization - but time limit exceeded error on leetcode
 #         # T: O(n * m * p) -> m = length of group
+#         mod = 10**9 + 7
 #         dp = {}
 #         def dfs(i, n, p):
 #             if i == len(group):
@@ -46,3 +48,22 @@ class Solution:
 #             return dp[(i, n, p)]
         
 #         return dfs(0, n, 0)
+
+
+        # with @cache
+        # T: O(n * m * p) -> m = length of group
+        mod = 10**9 + 7
+        @cache
+        def dfs(i, n, p):
+            if i == len(group):
+                return 1 if p >= minProfit else 0
+            # skip the i
+            skip = dfs(i + 1, n, p)
+            # or, pick i
+            pick = 0
+            if n - group[i] >= 0:
+                pick = dfs(i + 1, n - group[i], min(minProfit, p + profit[i]))
+            
+            return skip + pick
+        
+        return dfs(0, n, 0) % mod
