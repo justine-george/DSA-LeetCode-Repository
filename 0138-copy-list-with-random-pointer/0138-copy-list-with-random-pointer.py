9a026472145648229b8b0d13c16c898ffed8d4e6
Justine George <justinegeo96@gmail.com>
2023-04-21 15:01:56 -0500

    Time: 38 ms (58.73%), Space: 15.5 MB (6.10%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,40 +9,40 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # T: O(n). S: O(1) - keep cloned nodes right next to original
-        if not head:
-            return head
+#         # T: O(n), S: O(1) - keep cloned nodes right next to original
+#         if not head:
+#             return head
         
-        cur = head
-        while cur:
-            copy = Node(cur.val)
-            # insert new node right next to the original
-            # a,b,c => a,a',b,b',c,c'
-            copy.next = cur.next
-            cur.next = copy
-            cur = copy.next
+#         cur = head
+#         while cur:
+#             copy = Node(cur.val)
+#             # insert new node right next to the original
+#             # a,b,c => a,a',b,b',c,c'
+#             copy.next = cur.next
+#             cur.next = copy
+#             cur = copy.next
         
-        cur = head
-        while cur:
-            # point randoms
-            cur.next.random = cur.random.next if cur.random else None
-            cur = cur.next.next
+#         cur = head
+#         while cur:
+#             # point randoms
+#             cur.next.random = cur.random.next if cur.random else None
+#             cur = cur.next.next
         
-        # unwrap a,a',b,b',c,c' => a,b,c and a',b',c'
-        oldlist = head
-        newlist = head.next
-        headNew = head.next
-        while oldlist:
-            oldlist.next = oldlist.next.next
-            newlist.next = newlist.next.next if newlist.next else None
-            oldlist = oldlist.next
-            newlist = newlist.next
+#         # unwrap a,a',b,b',c,c' => a,b,c and a',b',c'
+#         oldlist = head
+#         newlist = head.next
+#         headNew = head.next
+#         while oldlist:
+#             oldlist.next = oldlist.next.next
+#             newlist.next = newlist.next.next if newlist.next else None
+#             oldlist = oldlist.next
+#             newlist = newlist.next
     
-        return headNew
+#         return headNew
     
     
     
-#         # T: O(n). S: O(n)
+#         # T: O(n), S: O(n)
 #         oldToCopy = {None: None}
         
 #         # first create the deep copies, we're not pointing yet
@@ -61,3 +61,26 @@ class Solution:
 #             cur = cur.next
         
 #         return oldToCopy[head]
+    
+    
+    
+        # T: O(n), S: O(n)
+        # recursion
+        oldToCopy = {}
+        
+        def dfs(node):
+            if node == None:
+                return None
+            
+            if node in oldToCopy:
+                return oldToCopy[node]
+            
+            newNode = Node(node.val)
+            oldToCopy[node] = newNode
+            
+            newNode.next = dfs(node.next)
+            newNode.random = dfs(node.random)
+            
+            return newNode
+        
+        return dfs(head)
