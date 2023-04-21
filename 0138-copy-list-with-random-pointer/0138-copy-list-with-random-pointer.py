10b0c1cf473ed6606fe035d987ab480830266123
Justine George <justinegeo96@gmail.com>
2023-04-21 15:24:24 -0500

    Time: 27 ms (98.26%), Space: 15 MB (14.87%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -24,7 +24,7 @@ class Solution:
         
         cur = head
         while cur:
-            # point randoms
+            # assign randoms for the new nodes
             cur.next.random = cur.random.next if cur.random else None
             cur = cur.next.next
         
