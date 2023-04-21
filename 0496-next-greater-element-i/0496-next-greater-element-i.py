07dc4731a0d6257e379e1267fa25d75c5d937db4
Justine George <justinegeo96@gmail.com>
2023-04-21 16:19:28 -0500

    Time: 43 ms (93.93%), Space: 14 MB (99.90%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,13 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # monotonic stack - keep it non increasing
-        st = [nums2[0]]
+        st = []
         
         map = {} # stores {el: next greatest to el} mapping
-        for i in range(1, len(nums2)):
-            while st and st[-1] < nums2[i]:
+        for num in nums2:
+            while st and st[-1] < num:
                 top = st.pop()
-                map[top] = nums2[i]
-            st.append(nums2[i])
+                map[top] = num
+            st.append(num)
             
-        return [map.get(n, -1) for n in nums1]
+        return [map.get(num, -1) for num in nums1]
