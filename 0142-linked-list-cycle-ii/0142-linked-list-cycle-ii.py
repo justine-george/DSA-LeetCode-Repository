7de390ad17130d94781fac72a821332ce6545a4e
Justine George <justinegeo96@gmail.com>
2023-04-22 01:04:23 -0500

    Time: 45 ms (92.45%), Space: 17.4 MB (49.93%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -9,17 +9,22 @@ class Solution:
         # floyd's cycle detection - find out start of the cycle in 2 phases
         # T: O(n), S: O(1)
         
-        # if there is no nodes or only one node, then no cycle
+        # If there are no nodes or only one node, then no cycle
         if head is None or head.next is None:
             return None
         
         # 1. find out the collision point
+        # Initialize slow and fast pointers to the head
         slow, fast = head, head
+        # Move slow pointer one step and fast pointer two steps at a time
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            
+            # If slow and fast pointers meet, a cycle is detected
             if slow == fast:
                 # 2. find out the start of cycle using a second slow pointer
+                # Move slow2 pointer from the head, while slow pointer continues from the meeting point
                 slow2 = head
                 while slow != slow2:
                     slow = slow.next
@@ -27,5 +32,5 @@ class Solution:
                 # When slow and slow2 pointers meet, it's the start of the cycle
                 return slow
         
-        # if no cycle is detected
+        # If no cycle detected, return None
         return None
