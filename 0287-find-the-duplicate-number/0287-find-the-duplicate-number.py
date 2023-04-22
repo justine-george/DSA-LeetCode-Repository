68646e922a408e7073a58d198cc8299c4ba8f8ce
Justine George <justinegeo96@gmail.com>
2023-04-22 00:39:52 -0500

    Time: 625 ms (69.54%), Space: 27.8 MB (86.43%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # https://www.youtube.com/watch?v=wjYnzkAhcNk
+        # convert to linkedlist cycle detection problem
+        # floyd's cycle detection - find out start of the cycle
+        
+        # 0 because it is never a part of the cycle
+        # numbers are in the range [1, n] inclusive
+        slow, fast = 0, 0
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]    
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        # now increment slow and slow2 until they intersect
+        # this will be the duplicate (start of the cycle)
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                break
+        
+        return slow
