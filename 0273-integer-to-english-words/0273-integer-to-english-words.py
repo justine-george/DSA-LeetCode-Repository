547421f66d7091216c9a226768225ba4126edf28
Justine George <justinegeo96@gmail.com>
2023-09-15 18:52:21 -0500

    Time: 36 ms (83.13%), Space: 16.6 MB (15.59%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -3,28 +3,41 @@ class Solution:
         single = [""] + "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen".split()
         tens = ["", ""] + "Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety".split()
         
-        def toWords(n):
+        def toString(n):
             if n == 0:
                 return ""
-            if n // (10**9) > 0:
-                return toWords(n // (10**9)) + "Billion " + toWords(n % (10**9))
-            if n // (10**6) > 0:
-                return toWords(n // (10**6)) + "Million " + toWords(n % (10**6))
-            if n // 1000 > 0:
-                return toWords(n // (10**3)) + "Thousand " + toWords(n % (10**3))
-            if n // 100 > 0:
-                return single[n // 100] + " " + "Hundred " + toWords(n % 100)
-            if n > 19 and (n // 10) > 0:
-                return tens[n // 10] + " " + toWords(n%10)
-            return single[n] + " "
+            if (n // 10**9):
+                return  toString(n // 10**9) + " Billion " + toString(n % 10**9)
+            if (n // 10**6):
+                return toString((n // 10**6)) + " Million " + toString(n % 10**6)
+            if (n // 10**3):
+                return toString((n // 10**3)) + " Thousand " + toString(n % 10**3)
+            
+            if (n // 100):
+                return (toString(n // 100) + " Hundred " + toString(n % 100)).strip()
+            
+            if n > 19:
+                return (tens[n // 10] + " " + toString(n % 10)).strip()
+            
+            if n > 0:
+                return single[n]
         
         if num == 0:
             return "Zero"
         
-        return toWords(num).strip()
-    
-
-    # 1     231231231
-    #   231     231231
-    #   2   31     231231
-    #   2   31     231  231
+        return toString(num).strip()
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                #231    231 231
