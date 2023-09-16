643323b574a3764ab3df85cfc51c4ca09f334ffb
Justine George <justinegeo96@gmail.com>
2023-09-15 21:13:34 -0500

    Time: 897 ms (86.79%), Space: 35.4 MB (76.10%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,27 +1,24 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        N = len(edges)
-        maxLength = -1
-        visited = set()
-        node_depth = [-1] * N
-        
-        def dfs(node, depth):
-            if node == -1:
-                return 0
+        used = set()
+        res = -1
+        for i in range(len(edges)):
+            # Use Floyd's algorithm to detect if a loop has been found, marking nodes as we go
+            slow = i
+            fast = edges[i]
+            while slow not in used and 0 <= fast and 0 <= edges[fast]:
+                used.add(slow)
+                slow = edges[slow]
+                fast = edges[edges[fast]]
+                if slow != fast:
+                    continue
 
-            if node in visited:
-                return depth - node_depth[node]
-            
-            if node_depth[node] != -1:
-                return 0
-            
-            visited.add(node)
-            node_depth[node] = depth
-            cycle_length = dfs(edges[node], depth + 1)
-            return cycle_length
-        
-        for i in range(N):
-            maxLength = max(maxLength, dfs(i, 0))
-            visited = set()
-            
-        return maxLength if maxLength > 0 else -1
+                # Found a loop, now count the length
+                size = 1
+                pos = edges[fast]
+                while pos != fast:
+                    size += 1
+                    pos = edges[pos]
+                res = max(res, size)
+                break
+        return res
