539cc31ee80e87c27cfed7741a9ae94ae93ec7d8
Justine George <justinegeo96@gmail.com>
2023-09-16 00:50:13 -0500

    Time: 842 ms (64.93%), Space: 31.3 MB (66.00%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -25,17 +25,10 @@ class Solution:
         # fill the map
         for sumVal, n in sumArray:
             insertNumIntoDict(sumVal, n)
-
-            
-        def getSumArray(arr):
-            sum = 0
-            for n in arr:
-                sum += n
-            return sum
         
         res = -1
         for key in map:
             if len(map[key]) == 2:
-                res = max(res, getSumArray(map[key]))
+                res = max(res, map[key][0] + map[key][1])
         
         return res
