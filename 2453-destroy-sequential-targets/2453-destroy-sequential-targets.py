0e028786384a52a661d0a141f88c6cf44ceea9dc
Justine George <justinegeo96@gmail.com>
2023-09-16 16:06:25 -0500

    Time: 560 ms (70.37%), Space: 35.7 MB (97.88%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.py
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.py
@@ -1,18 +1,13 @@
-from collections import defaultdict
-
 class Solution:
     def destroyTargets(self, nums: List[int], space: int) -> int:
-        mod_counts = defaultdict(int)
-        mod_smallest = defaultdict(lambda: float('inf'))
-
-        for num in nums:
-            mod = num % space
-            mod_counts[mod] += 1
-            mod_smallest[mod] = min(mod_smallest[mod], num)
-
-        # Find the modulus with maximum count
-        max_count = max(mod_counts.values())
-        max_count_mods = [mod for mod, count in mod_counts.items() if count == max_count]
-
-        # Among the moduli with maximum count, find the one associated with the smallest number
-        return min(mod_smallest[mod] for mod in max_count_mods)
+        nums.sort()
+        
+        modCounts = defaultdict(int)
+        for n in nums:
+            modCounts[n % space] += 1
+        
+        maxCount = max(modCounts.values())
+        
+        for n in nums:
+            if modCounts[n % space] == maxCount:
+                return n
