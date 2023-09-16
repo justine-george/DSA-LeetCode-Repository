2ff626ec2aa470a6140b02cf2f135f31b3b7bea6
Justine George <justinegeo96@gmail.com>
2023-09-15 21:00:50 -0500

    Time: 1185 ms (34.81%), Space: 168.9 MB (21.60%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -2,28 +2,22 @@ class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         N = len(edges)
         maxLength = -1
-        visited = set()    # To track nodes that are currently being visited in DFS
-        node_depth = [-1] * N  # To store the depth of each node
+        visited = set()
+        node_depth = [-1] * N
         
         def dfs(node, depth):
-            # If node is terminal or already processed in previous DFS
             if node == -1:
                 return 0
 
-            # If node is currently being visited, we detected a cycle
             if node in visited:
                 return depth - node_depth[node]
             
-            # If we've visited this node in a different DFS before
             if node_depth[node] != -1:
                 return 0
             
             visited.add(node)
             node_depth[node] = depth
-            
             cycle_length = dfs(edges[node], depth + 1)
-            
-            # visited.remove(node)
             return cycle_length
         
         for i in range(N):
