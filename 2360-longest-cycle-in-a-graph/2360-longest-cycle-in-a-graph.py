e0f1111927ef69e36c68b15e651e04d7d5cea49d
Justine George <justinegeo96@gmail.com>
2023-09-15 20:40:46 -0500

    Time: 1139 ms (42.98%), Space: 194.3 MB (8.18%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        N = len(edges)
+        maxLength = -1
+        visit = set()
+        depth = [float('inf')] * N
+        
+        def dfs(n, curDepth = 0):
+            if n in visit or n == -1:
+                return -1
+            
+            if depth[n] < curDepth:
+                return curDepth - depth[n]
+            
+            depth[n] = curDepth
+            val = dfs(edges[n], curDepth + 1)
+            visit.add(n)
+            return val
+            
+        
+        
+        for i in range(N):
+            maxLength = max(maxLength, dfs(i))
+        return maxLength
