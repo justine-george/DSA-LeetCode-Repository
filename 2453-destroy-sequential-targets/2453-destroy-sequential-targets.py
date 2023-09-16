67db97284a4752631237a2a064ba166335e2ee81
Justine George <justinegeo96@gmail.com>
2023-09-16 16:45:39 -0500

    Time: 545 ms (86.24%), Space: 35.7 MB (92.06%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.py
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.py
@@ -1,7 +1,5 @@
 class Solution:
     def destroyTargets(self, nums: List[int], space: int) -> int:
-        # nums.sort()
-        
         modMap = {}
         for n in nums:
             mod = n % space
