09ae77a401ce6a984f86dd218c2f58d17656c2d5
Justine George <justinegeo96@gmail.com>
2023-09-16 17:32:55 -0500

    Time: 52 ms (92.06%), Space: 16.7 MB (72.74%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.py
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.py
@@ -1,11 +1,8 @@
 class Solution:
-    def addBoldTag(self, s: str, words: List[str]) -> str:
-#         <b>a<b>a</b>a</b><b>b</b><b>b</b><b>b</b>
-        
-#         <b>aaabbb</b>
-        
+    def addBoldTag(self, s: str, words: List[str]) -> str:      
         isCharBold = [False] * len(s)
         
+        # mask all characters to be emboldened
         for word in words:
             start = s.find(word)
             length = len(word)
@@ -13,24 +10,20 @@ class Solution:
             while start != -1:
                 for i in range(start, start + length):
                     isCharBold[i] = True
-                
                 start = s.find(word, start + 1)
             
         res = ""
-        # isFirstBold = True
         isBolding = False
         for i, val in enumerate(isCharBold):
             if val:
                 if not isBolding:
                     isBolding = True
                     res += '<b>'
-                res += s[i]
             else:
                 if isBolding:
                     isBolding = False
                     res += '</b>'
-                res += s[i]
+            res += s[i]
         if isBolding:
             res += '</b>'
         return res
-                
