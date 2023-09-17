1da30f48f3bc57597f0c6a1dc1e527f330606a0c
Justine George <justinegeo96@gmail.com>
2023-09-17 03:39:24 -0500

    Time: 104 ms (71.32%), Space: 16.2 MB (91.30%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -1,21 +1,21 @@
 class Solution:
     def minimumMoves(self, grid: List[List[int]]) -> int:
         dist = lambda x,y: abs(x[0]-y[0])+ abs(x[1]-y[1])  
-        zeros, spare = [], []
+        zeros, extras = [], []
 
         for i,j in product(range(3),range(3)):
             stone = grid[i][j]
             if stone == 0: zeros.append((i,j))
-            if stone  > 1: spare.extend([(i,j)]*(stone-1))
+            if stone  > 1: extras.extend([(i,j)]*(stone-1))
 
-        return min((sum(map(dist, zeros, per))) for per in set(permutations(spare)))
+        return min((sum(map(dist, zeros, per))) for per in set(permutations(extras)))
         
 #         1 3 0
 #         1 0 0
 #         1 0 3
         
 #         zeros: [(0, 2), (1, 1), (1, 2), (2, 1)]
-#         spares: [(0, 1), (0, 1), (2, 2), (2, 2)]
+#         extras: [(0, 1), (0, 1), (2, 2), (2, 2)]
         
         # {
         #     ((2, 2), (0, 1), (0, 1),(2, 2)),
