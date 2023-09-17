3c6f1ad28fc3ccc08d69792581cbc325a5725422
Justine George <justinegeo96@gmail.com>
2023-09-17 02:39:09 -0500

    Time: 106 ms (71.01%), Space: 16.2 MB (91.30%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -6,8 +6,7 @@ class Solution:
         for i,j in product(range(3),range(3)):
 
             stone = grid[i][j]
-            if stone == 0: zeros.append((i,j))               # <-- 1a)
-            if stone  > 1: spare.extend([(i,j)]*(stone-1))   # <-- 1b)
+            if stone == 0: zeros.append((i,j))
+            if stone  > 1: spare.extend([(i,j)]*(stone-1))
 
-        return min((sum(map(dist, zeros, per))) for per in   # <-- 2) and 3) 
-                                 set(permutations(spare)))
+        return min((sum(map(dist, zeros, per))) for per in set(permutations(spare)))
