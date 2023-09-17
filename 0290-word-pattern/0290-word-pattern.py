9b37881be804d0f01c422890c61f9493be375bf0
Justine George <justinegeo96@gmail.com>
2023-09-17 04:12:21 -0500

    Time: 35 ms (77.41%), Space: 16.2 MB (70.83%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,23 +1,7 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split(" ")
-        
-        if len(pattern) != len(words):
-            return False
-        
-        mapCharToWord = {}
-        mapWordToChar = {}
-        for c, word in zip(pattern, words):
-            if c not in mapCharToWord:
-                mapCharToWord[c] = word
-            else:
-                if mapCharToWord[c] != word:
-                    return False
-            
-            if word not in mapWordToChar:
-                mapWordToChar[word] = c
-            else:
-                if mapWordToChar[word] != c:
-                    return False
-        
-        return True
+        s = s.split()
+
+        return (len(set(pattern)) ==
+                len(set(s)) ==
+                len(set(zip_longest(pattern,s))))
