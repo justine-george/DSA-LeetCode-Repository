910278fffec5dfaaa4d1c6d4d05797c3effe6a6c
Justine George <justinegeo96@gmail.com>
2023-09-17 17:42:26 -0500

    Time: 133 ms (97.95%), Space: 20.7 MB (47.53%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        dp = {} # map n to list of Full Binary Tree
+        
+        # return the list of Full binary trees with n nodes
+        def backtrack(n):
+            if n == 0:
+                return []
+            if n == 1:
+                return [TreeNode()]
+            if n in dp:
+                return dp[n]
+            
+            res = []
+            for l in range(n): # 0 to n - 1
+                r = n - 1 - l
+                leftTrees, rightTrees = backtrack(l), backtrack(r)
+                
+                for t1 in leftTrees:
+                    for t2 in rightTrees:
+                        res.append(TreeNode(0, t1, t2))
+            
+            dp[n] = res
+            return res
+        
+        return backtrack(n)
