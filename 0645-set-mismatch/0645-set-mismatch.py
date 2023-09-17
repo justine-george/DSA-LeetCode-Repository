c4b87701f99cf7def6925bfe0c0f41f762cfc5a6
Justine George <justinegeo96@gmail.com>
2023-09-17 04:27:04 -0500

    Time: 167 ms (88.07%), Space: 18.1 MB (45.24%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -6,7 +6,6 @@ class Solution:
 #         uniqueSum = 7
         
         n, duplicateSum, uniqueSum = len(nums), sum(nums), sum(set(nums))
-        
         seriesSum = (n * (n + 1)) // 2
         
         return [duplicateSum - uniqueSum, seriesSum - uniqueSum]
