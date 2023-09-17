e49fb163b66d28f794a5235ecae8eeb6299ead70
Justine George <justinegeo96@gmail.com>
2023-09-17 04:24:33 -0500

    Time: 168 ms (86.90%), Space: 18.3 MB (32.97%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,12 +1,9 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
 #         1 2 2 4
-        
-#         10
-        
-#         9
-        
-#         7
+#         seriesSum = 10
+#         duplicateSum = 9
+#         uniqueSum = 7
         
         n, duplicateSum, uniqueSum = len(nums), sum(nums), sum(set(nums))
         
