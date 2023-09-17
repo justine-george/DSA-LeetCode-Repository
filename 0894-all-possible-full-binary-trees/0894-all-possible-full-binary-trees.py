a9480fe907c92aa88395ec10df81eea2525b2160
Justine George <justinegeo96@gmail.com>
2023-09-17 18:10:13 -0500

    Time: 124 ms (99.74%), Space: 20.1 MB (64.52%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
@@ -6,20 +6,26 @@
 #         self.right = right
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        dp = {0: [], 1: [TreeNode()]} # map n to list of Full Binary Tree
+        dp = {}
         
-        # return the list of Full binary trees with n nodes
         def generateFBT(n):
-            if n % 2 == 0:
-                return []
             if n in dp:
                 return dp[n]
             
+            if n % 2 == 0:
+                return []
+            
+            if n == 0:
+                return []
+            
+            if n == 1:
+                return [TreeNode()]
+            
             res = []
-            for l in range(1, n, 2): # 1 to n - 1, step 2 since even n means no FBT
+            for l in range(1, n, 2):
                 r = n - 1 - l
+                
                 leftTrees, rightTrees = generateFBT(l), generateFBT(r)
-
                 for t1, t2 in product(leftTrees, rightTrees):
                     res.append(TreeNode(0, t1, t2))
             
