f02aa5e376e7ea8178d056fe102469f23fcd0fe7
Justine George <justinegeo96@gmail.com>
2023-09-17 20:52:23 -0500

    Time: 836 ms (37.82%), Space: 16.9 MB (24.94%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -7,9 +7,28 @@ class Solution:
             stone = grid[i][j]
             if stone == 0: zeros.append((i,j))
             if stone  > 1: extras.extend([(i,j)]*(stone-1))
+                
+        print(self.getPermutations(extras))
 
-        return min((sum(map(dist, zeros, per))) for per in set(permutations(extras)))
+        return min((sum(map(dist, zeros, per))) for per in self.getPermutations(extras))
+    
+    def getPermutations(self, list):
+        # backtrack
+        res = set()
+        def permute(list, l, r):
+            if l == r:
+                res.add(tuple(list))
+            else:
+                for i in range(l, r):
+                    list[l], list[i] = list[i], list[l]
+                    permute(list, l + 1, r)
+                    # undo
+                    list[l], list[i] = list[i], list[l]
         
+        permute(list, 0, len(list))
+        return res
+        
+    
 #         1 3 0
 #         1 0 0
 #         1 0 3
