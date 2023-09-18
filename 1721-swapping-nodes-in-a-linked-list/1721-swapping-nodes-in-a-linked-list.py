7445a6a787414b81bd504f844b5c9600884757e1
Justine George <justinegeo96@gmail.com>
2023-09-18 12:13:11 -0500

    Time: 706 ms (66.64%), Space: 50.6 MB (80.23%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -5,27 +5,46 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        dummy.next = head
+#         dummy = ListNode()
+#         dummy.next = head
         
-        l, r = dummy, head
-        while k > 1:
+#         l, r = dummy, head
+#         while k > 1:
+#             r = r.next
+#             k -= 1
+        
+#         # r is the kth node from start
+#         kstart = r
+        
+#         while r:
+#             l = l.next
+#             r = r.next
+        
+#         # l is the kth node from end
+#         kend = l
+        
+#         # swap
+#         kstart.val, kend.val = kend.val, kstart.val
+        
+#         return dummy.next
+    
+        cur = head
+        for i in range(k - 1):
+            cur = cur.next
+        
+        # kth node from start
+        l = cur
+        
+        r = head
+        while cur.next:
+            cur = cur.next
             r = r.next
-            k -= 1
-        
-        # r is the kth node from start
-        kstart = r
-        
-        while r:
-            l = l.next
-            r = r.next
-        
-        # l is the kth node from end
-        kend = l
-        
+            
+        # r is the kth node from end
         # swap
-        tmp = kstart.val
-        kstart.val = kend.val
-        kend.val = tmp
+        l.val, r.val = r.val, l.val
         
-        return dummy.next
+        return head
+        
+        
+        
