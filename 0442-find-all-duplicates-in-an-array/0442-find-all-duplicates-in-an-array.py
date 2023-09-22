7faca1687f04bc221e6099cf64f73f2546345be0
Justine George <justinegeo96@gmail.com>
2023-09-21 22:16:52 -0500

    Time: 311 ms (43.68%), Space: 24.1 MB (82.19%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -7,12 +7,7 @@ class Solution:
             curNum = nums[i -1]
             if nums[abs(curNum) - 1] < 0:
                 res.append(abs(curNum))
-            else:
-                nums[abs(curNum) - 1] *= -1
-        
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                nums[i] *= -1
+            nums[abs(curNum) - 1] *= -1
         
         return res
     
