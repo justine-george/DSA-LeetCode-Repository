5175028e138ef62a335ca4d2f6a745cabc8748f2
Justine George <justinegeo96@gmail.com>
2023-09-22 00:44:36 -0500

    Time: 281 ms (83.50%), Space: 26.5 MB (53.70%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,21 +1,26 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        # Initialization
-        res = []
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         
-        # Cyclic sort
-        i = 0
-        while i < len(nums):
-            correctPos = nums[i] - 1
-            if nums[i] != nums[correctPos]:
-                nums[i], nums[correctPos] = nums[correctPos], nums[i]
-            else:
-                i += 1
+        # Hash table for keeping track of the numbers in the array
+        # Note that we can also use a set here since we are not 
+        # really concerned with the frequency of numbers.
+        hash_table = {}
+        
+        # Add each of the numbers to the hash table
+        for num in nums:
+            hash_table[num] = 1
+        
+        # Response array that would contain the missing numbers
+        result = []    
+        
+        # Iterate over the numbers from 1 to N and add all those
+        # that don't appear in the hash table. 
+        for num in range(1, len(nums) + 1):
+            if num not in hash_table:
+                result.append(num)
                 
-        # Go through every element
-        # If nums[i] != i + 1, (i + 1) is the disappeared num
-        for i in range(len(nums)):
-            if nums[i] != i + 1:
-                res.append(i + 1)
-        
-        return res
+        return result        
