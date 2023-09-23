2744bbb45cfbb9908b99bb67765fb90ad4f2bcaf
Justine George <justinegeo96@gmail.com>
2023-09-23 01:26:06 -0500

    Time: 520 ms (88.87%), Space: 30.7 MB (20.78%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,20 +1,11 @@
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        # to avoid boundary check for m-1 and m + 1
-        l = 1
-        r = len(arr) - 2
+        def peakIndexInMountainArray(self, arr: list[int]) -> int:
         
-        while l <= r:
-            m = (l + r) // 2
-            
-            # if peak is found
-            if arr[m] > arr[m - 1] and arr[m] > arr[m + 1]:
-                return m
-            
-            # ascending part
-            if arr[m] <= arr[m + 1]:
-                l = m + 1
-            else:
-                r = m - 1
-        
-        return -1
+            l, r = 0, len(arr)-1 
+
+            while l <= r:
+                m = (l+r)//2
+
+                if arr[m+1] < arr[m] and arr[m-1] < arr[m]: return m
+                if arr[m+1] > arr[m]                      : l = m+1
+                else                                      : r = m
