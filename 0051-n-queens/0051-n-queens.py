c06e1238b9433bda047f44dbb3739ac2eae13baf
Justine George <justinegeo96@gmail.com>
2023-09-23 23:39:43 -0500

    Time: 58 ms (61.55%), Space: 16.7 MB (72.30%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,37 +1,39 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # keep track of these sets
         col = set()
-        posDiag = set()
-        negDiag = set()
-        
+        posDiag = set() # (r + c)
+        negDiag = set() # (r - c)
+
         res = []
         board = [["."] * n for i in range(n)]
-        
+
         def backtrack(r):
             if r == n:
                 copy = ["".join(row) for row in board]
                 res.append(copy)
                 return
-            
-            # go through every column
+
+            # try every single position on the current row
             for c in range(n):
                 if c in col or (r + c) in posDiag or (r - c) in negDiag:
                     continue
-                
+
+                # add to the sets and the board
                 col.add(c)
                 posDiag.add(r + c)
                 negDiag.add(r - c)
-                board[r][c] = "Q"
-                
+                board[r][c] = 'Q'
+
+                # now check next row with this updated board
                 backtrack(r + 1)
-                
+
+                # undo the changes
                 col.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
-                board[r][c] = "."
-                
-        
+                board[r][c] = '.'
+
+        # start from row 0
         backtrack(0)
-        
+
         return res
