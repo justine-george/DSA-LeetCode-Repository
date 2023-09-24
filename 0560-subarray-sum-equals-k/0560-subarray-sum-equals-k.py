425c5027a9cbf64b1bd7b0b5fe8c8a143619f126
Justine George <justinegeo96@gmail.com>
2023-09-24 17:53:13 -0500

    Time: 252 ms (77.16%), Space: 18.8 MB (82.66%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,12 +3,11 @@ class Solution:
         res = 0
         cur_sum = 0
         
-        prefix_sum_count = defaultdict(int)
-        prefix_sum_count[0] = 1
+        prefix_sum_count_map = {0: 1}
         
         for num in nums:
             cur_sum += num
-            res += prefix_sum_count[cur_sum - k]
-            prefix_sum_count[cur_sum] += 1
+            res += prefix_sum_count_map.get(cur_sum - k, 0)
+            prefix_sum_count_map[cur_sum] = 1 + prefix_sum_count_map.get(cur_sum, 0)
         
         return res
