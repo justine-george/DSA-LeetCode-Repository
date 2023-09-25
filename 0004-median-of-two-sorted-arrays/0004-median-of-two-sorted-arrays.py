d6b95d41da78d5c4320e5c9670e15b9c27212468
Justine George <justinegeo96@gmail.com>
2023-09-25 03:41:15 -0500

    Time: 90 ms (59.34%), Space: 16.5 MB (58.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,42 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """
+        Time complexity:
+            O(log(min(A, B)))
+        Reason: We only partition the smaller list
+        
+        Space complexity:
+            O(1)        
+        """
         A, B = nums1, nums2
         total = len(A) + len(B)
         half = total // 2
         
         if len(A) > len(B):
             A, B = B, A
-            
+        
         l, r = 0, len(A) - 1
         while True:
-            i = (l + r) // 2
-            j = half - i - 2
+            i = (l + r) // 2 # for A, the smaller list
+            j = half - i - 2 # for B
             
             a_left = A[i] if i >= 0 else float('-inf')
             a_right = A[i + 1] if (i + 1) < len(A) else float('inf')
             b_left = B[j] if j >= 0 else float('-inf')
             b_right = B[j + 1] if (j + 1) < len(B) else float('inf')
             
-            # correct partition
+            # if partition is right
             if a_left <= b_right and b_left <= a_right:
-                # odd
+                # odd number of elements in total
                 if total % 2:
                     return min(a_right, b_right)
-                # even
+                # even number of elements in total
                 else:
                     return (max(a_left, b_left) + min(a_right, b_right)) / 2
-                
-            # if a partition is too large
+            
+            # a partition is too big, reduce it by half
             if a_left > b_right:
-                r = i -1
+                r = i - 1
+            # a partition is not enough, 
             else:
                 l = i + 1
