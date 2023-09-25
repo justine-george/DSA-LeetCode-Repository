9376aa6526e34b213288e17cf25240871936e105
Justine George <justinegeo96@gmail.com>
2023-09-25 18:39:53 -0500

    Time: 121 ms (67.02%), Space: 16.3 MB (77.33%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -4,27 +4,29 @@ class Solution:
             return False
         nums.sort(reverse=True)
         
-        targetSum = sum(nums) / k
-        if nums[0] > targetSum:
+        target = sum(nums) / k
+        if nums[0] > target:
             return False
         
         used = [False] * len(nums)
         
-        def backtrack(index, k, subsetSum):
+        def backtrack(i, k, subset_sum):
             if k == 0:
                 return True
-            if subsetSum == targetSum:
+            
+            if subset_sum == target:
                 return backtrack(0, k - 1, 0)
             
-            for j in range(index, len(nums)):
-                if used[j] or subsetSum + nums[j] > targetSum or (j > 0 and nums[j] == nums[j - 1] and not used[j - 1]):
+            for j in range(i, len(nums)):
+                if used[j] or subset_sum + nums[j] > target or (
+                    j > 0 and not used[j - 1] and nums[j] == nums[j -1]):
                     continue
                 
                 used[j] = True
-                if backtrack(j + 1, k, subsetSum + nums[j]):
+                if backtrack(j + 1, k, subset_sum + nums[j]):
                     return True
                 used[j] = False
-            
+                
             return False
-        
+            
         return backtrack(0, k, 0)
