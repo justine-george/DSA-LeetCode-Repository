79cc978d5601b4d191dd5fbca019e66a73e9894f
Justine George <justinegeo96@gmail.com>
2023-09-25 18:32:56 -0500

    Update 0698-partition-to-k-equal-sum-subsets.py

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -10,21 +10,21 @@ class Solution:
         
         used = [False] * len(nums)
         
-        def isSquarePossible(index, k, subsetSum):
+        def backtrack(index, k, subsetSum):
             if k == 0:
                 return True
             if subsetSum == targetSum:
-                return isSquarePossible(0, k - 1, 0)
+                return backtrack(0, k - 1, 0)
             
             for j in range(index, len(nums)):
                 if used[j] or subsetSum + nums[j] > targetSum or (j > 0 and nums[j] == nums[j - 1] and not used[j - 1]):
                     continue
                 
                 used[j] = True
-                if isSquarePossible(j, k, subsetSum + nums[j]):
+                if backtrack(j + 1, k, subsetSum + nums[j]):
                     return True
                 used[j] = False
             
             return False
         
-        return isSquarePossible(0, k, 0)
+        return backtrack(0, k, 0)
