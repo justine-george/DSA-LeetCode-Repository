b66ad9339551bfb0cbe6a298c5600af891b2598c
Justine George <justinegeo96@gmail.com>
2023-09-26 15:46:48 -0500

    Time: 100 ms (51.16%), Space: 19.3 MB (34.74%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # adjacency list
+        pre_map = {i: [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            pre_map[crs].append(pre)
+
+        # visit set = all courses along the curr DFS path
+        visit_set = set()
+
+        def dfs(crs):
+            if crs in visit_set:
+                return False
+            
+            if pre_map[crs] == []:
+                return True
+            
+            visit_set.add(crs)
+
+            for pre in pre_map[crs]:
+                if not dfs(pre):
+                    return False
+
+            visit_set.remove(crs)
+            pre_map[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        
+        return True
