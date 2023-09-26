ba6d9860df3ee91e7b864517d9bcf0c92afc0bcf
Justine George <justinegeo96@gmail.com>
2023-09-26 17:57:16 -0500

    Time: 110 ms (15.45%), Space: 17.7 MB (94.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,5 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def dfs(crs):
-            if crs in visited:
-                return False
-            
-            if map[crs] == []:
-                return True
-            
-            visited.add(crs)
-            for pre in map[crs]:
-                if not dfs(pre):
-                    return False
-            visited.remove(crs)
-            map[crs] = []
-            return True
-        
-        map = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            map[crs].append(pre)
-
-        visited = set()
-
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return False
-        
-        return True
-
         # different approach, kahn's algorithm
         # [[1,4],[2,4],[3,1],[3,2]]
 
@@ -41,3 +14,54 @@ class Solution:
         # visited = 5
         
         # node = 3
+
+        indegree = [0] * numCourses
+        # pre: course mapping
+        map = [[] for i in range(numCourses)]
+        for c, pre in prerequisites:
+            map[pre].append(c)
+            indegree[c] += 1
+
+        queue = deque()
+        for i, c in enumerate(indegree):
+            if c == 0:
+                queue.append(i)
+
+        visitedCount = 0
+        while queue:
+            crs = queue.popleft()
+            visitedCount += 1
+            
+            for c in map[crs]:
+                indegree[c] -= 1
+                if indegree[c] == 0:
+                    queue.append(c)
+        
+        return visitedCount == numCourses
+
+        # def dfs(crs):
+        #     if crs in visited:
+        #         return False
+            
+        #     if map[crs] == []:
+        #         return True
+            
+        #     visited.add(crs)
+        #     for pre in map[crs]:
+        #         if not dfs(pre):
+        #             return False
+        #     visited.remove(crs)
+        #     map[crs] = []
+        #     return True
+        
+        # map = {i: [] for i in range(numCourses)}
+        # for crs, pre in prerequisites:
+        #     map[crs].append(pre)
+
+        # visited = set()
+
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
+        
+        # return True
