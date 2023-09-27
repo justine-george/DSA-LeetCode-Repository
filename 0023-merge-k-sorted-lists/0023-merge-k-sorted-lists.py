626d3a56ac2a8607ab9661f0e1ccabf9d90bbffb
Justine George <justinegeo96@gmail.com>
2023-09-26 22:02:00 -0500

    Time: 94 ms (66.21%), Space: 19.7 MB (96.98%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,34 +7,38 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists or len(lists) == 0:
             return None
-        
+
         while len(lists) > 1:
             mergedList = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedList.append(self.mergeTwoLists(l1, l2))
-            lists = mergedList
-        
-        return lists[0]
-        
-        
-    def mergeTwoLists(self, l1, l2):
-        dummy = ListNode()
-        res = dummy
 
+            for i in range(0, len(lists), 2):
+                # l1 = lists[i]
+                # l2 = lists[i + 1] if i + 1 < len(lists) else None
+                # mergedList.append(self.mergeTwoList(l1, l2))
+                if i + 1 < len(lists):
+                    mergedList.append(self.mergeTwoList(lists[i], lists[i + 1]))
+                else:
+                    mergedList.append(self.mergeTwoList(lists[i], None))
+            lists = mergedList
+
+        return lists[0]
+    
+    def mergeTwoList(self, l1, l2):
+        res = ListNode()
+        cur = res
         while l1 and l2:
-            if l1.val < l2.val:
-                res.next = l1
+            if l1.val <= l2.val:
+                cur.next = l1
                 l1 = l1.next
             else:
-                res.next = l2
+                cur.next = l2
                 l2 = l2.next
-            res = res.next
+            cur = cur.next
 
         if l1:
-            res.next = l1
-        elif l2:
-            res.next = l2
-
-        return dummy.next
+            cur.next = l1
+        
+        if l2:
+            cur.next = l2
+        
+        return res.next
