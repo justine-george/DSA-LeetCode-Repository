9d12d940ab6d0827c893da9708501d621309d31d
Justine George <justinegeo96@gmail.com>
2023-09-27 04:54:24 -0500

    Time: 42 ms (29.45%), Space: 16.1 MB (98.28%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,35 +1,35 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        # # T: O(rowIndex^2), S: O(rowIndex)
-        # prev_row = [0] * (rowIndex + 1)
-        # row = [0] * (rowIndex + 1)
-        # for k in range(rowIndex + 1):
-        #     print(prev_row)
-        #     # set start and end as 1
-        #     row[0] = row[k] = 1
+        # T: O(rowIndex^2), S: O(rowIndex)
+        prev_row = [0] * (rowIndex + 1)
+        row = [0] * (rowIndex + 1)
+        for k in range(rowIndex + 1):
+            print(prev_row)
+            # set start and end as 1
+            row[0] = row[k] = 1
 
-        #     # update middle values
-        #     for i in range(1, k):
-        #         row[i] = prev_row[i - 1] + prev_row[i]
+            # update middle values
+            for i in range(1, k):
+                row[i] = prev_row[i - 1] + prev_row[i]
 
-        #     # row is the prev_row for next iteration
-        #     prev_row = row[::]
+            # row is the prev_row for next iteration
+            prev_row = row[::]
         
-        # # return prev_row
         # return prev_row
+        return prev_row
 # p
 # c    [1, 0, 0, 0]
 #     [1, 1, 0, 0]
 #     [1, 2, 1, 0]
 #     [1, 3, 3, 1]
 
-        # recursive solution, but takes O(n^2) space
-        row = [1] * (rowIndex + 1)
-        if rowIndex == 0:
-            return row
+        # # recursive solution, but takes O(n^2) space
+        # row = [1] * (rowIndex + 1)
+        # if rowIndex == 0:
+        #     return row
         
-        prev_row = self.getRow(rowIndex - 1)
-        for i in range(1, len(row) - 1):
-            row[i] = prev_row[i - 1] + prev_row[i]
+        # prev_row = self.getRow(rowIndex - 1)
+        # for i in range(1, len(row) - 1):
+        #     row[i] = prev_row[i - 1] + prev_row[i]
         
-        return row
+        # return row
