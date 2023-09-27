a0dc4c8972188b9baebc9d77024604f284294fd6
Justine George <justinegeo96@gmail.com>
2023-09-27 15:52:11 -0500

    Time: 73 ms (32.63%), Space: 20.1 MB (5.18%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -6,11 +6,8 @@ class Solution:
         def backtrack(r, c, sum):
             if (r, c) in dp:
                 return dp[(r, c)]
-            # last level
-            # if r == n - 1:
-            #     dp[(r, c)] = sum + triangle[r][c]
-            #     return dp[(r, c)]
-
+            
+            # base case: we fell out of the tree
             if r == n:
                 dp[(r, c)] = sum
                 return sum
