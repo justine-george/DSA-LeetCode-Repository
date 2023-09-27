3c920551306dddd1af18ccdf1da2e972d25ebab6
Justine George <justinegeo96@gmail.com>
2023-09-27 16:21:29 -0500

    Time: 63 ms (83.74%), Space: 17.3 MB (82.53%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,21 +1,36 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Use a n + 1 size single row, overriding values, best approach
         # bottom up dp with space optimization, T: O(n^2), S: O(n)
 
+        n = len(triangle)
         # initialize prev_row with the last row
-        prev_row = triangle[-1][:]
-        new_row = [0] * len(prev_row)
+        cur_row = [0] * (n + 1)
 
         # start filling from bottom row
-        for i in range(len(prev_row) - 2, -1, -1):
+        for i in range(n - 1, -1, -1):
             # for each row, from left to right
             for j in range(i + 1):
-                new_row[j] = triangle[i][j] + min(prev_row[j], prev_row[j + 1])
+                cur_row[j] = triangle[i][j] + min(cur_row[j], cur_row[j + 1])
 
-            # at the end, prev_row is the new new_row
-            prev_row = new_row[:]
+        return cur_row[0]
+        
+        # # bottom up dp with space optimization, T: O(n^2), S: O(n)
 
-        return prev_row[0]
+        # # initialize prev_row with the last row
+        # prev_row = triangle[-1][:]
+        # new_row = [0] * len(prev_row)
+
+        # # start filling from bottom row
+        # for i in range(len(prev_row) - 2, -1, -1):
+        #     # for each row, from left to right
+        #     for j in range(i + 1):
+        #         new_row[j] = triangle[i][j] + min(prev_row[j], prev_row[j + 1])
+
+        #     # at the end, prev_row is the new new_row
+        #     prev_row = new_row[:]
+
+        # return prev_row[0]
 
         # # top down recursion with memoization, T: O(n^2), T: O(n^2)
         # n = len(triangle)
