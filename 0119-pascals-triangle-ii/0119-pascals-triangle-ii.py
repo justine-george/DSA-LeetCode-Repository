119f65cf0b9a1471aa26694086fea6f4f05b8c6f
Justine George <justinegeo96@gmail.com>
2023-09-27 04:46:04 -0500

    Time: 43 ms (22.67%), Space: 16.3 MB (27.07%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,19 +1,24 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        res = []
+        prev_row = [0] * (rowIndex + 1)
+        row = [0] * (rowIndex + 1)
         for k in range(rowIndex + 1):
-            # length of row is rowIndex + 1
-            row = [1] * (k + 1)
-
+            print(prev_row)
             # set start and end as 1
-            row[0] = row[-1] = 1
+            row[0] = row[k] = 1
 
             # update middle values
             for i in range(1, k):
-                row[i] = res[-1][i - 1] + res[-1][i]
-            
-            # add this row to the result
-            res.append(row)
+                row[i] = prev_row[i - 1] + prev_row[i]
+
+            # row is the prev_row for next iteration
+            prev_row = row[::]
         
-        # return the last row
-        return res[-1]
+        # return prev_row
+        return prev_row
+# p
+# c    [1, 0, 0, 0]
+#     [1, 1, 0, 0]
+#     [1, 2, 1, 0]
+#     [1, 3, 3, 1]
+
