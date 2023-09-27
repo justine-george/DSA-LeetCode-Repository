027c43aeaa4f36998d37d7fd949d129b12724df2
Justine George <justinegeo96@gmail.com>
2023-09-27 18:07:31 -0500

    Time: 43 ms (64.75%), Space: 16.3 MB (79.79%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,12 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+        # T: O(n * m), since k has a limit of 20. If no limit, T: O(n * m * k)
+        # S: O(n)
+        # bottom up dp
+        dp = [False] * (len(s) + 1) # extra 1 for the base case 
+
+        # Trivial case: to match empty, take nothing from the dict! 
+        # If we reach the end, we were able to match the word with the dict.
+        dp[-1] = True
 
         for i in range(len(s) - 1, -1, -1):
-            for w in wordDict:
-                if s[i: i + len(w)] == w and dp[i + len(w)]:
+            # go through each dict word, try to find match
+            for word in wordDict:
+                if s[i: i + len(word)] == word and dp[i + len(word)]:
+                    # match found and the remaining part of s is found in the dict
                     dp[i] = True
                     break
-        
         return dp[0]
