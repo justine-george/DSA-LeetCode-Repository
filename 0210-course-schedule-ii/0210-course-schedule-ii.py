bdb4df4f79979f79ee9ee6dc70e48e4fc6e4f1dd
Justine George <justinegeo96@gmail.com>
2023-09-26 20:13:35 -0500

    Time: 99 ms (62.05%), Space: 17.8 MB (90.33%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,64 +1,64 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # using DFS
-        def dfs(crs):
-            if crs in current_path:
-                return False
+        # # using DFS
+        # def dfs(crs):
+        #     if crs in current_path:
+        #         return False
 
-            if crs in visited:
-                return True
+        #     if crs in visited:
+        #         return True
 
-            current_path.add(crs)
-            for pre in map[crs]:
-                if not dfs(pre):
-                    return False
-            current_path.remove(crs)
-            
-            visited.add(crs)
-            res.append(crs)
-            return True
+        #     current_path.add(crs)
+        #     for pre in map[crs]:
+        #         if not dfs(pre):
+        #             return False
+        #     current_path.remove(crs)
 
-        res = []
-
-        # keep track of current visited
-        current_path = set()
-
-        # keep track of ones completely traversed
-        visited = set()
-
-        # build an adjacency list
-        map = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            map[crs].append(pre)
-        
-        for c in range(numCourses):
-            if c not in visited and not dfs(c):
-                return []
-
-        return res
-
-        # # kahn's algorithm using adj_list, preq_count_list and a deque
-        # res = []
-        # depends_on_count = [0] * numCourses
-        # adj_list = [[] for i in range(numCourses)]
-        # for crs, pre in prerequisites:
-        #     adj_list[pre].append(crs)
-        #     depends_on_count[crs] += 1
-        
-        # queue = deque()
-        # for c in range(numCourses):
-        #     if depends_on_count[c] == 0:
-        #         queue.append(c)
-
-        # visited_count = 0
-        # while queue:
-        #     crs = queue.popleft()
-        #     visited_count += 1
+        #     visited.add(crs)
         #     res.append(crs)
+        #     return True
 
-        #     for dependent in adj_list[crs]:
-        #         depends_on_count[dependent] -= 1
-        #         if depends_on_count[dependent] == 0:
-        #             queue.append(dependent)
+        # res = []
+
+        # # keep track of current visited
+        # current_path = set()
+
+        # # keep track of ones completely traversed
+        # visited = set()
+
+        # # build an adjacency list
+        # map = {i: [] for i in range(numCourses)}
+        # for crs, pre in prerequisites:
+        #     map[crs].append(pre)
         
-        # return res if visited_count == numCourses else []
+        # for c in range(numCourses):
+        #     if c not in visited and not dfs(c):
+        #         return []
+
+        # return res
+
+        # kahn's algorithm using adj_list, preq_count_list and a deque
+        res = []
+        depends_on_count = [0] * numCourses
+        adj_list = [[] for i in range(numCourses)]
+        for crs, pre in prerequisites:
+            adj_list[pre].append(crs)
+            depends_on_count[crs] += 1
+        
+        queue = deque()
+        for c in range(numCourses):
+            if depends_on_count[c] == 0:
+                queue.append(c)
+
+        visited_count = 0
+        while queue:
+            crs = queue.popleft()
+            visited_count += 1
+            res.append(crs)
+
+            for dependent in adj_list[crs]:
+                depends_on_count[dependent] -= 1
+                if depends_on_count[dependent] == 0:
+                    queue.append(dependent)
+        
+        return res if visited_count == numCourses else []
