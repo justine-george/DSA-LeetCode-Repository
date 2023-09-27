aea0d2f498e5a89200aded2f706be73173ce52d7
Justine George <justinegeo96@gmail.com>
2023-09-27 01:31:55 -0500

    Time: 844 ms (46.29%), Space: 41.1 MB (60.42%) - LeetHub

---
+++ 0502-ipo/0502-ipo.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        # 2 heap pattern
+
+        # maxheap
+        maxProfit = [] # store affordable projects based on the capital value from the capital minheap
+
+        # min heap, based on capital value. [0] th value would be min capital pair
+        minCapital = [(capital, profit) for capital, profit in zip(capital, profits)]
+        heapq.heapify(minCapital)
+
+        # do k times
+        for i in range(k):
+            while minCapital and minCapital[0][0] <= w:
+                # populate affordable projects in maxProfit maxheap
+                min_c, p_for_c = heapq.heappop(minCapital)
+                heapq.heappush(maxProfit, -1 * p_for_c)
+            if not maxProfit:
+                break
+            w += (-1 * heapq.heappop(maxProfit))
+
+        return w
