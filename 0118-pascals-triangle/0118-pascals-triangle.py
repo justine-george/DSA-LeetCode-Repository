392c8f7ea52b0ba7bd0640eb62c7fff7686de8ef
Justine George <justinegeo96@gmail.com>
2023-09-27 02:33:56 -0500

    Time: 39 ms (66.61%), Space: 16.1 MB (99.36%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,18 +1,37 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
-        for k in range(1, numRows + 1):
-            if k == 1:
-                res.append([1])
-            elif k == 2:
-                res.append([1, 1])
-            else:
-                ans = [1]
-                for i in range(k - 2):
-                    ans.append(res[-1][i] + res[-1][i + 1])
-                ans.append(1)
-                res.append(ans)
+        for k in range(numRows):
+            # length is k + 1
+            row = [1] * (k + 1)
+
+            # beginning and end is always 1
+            row[0] = row[-1] = 1
+
+            # update values in between using the row above
+            # e.g for k = 2, ie. 3rd row, this loop should run 1 time
+            # index from 1 
+            for i in range(1, k):
+                row[i] = res[-1][i - 1] + res[-1][i]
+            
+            # add row to the result
+            res.append(row)
+
         return res
+
+        # res = []
+        # for k in range(1, numRows + 1):
+        #     if k == 1:
+        #         res.append([1])
+        #     elif k == 2:
+        #         res.append([1, 1])
+        #     else:
+        #         ans = [1]
+        #         for i in range(k - 2):
+        #             ans.append(res[-1][i] + res[-1][i + 1])
+        #         ans.append(1)
+        #         res.append(ans)
+        # return res
 
         # i = 1
         # [1]
