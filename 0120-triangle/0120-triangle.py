240eeed962132f0fcba7f8912aa4a27cf12bec29
Justine George <justinegeo96@gmail.com>
2023-09-27 15:51:02 -0500

    Time: 79 ms (12.41%), Space: 20 MB (5.18%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -7,9 +7,13 @@ class Solution:
             if (r, c) in dp:
                 return dp[(r, c)]
             # last level
-            if r == n - 1:
-                dp[(r, c)] = sum + triangle[r][c]
-                return dp[(r, c)]
+            # if r == n - 1:
+            #     dp[(r, c)] = sum + triangle[r][c]
+            #     return dp[(r, c)]
+
+            if r == n:
+                dp[(r, c)] = sum
+                return sum
 
             dp[(r, c)] = triangle[r][c] + min(backtrack(r + 1, c, sum), backtrack(r + 1, c + 1, sum))
             return dp[(r, c)]
