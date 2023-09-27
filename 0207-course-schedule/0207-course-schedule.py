1759a0d1a0d1cee4fc9e84cd9c3e2ce92406f085
Justine George <justinegeo96@gmail.com>
2023-09-26 21:10:01 -0500

    Time: 96 ms (72.88%), Space: 19.2 MB (34.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,67 +1,69 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # different approach, kahn's algorithm
-        # [[1,4],[2,4],[3,1],[3,2]]
-
-        # adj
-        # [[],[3],[3],[],[1, 2]]
-
-        # indegree
-        # [0,0,0,0,0]
-
-        # queue = []
-
-        # visited = 5
-        
-        # node = 3
-
-        indegree = [0] * numCourses
-        # pre: course mapping
-        map = [[] for _ in range(numCourses)]
-        for c, pre in prerequisites:
-            map[pre].append(c)
-            indegree[c] += 1
-
-        queue = deque()
-        for i, c in enumerate(indegree):
-            if c == 0:
-                queue.append(i)
-
-        visitedCount = 0
-        while queue:
-            crs = queue.popleft()
-            visitedCount += 1
+        # using DFS
+        def dfs(crs):
+            if crs in current_cycle:
+                return False
             
-            for c in map[crs]:
-                indegree[c] -= 1
-                if indegree[c] == 0:
-                    queue.append(c)
-        
-        return visitedCount == numCourses
-
-        # def dfs(crs):
-        #     if crs in visited:
-        #         return False
+            if crs in visited:
+                return True
             
-        #     if map[crs] == []:
-        #         return True
+            current_cycle.add(crs)
+            for pre in map[crs]:
+                if not dfs(pre):
+                    return False
+            current_cycle.remove(crs)
             
-        #     visited.add(crs)
-        #     for pre in map[crs]:
-        #         if not dfs(pre):
-        #             return False
-        #     visited.remove(crs)
-        #     map[crs] = []
-        #     return True
-        
-        # map = {i: [] for i in range(numCourses)}
-        # for crs, pre in prerequisites:
-        #     map[crs].append(pre)
+            visited.add(crs)
+            return True
 
-        # visited = set()
-
-        # for crs in range(numCourses):
-        #     if not dfs(crs):
-        #         return False
+        map = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            map[crs].append(pre)
         
-        # return True
+        visited, current_cycle = set(), set()
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+
+        return True
+        
+        # # different approach, kahn's algorithm
+        # # [[1,4],[2,4],[3,1],[3,2]]
+
+        # # adj
+        # # [[],[3],[3],[],[1, 2]]
+
+        # # indegree
+        # # [0,0,0,0,0]
+
+        # # queue = []
+
+        # # visited = 5
+        
+        # # node = 3
+
+        # indegree = [0] * numCourses
+        # # pre: course mapping
+        # map = [[] for _ in range(numCourses)]
+        # for c, pre in prerequisites:
+        #     map[pre].append(c)
+        #     indegree[c] += 1
+
+        # queue = deque()
+        # for i, c in enumerate(indegree):
+        #     if c == 0:
+        #         queue.append(i)
+
+        # visitedCount = 0
+        # while queue:
+        #     crs = queue.popleft()
+        #     visitedCount += 1
+            
+        #     for c in map[crs]:
+        #         indegree[c] -= 1
+        #         if indegree[c] == 0:
+        #             queue.append(c)
+        
+        # return visitedCount == numCourses
