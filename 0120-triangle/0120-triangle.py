f96fecf033f4692655f910784599e98c1e1eb07e
Justine George <justinegeo96@gmail.com>
2023-09-27 15:44:17 -0500

    Time: 77 ms (17.5%), Space: 19.8 MB (8.31%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+
+        dp = {}
+        def backtrack(r, c, sum):
+            if (r, c) in dp:
+                return dp[(r, c)]
+            # last level
+            if r == n - 1:
+                dp[(r, c)] = sum + triangle[r][c]
+                return dp[(r, c)]
+
+            dp[(r, c)] = triangle[r][c] + min(backtrack(r + 1, c, sum), backtrack(r + 1, c + 1, sum))
+            return dp[(r, c)]
+        
+        return backtrack(0, 0, 0)
