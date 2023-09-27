e6bd4ade93caa35520d772c9fa9f9209e88a7be6
Justine George <justinegeo96@gmail.com>
2023-09-27 17:15:31 -0500

    Time: 59 ms (99.16%), Space: 16.2 MB (92.92%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.py
+++ 0815-champagne-tower/0815-champagne-tower.py
@@ -1,17 +1,18 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        prev = [poured]
+        # Initialize the flow
+        prev_row = [poured]
 
-        # go through every row, starting from second row
-        for i in range(1, query_row + 1):
-            cur = [0] * (i + 1) # size 2 for the second row
+        for row in range(1, query_row + 1):
+            cur_row = [0] * (row + 1)
             
-            for j in range(i):
-                extra = prev[j] - 1 if prev[j] > 1 else 0
+            # for every parent item
+            for j in range(row):
+                extra = prev_row[j] - 1
+                if extra > 0:
+                    cur_row[j] += (0.5 * extra)
+                    cur_row[j + 1] += (0.5 * extra)
 
-                cur[j] += (0.5 * extra)
-                cur[j + 1] += (0.5 * extra)
-            
-            prev = cur
+            prev_row = cur_row
         
-        return min(1, prev[query_glass])
+        return min(1, prev_row[query_glass])
