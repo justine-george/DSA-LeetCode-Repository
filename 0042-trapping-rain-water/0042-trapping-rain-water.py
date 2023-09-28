cb35b9881a2048d90fb60c565d684860fe52d47e
Justine George <justinegeo96@gmail.com>
2023-09-28 02:00:58 -0500

    Time: 109 ms (75.53%), Space: 18.7 MB (18.5%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,39 +1,39 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # # 2 pointer, T: O(n), S: O(1)
-        # if not height:
-        #     return 0
+        # 2 pointer, T: O(n), S: O(1)
+        if not height:
+            return 0
         
-        # l, r = 0, len(height) - 1
-        # lMax, rMax = height[l], height[r]
-        # res = 0
-
-        # while l < r:
-        #     if lMax < rMax:
-        #         l += 1
-        #         lMax = max(lMax, height[l])
-        #         res += lMax - height[l]
-        #     else:
-        #         r -= 1
-        #         rMax = max(rMax, height[r])
-        #         res += rMax - height[r]
-        
-        # return res
-
-        # Using stack, T: O(n), S: O(n)
-        # store indeces
-        stack = []
+        l, r = 0, len(height) - 1
+        lMax, rMax = height[l], height[r]
         res = 0
-        for i in range(len(height)):
-            # If the current bar is shorter, we simply push its index to the stack.
-            # If it's taller, we need to calculate trapped water.
-            while stack and height[stack[-1]] < height[i]:
-                popped_index = stack.pop()
-                if not stack:
-                    break
-                distance = i - stack[-1] - 1
-                bounded_height = min(height[stack[-1]], height[i]) - height[popped_index]
-                res += (distance * bounded_height)
-            stack.append(i)
+
+        while l < r:
+            if lMax < rMax:
+                l += 1
+                lMax = max(lMax, height[l])
+                res += lMax - height[l]
+            else:
+                r -= 1
+                rMax = max(rMax, height[r])
+                res += rMax - height[r]
         
         return res
+
+        # # Using stack, T: O(n), S: O(n)
+        # # store indeces
+        # stack = []
+        # res = 0
+        # for i in range(len(height)):
+        #     # If the current bar is shorter, we simply push its index to the stack.
+        #     # If it's taller, we need to calculate trapped water.
+        #     while stack and height[stack[-1]] < height[i]:
+        #         popped_index = stack.pop()
+        #         if not stack:
+        #             break
+        #         distance = i - stack[-1] - 1
+        #         bounded_height = min(height[stack[-1]], height[i]) - height[popped_index]
+        #         res += (distance * bounded_height)
+        #     stack.append(i)
+        
+        # return res
