25b7cf39d0600c8d858b4bc7577299f416c6d15f
Justine George <justinegeo96@gmail.com>
2023-09-28 01:21:48 -0500

    Time: 109 ms (75.53%), Space: 18.6 MB (34.04%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,36 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        # # 2 pointer, T: O(n), S: O(1)
+        # if not height:
+        #     return 0
         
-        l, r = 0, len(height) - 1
-        lMax, rMax = height[l], height[r]
-        res = 0
+        # l, r = 0, len(height) - 1
+        # lMax, rMax = height[l], height[r]
+        # res = 0
 
-        while l < r:
-            if lMax < rMax:
-                l += 1
-                lMax = max(lMax, height[l])
-                res += lMax - height[l]
-            else:
-                r -= 1
-                rMax = max(rMax, height[r])
-                res += rMax - height[r]
+        # while l < r:
+        #     if lMax < rMax:
+        #         l += 1
+        #         lMax = max(lMax, height[l])
+        #         res += lMax - height[l]
+        #     else:
+        #         r -= 1
+        #         rMax = max(rMax, height[r])
+        #         res += rMax - height[r]
+        
+        # return res
+
+        # store indeces
+        stack = []
+        res = 0
+        for i in range(len(height)):
+            while stack and height[stack[-1]] < height[i]:
+                index = stack.pop()
+                if not stack:
+                    break
+                distance = i - stack[-1] - 1
+                bounded_height = min(height[stack[-1]], height[i]) - height[index]
+                res += (distance * bounded_height)
+            stack.append(i)
         
         return res
