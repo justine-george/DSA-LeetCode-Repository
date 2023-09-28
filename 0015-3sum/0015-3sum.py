7c9ecb2ef38201dac44df60650e3cc0bfa8cd4cd
Justine George <justinegeo96@gmail.com>
2023-09-28 16:42:38 -0500

    Time: 809 ms (89.51%), Space: 20.3 MB (81.14%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # two pointers on a sorted array
-        res = []
         nums.sort()
+        res = []
 
-        for i, n in enumerate(nums):
-            # since array is sorted, and total sum should be 0, first number cannot be positive
-            if n > 0:
+        for i in range(len(nums)):
+            if nums[i] > 0:
                 break
 
-            if i > 0 and n == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
+
             l, r = i + 1, len(nums) - 1
             while l < r:
-                three_sum = n + nums[l] + nums[r]
+                sum = nums[i] + nums[l] + nums[r]
 
-                if three_sum > 0:
+                if sum > 0:
                     r -= 1
-                elif three_sum < 0:
+                elif sum < 0:
                     l += 1
                 else:
-                    res.append([n, nums[l], nums[r]])
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-                
         
         return res
