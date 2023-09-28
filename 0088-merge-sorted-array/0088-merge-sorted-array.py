0fab5914db658203e471d2663a1212ea4c965162
Justine George <justinegeo96@gmail.com>
2023-09-27 22:38:46 -0500

    Time: 47 ms (31.4%), Space: 16 MB (99.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,29 +4,46 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         # iterating backwards using 3 pointers
-        # i, l, r = len(nums1) - 1, m - 1, n 
+        # T: O(n + m), Space efficient - O(1)
+        i, l, r = len(nums1) - 1, m - 1, n - 1
 
-
-        
-        # iterating forward using 3 pointers
-        # T: O(n + m), S: O(m)
-        i, l, r = 0, 0, 0
-        nums1copy = nums1[:m]
-        while l < m and r < n:
-            if nums1copy[l] <= nums2[r]:
-                nums1[i] = nums1copy[l]
-                l += 1
+        while l >= 0 and r >= 0:
+            if nums1[l] > nums2[r]:
+                nums1[i] = nums1[l]
+                l -= 1
             else:
                 nums1[i] = nums2[r]
-                r += 1
-            i += 1
+                r -= 1
+            i -= 1
         
-        while l < m:
-            nums1[i] = nums1copy[l]
-            i += 1
-            l += 1
-        
-        while r < n:
+        while l >= 0:
+            nums1[i] = nums1[l]
+            l -= 1
+            i -= 1
+        while r >= 0:
             nums1[i] = nums2[r]
-            i += 1
-            r += 1
+            r -= 1
+            i -= 1
+        
+        # # iterating forward using 3 pointers
+        # # T: O(n + m), S: O(m)
+        # i, l, r = 0, 0, 0
+        # nums1copy = nums1[:m]
+        # while l < m and r < n:
+        #     if nums1copy[l] <= nums2[r]:
+        #         nums1[i] = nums1copy[l]
+        #         l += 1
+        #     else:
+        #         nums1[i] = nums2[r]
+        #         r += 1
+        #     i += 1
+        
+        # while l < m:
+        #     nums1[i] = nums1copy[l]
+        #     i += 1
+        #     l += 1
+        
+        # while r < n:
+        #     nums1[i] = nums2[r]
+        #     i += 1
+        #     r += 1
