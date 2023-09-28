c6e4bcb64b19ea54d0fbe59ba508a5c48a498aeb
Justine George <justinegeo96@gmail.com>
2023-09-28 16:26:25 -0500

    Time: 995 ms (71.71%), Space: 20.3 MB (91.99%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # T: O(n^2), S: O(1)
+        # two pointers on a sorted array
         res = []
         nums.sort()
-        
-        # for each element, take the sorted elements to the right and 
-        # use two pointer to find sum (two sum II) 
-        for i, val in enumerate(nums):
-            if i > 0 and val == nums[i - 1]: # skip the duplicates
+
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
                 continue
-    
-            l, r = i + 1, len(nums) - 1
             
+            l, r = i + 1, len(nums) - 1
             while l < r:
-                sum = val + nums[l] + nums[r]
-                
-                if sum == 0:
-                    res.append([val, nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l - 1]: # skip the duplicates
-                        l += 1
-                elif sum > 0:
+                three_sum = n + nums[l] + nums[r]
+
+                if three_sum > 0:
                     r -= 1
-                else:
+                elif three_sum < 0:
                     l += 1
+                else:
+                    res.append([n, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
         
         return res
