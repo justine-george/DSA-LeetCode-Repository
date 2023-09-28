967dc2fa5e74a71f677b1f6792544f9355380110
Justine George <justinegeo96@gmail.com>
2023-09-28 14:21:56 -0500

    Time: 109 ms (75.53%), Space: 18 MB (96.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # 2 pointer, T: O(n), S: O(1)
+        if not height:
+            return 0
+        
         l, r = 0, len(height) - 1
         lMax, rMax = height[l], height[r]
-        water_amount = 0
-        
+        res = 0
+
         while l < r:
-            # find out the bottleneck
+            # lMax is the bottleneck
             if lMax < rMax:
                 l += 1
                 lMax = max(lMax, height[l])
-                water_amount += (lMax - height[l])
+                res += lMax - height[l]
+            # rMax is the bottleneck
             else:
                 r -= 1
                 rMax = max(rMax, height[r])
-                water_amount += (rMax - height[r])
+                res += rMax - height[r]
         
-        return water_amount
-            
+        return res
+
+        # # Using stack, T: O(n), S: O(n)
+        # # store indeces
+        # stack = []
+        # res = 0
+        # for i in range(len(height)):
+        #     # If the current bar is shorter, we simply push its index to the stack.
+        #     # If it's taller, we need to calculate trapped water.
+        #     while stack and height[stack[-1]] < height[i]:
+        #         popped_index = stack.pop()
+        #         if not stack:
+        #             break
+        #         distance = i - stack[-1] - 1
+        #         bounded_height = min(height[stack[-1]], height[i]) - height[popped_index]
+        #         res += (distance * bounded_height)
+        #     stack.append(i)
+        
+        # return res
