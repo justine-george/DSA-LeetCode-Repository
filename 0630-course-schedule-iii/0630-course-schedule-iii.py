c42f98e39423b79e44d3a050dbb45bcab9dfc576
Justine George <justinegeo96@gmail.com>
2023-09-29 02:07:50 -0500

    Time: 630 ms (62.09%), Space: 21.8 MB (78.51%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -23,3 +23,24 @@ class Solution:
                 time_lapsed -= (-1 * heapq.heappop(heap))
         
         return len(heap)
+
+        # # T: O(n*d), S: O(n*d) - wont work here, memory exceeded
+        # # n - len(courses), d - max value of the time_lapsed
+        # memo = {}
+        # def backtrack(i, time_lapsed):
+        #     if i == len(courses):
+        #         return 0
+            
+        #     if (i, time_lapsed) in memo:
+        #         return memo[(i, time_lapsed)]
+            
+        #     taken_count = 0
+        #     if time_lapsed + courses[i][0] <= courses[i][1]:
+        #         taken_count = 1 + backtrack(i + 1, time_lapsed + courses[i][0])
+            
+        #     not_taken_count = backtrack(i + 1, time_lapsed)
+        #     memo[(i, time_lapsed)] = max(taken_count, not_taken_count)
+        #     return memo[(i, time_lapsed)]
+
+        # courses.sort(key = lambda x: x[1])
+        # return backtrack(0, 0)
