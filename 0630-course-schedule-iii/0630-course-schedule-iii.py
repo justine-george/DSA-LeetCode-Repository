72bc6b38479026ac29b967028f14352d76c0b8be
Justine George <justinegeo96@gmail.com>
2023-09-29 00:57:50 -0500

    Time: 648 ms (35.52%), Space: 22 MB (65.37%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,18 +1,26 @@
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        courses.sort(key = lambda x: x[1])
+        # T: O(nlogn), S: O(n)
+        # Approach: Greedily try to minimize time_lapsed by:
+            # 1. picking courses from a list sorted by lastDay 
+            # 2. add it to the maxheap of durations
+            # 3. if adding any course makes time_lapsed > its deadline, then:
+                #  pop from heap and reduce it from the time_lapsed
 
+        # sort by lastDay
+        courses.sort(key=lambda x:x[1])
+
+        # add to maxheap of times
         heap = []
+        time_lapsed = 0
 
-        timeSoFar = 0
+        for time, deadline in courses:
+            heapq.heappush(heap, -time)
+            time_lapsed += time
 
-        for duration, lastDay in courses:
-            # max heap
-            heapq.heappush(heap, -duration)
-            timeSoFar += duration
-
-            if timeSoFar > lastDay:
-                biggestDuration = -1 * heapq.heappop(heap)
-                timeSoFar -= biggestDuration
+            if time_lapsed > deadline:
+                biggest_time = -1 * heapq.heappop(heap)
+                # ensures we save time by removing the lengthiest course so far
+                time_lapsed -= biggest_time
         
         return len(heap)
