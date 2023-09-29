a827f83ba4ba52c2af52227fdffe158f11ffe69f
Justine George <justinegeo96@gmail.com>
2023-09-28 19:55:49 -0500

    Time: 96 ms (72.83%), Space: 19.1 MB (60.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,70 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # indegree list - depends on count
-        indegree = [0] * numCourses
-        # form the adjacency list, [x serves y]
+        def dfs(crs):
+            if crs in cycle_set:
+                return False
+            
+            if crs in visited_set:
+                return True
+
+            cycle_set.add(crs)
+            for pre in adj_list[crs]:
+                if not dfs(pre):
+                    return False
+            cycle_set.remove(crs)
+
+            visited_set.add(crs)
+            return True
+        
         adj_list = [[] for i in range(numCourses)]
         for crs, pre in prerequisites:
-            adj_list[pre].append(crs)
-            indegree[crs] += 1
+            adj_list[crs].append(pre)
         
-        queue = deque()
-        # add those with indegree 0 to the queue
+        # 2 sets
+        visited_set = set() 
+        cycle_set = set()
+
         for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+            if not dfs(i):
+                return False
+        
+        return True
 
-        res = []
-        while queue:
-            pre = queue.popleft()
-            res.append(pre)
-            for crs in adj_list[pre]:
-                indegree[crs] -= 1
-                if indegree[crs] == 0:
-                    queue.append(crs)
+        # # different approach, kahn's algorithm
+        # # [[1,4],[2,4],[3,1],[3,2]]
 
-        # if we can visit all, then all courses can be finished
-        return len(res) == numCourses
+        # # adj
+        # # [[],[3],[3],[],[1, 2]]
+
+        # # indegree
+        # # [0,0,0,0,0]
+
+        # # queue = []
+
+        # # visited = 5
+        
+        # # node = 3
+
+        # indegree = [0] * numCourses
+        # # pre: course mapping
+        # map = [[] for _ in range(numCourses)]
+        # for c, pre in prerequisites:
+        #     map[pre].append(c)
+        #     indegree[c] += 1
+
+        # queue = deque()
+        # for i, c in enumerate(indegree):
+        #     if c == 0:
+        #         queue.append(i)
+
+        # visitedCount = 0
+        # while queue:
+        #     crs = queue.popleft()
+        #     visitedCount += 1
+            
+        #     for c in map[crs]:
+        #         indegree[c] -= 1
+        #         if indegree[c] == 0:
+        #             queue.append(c)
+        
+        # return visitedCount == numCourses
