819d733c8367fe30e1159e972368b1e92e2ec8df
Justine George <justinegeo96@gmail.com>
2023-10-02 01:02:02 -0500

    Time: 213 ms (45.79%), Space: 17.3 MB (94.09%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,32 +1,18 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
         # figure out number of moves
-        a_moves = 0
-        b_moves = 0
-        isA = colors[0] == "A"
-        start = 0
-        i = 1
-        while i < len(colors):
-            if isA:
-                if colors[i] != colors[i - 1]:
-                    # calculate length of A
-                    if i - start >= 3:
-                        a_moves += (i - start) - 2
-                    isA = False
-                    start = i
-            else:
-                if colors[i] != colors[i - 1]:
-                    # calculate length of B
-                    if i - start >= 3:
-                        b_moves += (i - start) - 2
-                    isA = True
-                    start = i
-            i += 1
-
-        if i - start >= 3:
-            if isA:
-                a_moves += (i - start) - 2
-            else:
-                b_moves += (i - start) - 2
+        a_moves, b_moves = 0, 0
+        l = 0
+        for r in range(len(colors)):
+            if colors[l] != colors[r]:
+                l = r
+            
+            extra = r - l + 1 - 2
+            if extra > 0:
+                if colors[l] == 'A':
+                    a_moves += 1
+                else:
+                    b_moves += 1
         
+        # alice wins if a_moves > b_moves
         return a_moves > b_moves
