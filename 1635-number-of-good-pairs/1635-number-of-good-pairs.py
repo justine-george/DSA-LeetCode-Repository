d713ca00f87acfb41aea0a9164c08106b969cefa
Justine George <justinegeo96@gmail.com>
2023-10-02 19:45:22 -0500

    Time: 34 ms (91.11%), Space: 16.3 MB (18.82%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,24 +1,24 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        map = {}
-        res = 0
-        for i in range(len(nums)):
-            if nums[i] in map:
-                res += len(map[nums[i]])
-                map[nums[i]].append(i)
-            else:
-                map[nums[i]] = [i]
+        # map = {}
+        # res = 0
+        # for i in range(len(nums)):
+        #     if nums[i] in map:
+        #         res += len(map[nums[i]])
+        #         map[nums[i]].append(i)
+        #     else:
+        #         map[nums[i]] = [i]
         
+        # return res
+
+        # If a number appears n times, then n * (n – 1) // 2 good pairs can be made with this number.
+
+        count_map = {}
+        for i, n in enumerate(nums):
+            count_map[n] = count_map.get(n, 0) + 1
+
+        res = 0
+        for n, count in count_map.items():
+            res += (count * (count - 1) // 2)
+
         return res
-
-        # [1,2,3,1,1,3]
-        #  0 1 2 3 4 5
-
-        # map = {
-        #     1: [0, 3, 4]
-        #     2: [1]
-        #     3: [2]
-
-        # }
-
-        # res = 1 + 2 + 1
