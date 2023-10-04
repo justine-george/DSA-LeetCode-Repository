5677b7fd55ad019f19dcdf38c005cc9a61e197ff
Justine George <justinegeo96@gmail.com>
2023-10-03 23:15:42 -0500

    Time: 582 ms (81.62%), Space: 29.4 MB (31.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,9 +4,12 @@ class Solution:
         l, r = 0, n - 1
         max_water = 0
         while l < r:
-            max_water = max(max_water, (r - l) * min(height[l], height[r]))
+
+            # max_water = max(max_water, (r - l) * min(height[l], height[r]))
             if height[l] < height[r]:
+                max_water = max(max_water, (r - l) * height[l])
                 l += 1
             else:
+                max_water = max(max_water, (r - l) * height[r])
                 r -= 1
         return max_water
