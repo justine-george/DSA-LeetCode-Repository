799d35a88ee8517aa10ed2e220d7a6b1a8b14fc7
Justine George <justinegeo96@gmail.com>
2023-10-05 21:14:52 -0500

    Time: 47 ms (17.49%), Space: 16.1 MB (97.72%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,16 +1,31 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
+        # Bottom up dp
+        # # T: O(n^2), S: O(n)
+        # dp value should not be smaller than the n value (except for the last value, n)
+        # n  1 2 3 4
+        # dp 1 2 3 4
+
         dp = {1: 1}
-        def dfs(num):
-            if num in dp:
-                return dp[num]
-            
-            # for the original number n, it's mandatory to break it into smaller integers
-            res = 0 if num == n else num
+        for num in range(2, n + 1):
+            dp[num] = 0 if num == n else num
             for i in range(1, num):
-                res = max(res, dfs(i) * dfs(num - i))
-            
-            dp[num] = res
-            return res
+                dp[num] = max(dp[num], dp[i] * dp[num - i])
         
-        return dfs(n)
+        return dp[n]
+
+
+        # # T: O(n^2), S: O(n)
+        # dp = {1: 1}
+        # def dfs(num):
+        #     if num in dp:
+        #         return dp[num]
+            
+        #     # for the original number n, it's mandatory to break it into smaller integers
+        #     dp[num] = 0 if num == n else num
+        #     for i in range(1, num):
+        #         dp[num] = max(dp[num], dfs(i) * dfs(num - i))
+            
+        #     return dp[num]
+        
+        # return dfs(n)
