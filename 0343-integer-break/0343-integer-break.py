ed558a9a9ac62fd7567744f831239e9c31d14eef
Justine George <justinegeo96@gmail.com>
2023-10-05 19:36:21 -0500

    Time: 41 ms (51.14%), Space: 16.3 MB (27.37%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,17 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = {1: 1}
+        def dfs(num):
+            if num in dp:
+                return dp[num]
+            if num == 1:
+                return 1
+            
+            res = 0 if num == n else num
+            for i in range(1, num):
+                res = max(res, dfs(i) * dfs(num - i))
+            
+            dp[num] = res
+            return res
+        
+        return dfs(n)
