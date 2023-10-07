142cc92e965c9c118b81c3dfff31827bb3d5d794
Justine George <justinegeo96@gmail.com>
2023-10-07 00:03:47 -0500

    Time: 148 ms (70.52%), Space: 17.7 MB (98.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,14 +2,24 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Magic
         # T: O(n), S: O(1)
-        res, count = nums[0], 0
-        for i in range(1, len(nums)):
-            if nums[i] != res:
-                if count == 0:
-                    res = nums[i]
-                else:
-                    count -= 1
-            else:
-                count += 1
-
+        res, count = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
         return res
+
+        # # Magic
+        # # T: O(n), S: O(1)
+        # # Same code as the one above, but longer
+        # res, count = nums[0], 0
+        # for i in range(1, len(nums)):
+        #     if nums[i] != res:
+        #         if count == 0:
+        #             res = nums[i]
+        #         else:
+        #             count -= 1
+        #     else:
+        #         count += 1
+
+        # return res
