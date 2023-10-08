21745250b6aa5b55a7b1ed568311c9d88ec82e78
Justine George <justinegeo96@gmail.com>
2023-10-08 15:33:54 -0500

    Time: 2452 ms (38.87%), Space: 16.4 MB (99.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,25 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # # dp: T: O(n^2), S: O(n)
+        # # LIS starting from index i idea - bottom up, iterate in reverse
+
+        # dp = [1] * len(nums)
+        # for i in range(len(nums) - 2, -1, -1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] < nums[j]:
+        #             dp[i] = max(dp[i], 1 + dp[j])
+        
+        # # LIS could start from any index
+        # return max(dp)
+
+
         # dp: T: O(n^2), S: O(n)
-        # LIS starting from index i idea - bottom up, iterate in reverse
+        # LIS ending at index i idea - bottom up, iterate from the start
 
         dp = [1] * len(nums)
-        for i in range(len(nums) - 2, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
                     dp[i] = max(dp[i], 1 + dp[j])
         
         # LIS could start from any index
