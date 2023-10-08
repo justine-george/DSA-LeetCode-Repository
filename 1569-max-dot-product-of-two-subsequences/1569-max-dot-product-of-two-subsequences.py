421bb9dd84f48b76b6a2c83d89992bc2bb4c1d11
Justine George <justinegeo96@gmail.com>
2023-10-08 18:23:45 -0500

    Time: 259 ms (68.96%), Space: 19.1 MB (74.48%) - LeetHub

--- 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.py
@@ -10,11 +10,19 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 dp[i][j] = max(
+                    # include both nums1[i-1] and nums2[j-1] in the dot product
                     nums1[i - 1] * nums2[j - 1] + dp[i - 1][j - 1],
+
+                    # start a new subsequence with just nums1[i-1] and nums2[j-1]
                     nums1[i - 1] * nums2[j - 1],
+                    
+                    # not include nums1[i-1] but consider the previous subsequences of nums2
                     dp[i - 1][j],
+                    
+                    # not include nums2[j-1] but consider the previous subsequences of nums1
                     dp[i][j - 1]
                 )
 
-        # 3. return
+        # 3. return max dot product for the entire sequence
         return dp[m][n]
+
