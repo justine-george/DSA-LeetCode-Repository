3d0887cc169dfac4e5404376572a1fd58c15497a
Justine George <justinegeo96@gmail.com>
2023-10-08 15:29:01 -0500

    Time: 2460 ms (35.81%), Space: 16.4 MB (95.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,10 +4,12 @@ class Solution:
         # LIS starting from index i idea - bottom up, iterate in reverse
 
         dp = [1] * len(nums)
+        maxLen = 1
         for i in range(len(nums) - 2, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
                     dp[i] = max(dp[i], 1 + dp[j])
+            maxLen = max(maxLen, dp[i])
         
         # LIS could start from any index
-        return max(dp)
+        return maxLen
