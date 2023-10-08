3779ece2065bb1725d2932f1a446a40c8b7ebdbf
Justine George <justinegeo96@gmail.com>
2023-10-08 15:34:52 -0500

    Time: 2534 ms (17.31%), Space: 16.5 MB (95.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,26 +1,24 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # # dp: T: O(n^2), S: O(n)
-        # # LIS starting from index i idea - bottom up, iterate in reverse
-
-        # dp = [1] * len(nums)
-        # for i in range(len(nums) - 2, -1, -1):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] < nums[j]:
-        #             dp[i] = max(dp[i], 1 + dp[j])
-        
-        # # LIS could start from any index
-        # return max(dp)
-
-
         # dp: T: O(n^2), S: O(n)
-        # LIS ending at index i idea - bottom up, iterate from the start
+        # LIS starting from index i idea - bottom up, iterate in reverse
 
         dp = [1] * len(nums)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[j] < nums[i]:
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
                     dp[i] = max(dp[i], 1 + dp[j])
         
         # LIS could start from any index
         return max(dp)
+
+
+        # # dp: T: O(n^2), S: O(n)
+        # # LIS ending at index i idea - bottom up, iterate from the start
+        # dp = [1] * len(nums)
+        # for i in range(1, len(nums)):
+        #     for j in range(i):
+        #         if nums[j] < nums[i]:
+        #             dp[i] = max(dp[i], 1 + dp[j])
+        # # LIS could end at any index
+        # return max(dp)
