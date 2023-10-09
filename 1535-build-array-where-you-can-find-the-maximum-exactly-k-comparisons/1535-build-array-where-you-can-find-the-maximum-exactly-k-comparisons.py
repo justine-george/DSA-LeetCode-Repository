d03a01ce96ebabb02101eb6f04fe121313c27fd8
Justine George <justinegeo96@gmail.com>
2023-10-08 23:37:38 -0500

    Time: 832 ms (42.91%), Space: 43.5 MB (16.3%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -8,10 +8,12 @@ class Solution:
                 
                 return 0
             
+            # when ith number is lteq max_so_far
             # search cost doesn't increase when ith number is lteq max_so_far
             # max_so_far choices
             ans = (max_so_far * dp(i + 1, max_so_far, remain)) % MOD
 
+            # when ith number is gt max_so_far
             # search cost increases with ith values gt max_so_far 
             for num in range(max_so_far + 1, m + 1):
                 ans = (ans + dp(i + 1, num, remain - 1)) % MOD
