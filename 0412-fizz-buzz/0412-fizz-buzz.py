d09d36e56f87f57fc3502b8959258a6cc97a1c51
Justine George <justinegeo96@gmail.com>
2023-10-09 14:54:01 -0500

    Time: 54 ms (16.67%), Space: 17.3 MB (82.84%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,12 +3,11 @@ class Solution:
         res = []
         for i in range(1, n + 1):
             temp = ""
-            if i % 3 == 0 or i % 5 == 0:
-                if i % 3 == 0:
-                    temp += "Fizz"
-                if i % 5 == 0:
-                    temp += "Buzz"
-            else:
+            if i % 3 == 0:
+                temp += "Fizz"
+            if i % 5 == 0:
+                temp += "Buzz"
+            if temp == "":
                 temp += str(i)
             res.append(temp)
         
