e38aea45cae627130ddb464a70644c90562032e6
Justine George <justinegeo96@gmail.com>
2023-10-09 14:56:52 -0500

    Time: 52 ms (24.96%), Space: 17.4 MB (50.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,12 +1,12 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
+        map = {3: "Fizz", 5: "Buzz"}
         for i in range(1, n + 1):
             temp = ""
-            if i % 3 == 0:
-                temp += "Fizz"
-            if i % 5 == 0:
-                temp += "Buzz"
+            for key in map:
+                if i % key == 0:
+                    temp += map[key]
             if temp == "":
                 temp += str(i)
             res.append(temp)
