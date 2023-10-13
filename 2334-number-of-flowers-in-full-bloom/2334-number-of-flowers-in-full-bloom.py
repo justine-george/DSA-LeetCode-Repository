f323d9ccfa6ba26b2ce2f5b35971a059acdc4c28
Justine George <justinegeo96@gmail.com>
2023-10-12 20:27:10 -0500

    Time: 914 ms (46.13%), Space: 44.6 MB (56.31%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.py
@@ -3,25 +3,51 @@ class Solution:
         people = [(p, i) for i, p in enumerate(people)]
         res = [0] * len(people)
         count = 0
-        start = [f[0] for f in flowers]
-        end = [f[1] for f in flowers]
-        # make 2 min heaps - O(n) operation
-        heapq.heapify(start)
-        heapq.heapify(end)
+
+        flowers.sort()
+        # using one min heap
+        end = []
 
         # m people, n flowers
         # mlogm (sorting people) + nlogn (pop every flower)
 
+        j = 0
         for p, i in sorted(people):
             # flower came into bloom
-            while start and start[0] <= p:
-                heapq.heappop(start)
-                count += 1
+            while j < len(flowers) and flowers[j][0] <= p:
+                heapq.heappush(end, flowers[j][1])
+                j += 1
 
-            # flower withered
             while end and end[0] < p:
                 heapq.heappop(end)
-                count -= 1
-            res[i] = count
+
+            res[i] = len(end)
 
         return res
+
+        
+        # people = [(p, i) for i, p in enumerate(people)]
+        # res = [0] * len(people)
+        # count = 0
+        # start = [f[0] for f in flowers]
+        # end = [f[1] for f in flowers]
+        # # make 2 min heaps - O(n) operation
+        # heapq.heapify(start)
+        # heapq.heapify(end)
+
+        # # m people, n flowers
+        # # mlogm (sorting people) + nlogn (pop every flower)
+
+        # for p, i in sorted(people):
+        #     # flower came into bloom
+        #     while start and start[0] <= p:
+        #         heapq.heappop(start)
+        #         count += 1
+
+        #     # flower withered
+        #     while end and end[0] < p:
+        #         heapq.heappop(end)
+        #         count -= 1
+        #     res[i] = count
+
+        # return res
