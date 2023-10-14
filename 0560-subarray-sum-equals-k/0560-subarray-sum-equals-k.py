9d20d94c480dced003e6ad1eb4fc00a16a025891
Justine George <justinegeo96@gmail.com>
2023-10-14 02:04:31 -0500

    Time: 256 ms (63.58%), Space: 18.9 MB (73.73%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # 1 1 1
-        # 1 2 3 running sum
-
-        # -1    1
-        #  0    2 
-        #  1     3
-
         map = {0: 1}
-
         res = 0
         presum = 0
         for n in nums:
@@ -16,5 +8,4 @@ class Solution:
             if presum - k in map:
                 res += map[presum - k]
             map[presum] = map.get(presum, 0) + 1
-        
         return res
