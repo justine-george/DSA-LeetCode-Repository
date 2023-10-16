707c6953aa3ed9cf99f4ca963834b777a4374ab3
Justine George <justinegeo96@gmail.com>
2023-10-15 22:29:25 -0500

    Time: 44 ms (18.96%), Space: 16.3 MB (20.05%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,35 +1,15 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        # T: O(rowIndex^2), S: O(rowIndex), space efficient
-        prev_row = [0] * (rowIndex + 1)
-        row = [0] * (rowIndex + 1)
-        for k in range(rowIndex + 1):
-            print(prev_row)
-            # set start and end as 1
-            row[0] = row[k] = 1
+        if rowIndex == 0:
+            return [1]
 
-            # update middle values
-            for i in range(1, k):
-                row[i] = prev_row[i - 1] + prev_row[i]
+        prev = [1, 1]
+        l = 2
+        while l <= rowIndex:
+            cur = [1] * (l + 1)
+            for i in range(1, l):
+                cur[i] = prev[i - 1] + prev[i]
+            prev = cur[:]
+            l += 1
 
-            # row is the prev_row for next iteration
-            prev_row = row[:]
-        
-        # return prev_row
-        return prev_row
-# p
-# c    [1, 0, 0, 0]
-#     [1, 1, 0, 0]
-#     [1, 2, 1, 0]
-#     [1, 3, 3, 1]
-
-        # # recursive solution, but takes O(n^2) space
-        # row = [1] * (rowIndex + 1)
-        # if rowIndex == 0:
-        #     return row
-        
-        # prev_row = self.getRow(rowIndex - 1)
-        # for i in range(1, len(row) - 1):
-        #     row[i] = prev_row[i - 1] + prev_row[i]
-        
-        # return row
+        return prev
