8d3fb248030ef7f234919ef10c66085b28012dd2
Justine George <justinegeo96@gmail.com>
2023-10-17 16:24:16 -0500

    Time: 252 ms (95.44%), Space: 19.2 MB (51.98%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
@@ -14,6 +14,7 @@ class Solution:
                 root = i
                 break
         
+        # iterative dfs with cycle detection
         visited = set()
         stack = [root]
         while stack:
