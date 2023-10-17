c4dc32a76fe2bfc5325f3b022d28ab1cddbc0671
Justine George <justinegeo96@gmail.com>
2023-10-17 16:52:40 -0500

    Time: 457 ms (96.74%), Space: 30.9 MB (53.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # O(n), O(n) solution
         visited = set()
         for n in nums:
             if n in visited:
                 return True
             visited.add(n)
         return False
+
+        # # O(nlogn), O(1) solution
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i - 1]:
+        #         return True
+        # return False
