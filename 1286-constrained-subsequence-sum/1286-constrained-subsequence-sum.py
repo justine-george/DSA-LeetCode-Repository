2293440d16fe7b98372a465e3dace2ca72a086c0
Justine George <justinegeo96@gmail.com>
2023-10-21 17:58:46 -0500

    Time: 1254 ms (97.02%), Space: 29.9 MB (97.87%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
@@ -1,18 +1,34 @@
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        # T: O(nlogn), S: O(n)
-        res = nums[0]
-        max_heap = [(-nums[0], 0)] # max_sum, index
-
-        for i in range(1, len(nums)):
-            while i - max_heap[0][1] > k:
-                heapq.heappop(max_heap)
-
-            cur_max = max(nums[i], nums[i] - max_heap[0][0])
-            heapq.heappush(max_heap, (-cur_max, i))
-            res = max(res, cur_max)
+        # T: O(n), S: O(n), Monotonic decreasing queue(max would be at 0th index)
+        dp = [0] * len(nums)
+        queue = deque()
         
-        return res
+        for i in range(len(nums)):
+            if queue and i - queue[0] > k:
+                queue.popleft()
+            
+            dp[i] = nums[i] + (dp[queue[0]] if queue else 0)
+
+            while queue and dp[queue[-1]] < dp[i]:
+                queue.pop()
+            
+            if dp[i] > 0:
+                queue.append(i)
+        
+        return max(dp)
+        
+        # # T: O(nlogn), S: O(n)
+        # res = nums[0]
+        # max_heap = [(-nums[0], 0)] # max_sum, index
+
+        # for i in range(1, len(nums)):
+        #     while i - max_heap[0][1] > k:
+        #         heapq.heappop(max_heap)
+        #     cur_max = max(nums[i], nums[i] - max_heap[0][0])
+        #     heapq.heappush(max_heap, (-cur_max, i))
+        #     res = max(res, cur_max)
+        # return res
         
         # # T: O(nk), S: O(n) not optimal here, TLE
         # dp = [0] * len(nums)
