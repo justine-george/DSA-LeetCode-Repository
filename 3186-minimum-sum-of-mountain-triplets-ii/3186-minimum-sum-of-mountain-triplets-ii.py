89983e96931d25d8db09d1024bfa3714bf0e5424
Justine George <justinegeo96@gmail.com>
2023-10-21 23:25:39 -0500

    Time: 683 ms (100%), Space: 30.9 MB (100%) - LeetHub

--- 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.py
+++ 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumSum(self, nums: List[int]) -> int:
         n = len(nums)
         res = float('inf')
-        skip = [False] * n
+        # skip = [False] * n
         
         pre = [0] * n # min number to left of this index
         cur_min = nums[0]
@@ -10,8 +10,8 @@ class Solution:
             cur_min = min(cur_min, nums[i - 1])
             pre[i] = cur_min
             
-            if nums[i] <= cur_min:
-                skip[i] = True
+            # if nums[i] <= cur_min:
+            #     skip[i] = True
         
         post = [0] * n # min number to right of this index
         cur_min = nums[-1]
@@ -19,12 +19,12 @@ class Solution:
             cur_min = min(cur_min, nums[i + 1])
             post[i] = cur_min
             
-            if nums[i] <= cur_min:
-                skip[i] = True
+            # if nums[i] <= cur_min:
+            #     skip[i] = True
             
         for i in range(1, n - 1):
-            if not skip[i]:
+            # if not skip[i]:
+            if nums[i] > pre[i] and nums[i] > post[i]:
                 res = min(res, pre[i] + nums[i] + post[i])
         
         return res if res != float('inf') else -1
-    
