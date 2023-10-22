2d410a45032409a57134c7db7b44908a59595cd2
Justine George <justinegeo96@gmail.com>
2023-10-22 00:47:11 -0500

    Time: 895 ms (100%), Space: 34.3 MB (100%) - LeetHub

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.py
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.py
@@ -22,31 +22,3 @@ class Solution:
             if val:
                 res = min(res, val)
         return res
-
-
-
-        # max_heap = []
-        # for c_val in cMap.values():
-        #     max_val = max(max_val, c_val)
-        #     min_val = min(min_val, c_val)
-        #     heapq.heappush(max_heap, -c_val)
-            
-        # while max_heap and max_val - min_val > 1:
-        #     cur_max = -1 * heapq.heappop(max_heap)
-            
-        #     i = 2
-        #     while (cur_max // i) - min_val > 1:
-        #         i += 1
-            
-        #     min_val = min(min_val, cur_max // i, cur_max - (cur_max // i))
-            
-        #     if cur_max % i != 0:
-        #         heapq.heappush(max_heap, - (cur_max % i))
-            
-        #     while i > 0:
-        #         heapq.heappush(max_heap, - (cur_max // i))
-        #         i -= 1
-            
-        #     max_val = -1 * max_heap[0]
-        
-        # return len(max_heap)
