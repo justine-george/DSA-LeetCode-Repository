18b81ac13e315c9c1e0dead32c19b93adc08e6ec
Justine George <justinegeo96@gmail.com>
2023-10-22 19:37:27 -0500

    Time: 1033 ms (88.04%), Space: 30.4 MB (75.87%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,28 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # T: O(n), S: O(1) - reverse iterate, only constant space
         n = len(temperatures)
         res = [0] * n
-        
-        for i in range(n - 1, -1, -1):
-            it = i + 1
-            while it < n:
-                if temperatures[it] > temperatures[i]:
-                    res[i] = it - i
-                    break
-                elif res[it] == 0:
-                    break
-                else:
-                    it += res[it]
 
+        stack = [] # maintain monotonic decreasing stack
+        for i in range(n):
+            # current temp is greater than top of the stack
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                index = stack.pop()
+                res[index] = i - index
+            stack.append(i)
+        
         return res
-    
-        # # T: O(n), S: O(n)
-        # res = [0] * len(temperatures)
-        # st = [] # keep stack monotonic non-increasing, save indexes only
-        # for i in range(len(temperatures)):
-        #     while st and temperatures[st[-1]] < temperatures[i]:
-        #         index = st.pop()
-        #         res[index] = i - index
-        #     st.append(i)
-        # return res
