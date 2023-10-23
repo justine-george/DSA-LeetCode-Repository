014b476aa48d911c608f5a2b85c7f2c3785f6e2e
Justine George <justinegeo96@gmail.com>
2023-10-22 20:13:28 -0500

    Time: 60 ms (84.64%), Space: 17.5 MB (41.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # T: O(n), S: O(n)
         map = {}
         for i, n in enumerate(nums):
             if target - n in map:
