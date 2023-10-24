07779c2c84ddab04ac41477fa05e916541d8a6ff
Justine George <justinegeo96@gmail.com>
2023-10-23 22:03:23 -0500

    Time: 115 ms (16.38%), Space: 22.2 MB (21.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         cMap = defaultdict(list)
-
-        def getFreq(word):
+        for word in strs:
             charmap = [0] * 26
             for c in word:
                 charmap[ord(c) - ord('a')] += 1
-            res = ""
-            for i in charmap:
-                res += "#" + str(i)
-            return res
-
-        for word in strs:
-            freqMap = getFreq(word)
-            cMap[freqMap].append(word)
-        
+            cMap[tuple(charmap)].append(word)
         return cMap.values()
