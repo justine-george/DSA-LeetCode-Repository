49d9476e3e50b0ea329113d4245856a2e38a97f2
Justine George <justinegeo96@gmail.com>
2023-10-23 21:56:02 -0500

    Time: 136 ms (7.2%), Space: 20.4 MB (46.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # T: O(m*n)
-        res = collections.defaultdict(list) # mapping charcount list of words that are anagrams
-        
+        cMap = defaultdict(list)
+
+        def getFreq(word):
+            map = defaultdict(int)
+            for c in sorted(word):
+                map[c] += 1
+            res = []
+            for key in map:
+                res.append(str(key) + str(map[key]))
+            return "".join(res)
+
         for word in strs:
-            count = [0] * 26 # one for each character a- z
-            for c in word:
-                count[ord(c) - ord('a')] += 1
-                
-            res[tuple(count)].append(word)
-                
-        return res.values()
+            freqMap = getFreq(word)
+            print(freqMap)
+            cMap[freqMap].append(word)
+        
+        return cMap.values()
