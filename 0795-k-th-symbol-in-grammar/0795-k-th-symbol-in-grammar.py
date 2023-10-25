0d6aa99a2723f2ff8e18f5f290624dca53932103
Justine George <justinegeo96@gmail.com>
2023-10-25 18:27:48 -0500

    Time: 36 ms (73.87%), Space: 16.2 MB (79.28%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -11,10 +11,29 @@ class Solution:
         # 5,6 -> 3
         # 7,8 -> 4
 
-        if k == 1:
-            return 0
-        elif k % 2:
-            return self.kthGrammar(n - 1, (k + 1) / 2)
-        else:
-            val = self.kthGrammar(n - 1, k / 2)
-            return 0 if val else 1
+        # if k == 1:
+        #     return 0
+        # elif k % 2:
+        #     return self.kthGrammar(n - 1, (k + 1) / 2)
+        # else:
+        #     val = self.kthGrammar(n - 1, k / 2)
+        #     return 0 if val else 1
+
+        # binary search tree solution
+        # T: O(n), S: O(1)
+
+        def dfs(n, k, root):
+            if n == 1:
+                return root
+            
+            total_nodes = 2 ** (n - 1)
+
+            # right half
+            if k > (total_nodes / 2):
+                next_root_val = 1 if root == 0 else 0
+                return dfs(n - 1, k - (total_nodes / 2), next_root_val)
+            else:
+                next_root_val = 0 if root == 0 else 1
+                return dfs(n - 1, k, next_root_val)
+        
+        return dfs(n, k, 0)
