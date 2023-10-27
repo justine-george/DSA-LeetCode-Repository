70f357fc101cf7b9cf22868481c10ca24983bd2e
Justine George <justinegeo96@gmail.com>
2023-10-27 00:19:29 -0500

    Time: 482 ms (62.86%), Space: 16.3 MB (79.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # start from each character and go outwards
-        # consider both odd and even length palindromes
-        # O(n**2)
-        resLength, res_start_index, res_end_index = 0, 0, 0
-
+        l_index, r_index = 0, 0
+        max_len = 0
         for i in range(len(s)):
-            # odd
-            resLength, res_start_index, res_end_index = self.checkPalindrome(i, i, s, resLength, res_start_index, res_end_index)
+            l, r = i, i
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    break
+                if r - l + 1 > max_len:
+                    max_len = r - l + 1
+                    l_index, r_index = l, r
+                l -= 1
+                r += 1
 
-            # even
-            resLength, res_start_index, res_end_index = self.checkPalindrome(i, i + 1, s, resLength, res_start_index, res_end_index)
+            l, r = i, i + 1
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    break
+                if r - l + 1 > max_len:
+                    max_len = r - l + 1
+                    l_index, r_index = l, r
+                l -= 1
+                r += 1
+
         
-        return s[res_start_index: res_end_index + 1]
-
-    def checkPalindrome(self, l, r, s, resLength, res_start_index, res_end_index):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            if (r - l + 1) > resLength:
-                resLength = r - l + 1
-                res_start_index = l
-                res_end_index = r
-            l -= 1
-            r += 1
-        return (resLength, res_start_index, res_end_index)
+        return s[l_index: r_index + 1]
+            
