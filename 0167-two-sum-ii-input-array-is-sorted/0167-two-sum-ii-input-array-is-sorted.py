0f0be77ba678488780bd0ead77c33e314032c49a
Justine George <justinegeo96@gmail.com>
2023-10-27 19:55:53 -0500

    Time: 169 ms (6.55%), Space: 17.2 MB (69.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,14 +1,21 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # two pointers, for both ends of the list
-        l, r = 0, len(numbers) - 1
-        
-        while l <= r:
-            sum = numbers[l] + numbers[r]
-            
-            if target == sum:
-                return [l + 1, r + 1]
-            elif target < sum:
-                r -= 1
-            else:
-                l += 1
+        def bin_search(t, i):
+            l, r = i + 1, len(numbers) - 1
+            while l <= r:
+                m = (l + r) // 2
+
+                if numbers[m] == t and m != i:
+                    return m
+                
+                if numbers[m] < t:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return None
+
+
+        for i, n in enumerate(numbers):
+            val = bin_search(target - n, i)
+            if val:
+                return [i + 1, val + 1] if i < val else [val + 1, i + 1]
