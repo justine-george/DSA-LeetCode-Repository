4a166d7ca814be6c30ff825fea76d80aea290803
Justine George <justinegeo96@gmail.com>
2023-10-28 17:53:24 -0500

    Time: 273 ms (53.48%), Space: 16.5 MB (68.52%) - LeetHub

--- 1332-count-vowels-permutation/1332-count-vowels-permutation.py
+++ 1332-count-vowels-permutation/1332-count-vowels-permutation.py
@@ -1,18 +1,19 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        dp = {(i, 1): 1 for i in ('a', 'e', 'i', 'o', 'u')}
-
+        prev = [1] * 5
         for i in range(2, n + 1):
-            for j in ('a', 'e', 'i', 'o', 'u'):
-                if j == 'a':
-                    dp[('a', i)] = dp[('e', i - 1)] + dp[('i', i - 1)] + dp[('u', i - 1)]
-                elif j == 'e':
-                    dp[('e', i)] = dp[('a', i - 1)] + dp[('i', i - 1)]
-                elif j == 'i':
-                    dp[('i', i)] = dp[('e', i - 1)] + dp[('o', i - 1)]
-                elif j == 'o':
-                    dp[('o', i)] = dp[('i', i - 1)]
+            cur = [0] * 5
+            for j in range(5):
+                if j == 0:
+                    cur[0] = prev[1] + prev[2] + prev[4]
+                elif j == 1:
+                    cur[1] = prev[0] + prev[2]
+                elif j == 2:
+                    cur[2] = prev[1] + prev[3]
+                elif j == 3:
+                    cur[3] = prev[2]
                 else:
-                    dp[('u', i)] = dp[('i', i - 1)] + dp[('o', i - 1)]
+                    cur[4] = prev[2] + prev[3]
+            prev = cur
 
-        return sum(dp[(i, n)] for i in ('a', 'e', 'i', 'o', 'u')) % (10 ** 9 + 7)
+        return sum(prev) % (10 ** 9 + 7)
