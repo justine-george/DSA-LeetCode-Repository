5e1e12841c44842c48432822cf26c5eb27284cde
Justine George <justinegeo96@gmail.com>
2023-11-01 14:11:38 -0500

    Time: 102 ms (58.33%), Space: 16.3 MB (49.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
         l = r = 0
-        freq_map = [0] * 26
+        freq_map = {}
         max_freq = 0
         max_len = 0
-        while l <= r and r < len(s):
-            freq_map[ord(s[r]) - ord('A')] += 1
-            max_freq = max(max_freq, freq_map[ord(s[r]) - ord('A')])
+        while r < len(s):
+            freq_map[s[r]] = freq_map.get(s[r], 0) + 1
+            max_freq = max(max_freq, freq_map[s[r]])
             while l <= r and (r - l + 1) - max_freq > k:
-                freq_map[ord(s[l]) - ord('A')] -= 1
+                freq_map[s[l]] -= 1
                 l += 1
             max_len = max(max_len, r - l + 1)
             r += 1
