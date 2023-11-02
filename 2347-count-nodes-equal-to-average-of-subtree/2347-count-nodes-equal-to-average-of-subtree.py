48de62e0c8ea41cc376dcc624f4514d764cf835f
Justine George <justinegeo96@gmail.com>
2023-11-02 04:17:37 -0500

    Time: 51 ms (49.33%), Space: 17.4 MB (96.93%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
@@ -9,10 +9,6 @@ class Solution:
         res = 0
         def avg(node):
             nonlocal res
-
-            # leaf node
-            if not node:
-                return 0, 0
             
             totalL = totalR = countL = countR = 0
             if node.left:
@@ -25,9 +21,7 @@ class Solution:
             if totalSum // totalCount == node.val:
                 res += 1
             
-            print(f"Node: {node.val}, totalCount: {totalCount}, totalSum: {totalSum}")
-            
             return totalCount, totalSum
             
-        level, total = avg(root)
+        avg(root)
         return res
