d1fc61cd30da88e958b49508dbe5254088185b6c
Justine George <justinegeo96@gmail.com>
2023-11-02 07:34:47 -0500

    Time: 1259 ms (97.89%), Space: 33.1 MB (72.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -26,10 +26,3 @@ class Solution:
             res.append(nums[queue[0]])
         
         return res
-
-        # 0 1 -> nums[1] = 3, now remove index 0 from left if exists
-        # 1 -> nums[1] = 3, now remove index 1 from left if exists
-        # 4 -> nums[4] = 5, now remove index 2 from left if exists
-        # 4 -> nums[4] = 5, now remove index 3 from left if exists
-        # 4, 6 -> nums[6] = 6, now remove index 4 from left if exists
-        # 6, 7 -> nums[7] = 7, now remove index 5 from left if exists
