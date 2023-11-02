4e74b957ba3efc43a2d71656f2917c67872f2e0c
Justine George <justinegeo96@gmail.com>
2023-11-02 04:20:23 -0500

    Time: 49 ms (58.69%), Space: 17.5 MB (58.02%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
@@ -7,20 +7,20 @@
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         res = 0
+
         def avg(node):
             nonlocal res
-            
+            if not node:
+                return 0, 0
             totalL = totalR = countL = countR = 0
-            if node.left:
-                countL, totalL = avg(node.left)
-            if node.right:
-                countR, totalR = avg(node.right)
-
+            # if node.left:
+            countL, totalL = avg(node.left)
+            # if node.right:
+            countR, totalR = avg(node.right)
             totalCount = 1 + countL + countR
             totalSum = totalL+ totalR + node.val
             if totalSum // totalCount == node.val:
                 res += 1
-            
             return totalCount, totalSum
             
         avg(root)
