a896534f767f23f3108854847bbb4e8fe2dd5076
Justine George <justinegeo96@gmail.com>
2023-11-03 08:25:08 -0500

    Time: 33 ms (88.07%), Space: 16.1 MB (88.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,8 +4,12 @@ class Solution:
         stack = []
         map = {'(': ')', '{': '}', '[': ']'}
         for c in s:
+            # if it is opening, add
             if c in map:
                 stack.append(c)
+            # if it is closing, check:
+            #   1. closing to an empty stack is False
+            #   2. map[stack.pop()] this should be same as the incoming closing
             elif not stack or c != map[stack.pop()]:
                 return False
         return not stack
