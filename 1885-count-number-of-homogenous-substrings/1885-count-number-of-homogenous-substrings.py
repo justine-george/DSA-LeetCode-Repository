88765d3c6f5ce81ecaddf0425a9854aa5bc7dd6e
Justine George <justinegeo96@gmail.com>
2023-11-08 21:46:54 -0600

    Time: 159 ms (21.58%), Space: 17.4 MB (30.58%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
@@ -1,26 +1,15 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        # 1 1
-        # 2 3
-        # 3 6
-        # 4 10
-        # 5 15
         def getSumTillN(n):
             return (n * (n + 1)) // 2
 
-        # {n_at_a_time: count}
-        map = defaultdict(int)
         i = 0
+        res = 0
         while i < len(s):
             start = i
             while i < len(s) and s[i] == s[start]:
                 i += 1
             n_at_a_time = i - start
-            map[n_at_a_time] += 1
-        
-        res = 0
-        for n_at_a_time in map:
-            res += getSumTillN(n_at_a_time) * map[n_at_a_time]
+            res += getSumTillN(n_at_a_time)
             res %= (10 ** 9 + 7)
-        
         return res
