a0bd7c3f73d10d457ea2c680255dac227684356b
Justine George <justinegeo96@gmail.com>
2023-11-11 19:44:20 -0600

    Time: 692 ms (69.9%), Space: 32.3 MB (27.55%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
@@ -1,15 +1,15 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
         n = len(dist)
-        time_taken = [dist[i]/speed[i] for i in range(n)]
+        time_taken = [math.ceil(dist[i]/speed[i]) for i in range(n)]
 
         time_taken.sort()
 
         kill_count = 0
         # i is the time elapsed
         for i in range(n):
-            if time_taken[i] - i > 0:
-                kill_count += 1
-            else:
-                break
+            if time_taken[i] <= i:
+                return kill_count
+            kill_count += 1
+        
         return kill_count
