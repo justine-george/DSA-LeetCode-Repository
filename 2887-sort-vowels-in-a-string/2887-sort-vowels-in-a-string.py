9fb930a8e73f4d81746958a2457130fddcb2823d
Justine George <justinegeo96@gmail.com>
2023-11-13 09:15:09 -0600

    Time: 179 ms (36.4%), Space: 32.1 MB (5.22%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -4,6 +4,19 @@ class Solution:
         vowel_set = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
 
         vowels = [s[i] for i in range(n) if s[i] in vowel_set]
-        vowels.sort(reverse = True)
+        # vowels.sort(reverse = True)
 
-        return "".join([vowels.pop() if s[i] in vowel_set else s[i] for i in range(n)])
+        # return "".join([vowels.pop() if s[i] in vowel_set else s[i] for i in range(n)])
+        vowel_indices = [i for i in range(n) if s[i] in vowel_set]
+        vowels.sort()
+
+        d = {i: v for i, v in zip(vowel_indices, vowels)}
+
+        res = []
+        for i in range(n):
+            if s[i] in vowel_set:
+                res.append(d[i])
+            else:
+                res.append(s[i])
+
+        return "".join(res)
