a407418b6fea7bc67de914cb802b89e570aa1162
Justine George <justinegeo96@gmail.com>
2024-02-29 20:36:52 -0600

    Time: 66 ms (63.65%) | Memory: 18 MB (89.80%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,39 +1,70 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        cur, res = 0, 0
-        sign = 1 # 1 for +, and -1 for -
-
+        cur = 0
+        op = '+'
         stack = []
 
-        i = 0
+        def helper(op, cur):
+            if op == '+':
+                stack.append(cur)
+            elif op == '-':
+                stack.append(-cur)
+
         for char in s:
             if char.isdigit():
                 cur = cur * 10 + int(char)
-            elif char in '+-':
-                res += sign * cur
+            elif char in '(':
+                stack.append(op)
 
-                sign = 1 if char == '+' else -1
                 cur = 0
-            elif char == '(':
-                stack.append((res, sign))
-                # start a new calculate function
-                sign = 1
-                res = 0
-            elif char == ')':
-                # take care of edge case, similar to what we did when we see + or -
-                res += sign * cur
+                op = '+'
+            elif char in '+-)':
+                helper(op, cur)
+                if char == ')':
+                    cur = 0
+                    while isinstance(stack[-1], int):
+                        cur += stack.pop()
+                    op = stack.pop()
+                    helper(op, cur)
 
-                prev_res, sign = stack.pop()
-                res = prev_res + res * sign
                 cur = 0
+                op = char
         
-        # take care of edge case, similar to what we did when we see + or -
-        res += sign * cur
+        helper(op, cur)
 
-        return res
-
+        return sum(stack)
 
 
+        # cur, res = 0, 0
+        # sign = 1 # 1 for +, and -1 for -
 
+        # stack = []
+
+        # i = 0
+        # for char in s:
+        #     if char.isdigit():
+        #         cur = cur * 10 + int(char)
+        #     elif char in '+-':
+        #         res += sign * cur
+
+        #         sign = 1 if char == '+' else -1
+        #         cur = 0
+        #     elif char == '(':
+        #         stack.append((res, sign))
+        #         # start a new calculate function
+        #         sign = 1
+        #         res = 0
+        #     elif char == ')':
+        #         # take care of edge case, similar to what we did when we see + or -
+        #         res += sign * cur
+
+        #         prev_res, sign = stack.pop()
+        #         res = prev_res + res * sign
+        #         cur = 0
+        
+        # # take care of edge case, similar to what we did when we see + or -
+        # res += sign * cur
+
+        # return res
 
 
