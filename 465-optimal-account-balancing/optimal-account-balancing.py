f280dd12b4f00e70e94793a3935b5b3a153bcf9e
Justine George <justinegeo96@gmail.com>
2024-03-02 01:36:29 -0600

    Time: 314 ms (52.26%) | Memory: 16.6 MB (52.26%) - LeetSync

--- 465-optimal-account-balancing/optimal-account-balancing.py
+++ 465-optimal-account-balancing/optimal-account-balancing.py
@@ -1,48 +1,38 @@
+from collections import defaultdict
+
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
         score = defaultdict(int)
+        for f, t, amount in transactions:
+            score[f] += amount
+            score[t] -= amount
+        
+        positives = [v for v in score.values() if v > 0]
+        negatives = [v for v in score.values() if v < 0]
 
-        for f, t, a in transactions:
-            score[f] += a
-            score[t] -= a
-        
-        positives = [val for val in score.values() if val > 0]
-        negatives = [val for val in score.values() if val < 0]
-        
-        dp = {}
         def recurse(positives, negatives):
             if len(positives) + len(negatives) == 0:
                 return 0
-            
-            key = (tuple(sorted(positives)), tuple(sorted(negatives)))
-            if key in dp:
-                return dp[key]
-            
 
-            neg = negatives[0]
+            negative = negatives[0]
             count = inf
 
-            for pos in positives:
+            for positive in positives:
                 new_positives = positives.copy()
                 new_negatives = negatives.copy()
 
-                # we want to reduce the number of nodes to zero essentially
-                new_positives.remove(pos)
-                new_negatives.remove(neg)
+                new_positives.remove(positive)
+                new_negatives.remove(negative)
 
-                # best possible case
-                if pos == -neg:
+                if positive == -negative:
                     pass
-                # give out all and clear debt, but receiver yet to receive little more
-                elif pos > -neg:
-                    new_positives.append(pos+neg)
-                # receiver got all, but a little more money to give out for the person in debt
+                elif positive > -negative:
+                    new_positives.append(positive + negative)
                 else:
-                    new_negatives.append(pos+neg)
+                    new_negatives.append(positive + negative)
                 
                 count = min(count, recurse(new_positives, new_negatives))
-            
-            dp[key] = count + 1
+
             return count + 1
-        
+
         return recurse(positives, negatives)
