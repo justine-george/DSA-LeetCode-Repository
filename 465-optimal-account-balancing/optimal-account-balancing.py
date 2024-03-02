e8c5855addbb9264da7404985a225b9805205fb1
Justine George <justinegeo96@gmail.com>
2024-03-02 00:43:56 -0600

    Time: 74 ms (80.26%) | Memory: 18.8 MB (5.65%) - LeetSync

--- 465-optimal-account-balancing/optimal-account-balancing.py
+++ 465-optimal-account-balancing/optimal-account-balancing.py
@@ -8,10 +8,16 @@ class Solution:
         
         positives = [val for val in score.values() if val > 0]
         negatives = [val for val in score.values() if val < 0]
-
+        
+        dp = {}
         def recurse(positives, negatives):
             if len(positives) + len(negatives) == 0:
                 return 0
+            
+            key = (tuple(sorted(positives)), tuple(sorted(negatives)))
+            if key in dp:
+                return dp[key]
+            
 
             neg = negatives[0]
             count = inf
@@ -36,6 +42,7 @@ class Solution:
                 
                 count = min(count, recurse(new_positives, new_negatives))
             
+            dp[key] = count + 1
             return count + 1
         
         return recurse(positives, negatives)
