40bf3b2f5753e1d28faf8a3bdbf11aaa147aa59b
Justine George <justinegeo96@gmail.com>
2024-03-02 02:22:15 -0600

    Time: 48 ms (71.99%) | Memory: 36.6 MB (8.23%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        self.counter = 0
+        self.targetSum = targetSum
+        self.dfs(root, 0, {0: 1})
+        return self.counter
+
+    def dfs(self, node, curSum, prevSums):
+        if not node:
+            return
+        
+        curSum += node.val
+        self.counter += prevSums.get(curSum - self.targetSum, 0)
+
+        # update map
+        new_prevSums = prevSums.copy()
+        new_prevSums[curSum] = new_prevSums.get(curSum, 0) + 1
+
+        # traverse left and right
+        self.dfs(node.left, curSum, new_prevSums)
+        self.dfs(node.right, curSum, new_prevSums)
+
+        # # backtrack
+        # prevSums[curSum] -= 1
+        # if prevSums[curSum] == 0:
+        #     del prevSums[curSum]
+
+        # if not root:
+        #     return 0
+
+        # stack = [(root, 0, {0: 1})]
+        # counter = 0
+        # while stack:
+        #     currentNode, curSum, prevSums = stack.pop()
+        #     if not currentNode:
+        #         continue
+
+        #     curSum += currentNode.val
+        #     counter += prevSums.get(curSum - targetSum, 0)
+
+        #     newSums = prevSums.copy()
+        #     newSums[curSum] = newSums.get(curSum, 0) + 1
+
+        #     stack.append((currentNode.right, curSum, newSums))
+        #     stack.append((currentNode.left, curSum, newSums))
+
+        # return counter
