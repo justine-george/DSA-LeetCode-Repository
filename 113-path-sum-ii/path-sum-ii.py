d64f83eec6bc2e3ee6de3a00bf1c0a1d590971b8
Justine George <justinegeo96@gmail.com>
2024-03-02 02:31:15 -0600

    Time: 39 ms (79.08%) | Memory: 21.7 MB (13.34%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+
+        self.res = []
+        self.dfs(root, [], targetSum)
+        return self.res
+
+    def dfs(self, node, curPath, remSum):
+        curPath.append(node.val)
+        newRemSum = remSum - node.val
+
+        if not node.left and not node.right and newRemSum == 0:
+            self.res.append(curPath)
+            return
+        if node.right:
+            self.dfs(node.right, curPath.copy(), newRemSum)
+        if node.left:
+            self.dfs(node.left, curPath.copy(), newRemSum)
+
+
+        # res = []
+        # if not root:
+        #     return res
+        
+        # stack = [(root, [], targetSum)]
+        # while stack:
+        #     cur, path, remSum = stack.pop()
+        #     if cur:
+        #         path.append(cur.val)
+        #         newRemSum = remSum - cur.val
+        #         if not cur.left and not cur.right and newRemSum == 0:
+        #             res.append(path)
+        #             continue
+        #         if cur.right:
+        #             stack.append((cur.right, path[:], newRemSum))
+        #         if cur.left:
+        #             stack.append((cur.left, path[:], newRemSum))
+        # return res
+
+        # target = 22
+
+        # [
+        #     22, 5, []
+        # ]
+
+        # 22, 5, []
+        
+        # target - sum(list) = 17
+
+
+
