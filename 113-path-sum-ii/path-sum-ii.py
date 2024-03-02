20a0b3cda6afc06df1eba50fea2ec9c0a7aa4d96
Justine George <justinegeo96@gmail.com>
2024-03-02 02:34:22 -0600

    Time: 40 ms (74.18%) | Memory: 17.5 MB (61.97%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -6,44 +6,44 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+    #     if not root:
+    #         return []
+
+    #     self.res = []
+    #     self.dfs(root, [], targetSum)
+    #     return self.res
+
+    # def dfs(self, node, curPath, remSum):
+    #     newPath = curPath + [node.val]
+    #     newRemSum = remSum - node.val
+
+    #     if not node.left and not node.right and newRemSum == 0:
+    #         self.res.append(newPath)
+    #         return
+
+    #     if node.right:
+    #         self.dfs(node.right, newPath, newRemSum)
+    #     if node.left:
+    #         self.dfs(node.left, newPath, newRemSum)
+
+        res = []
         if not root:
-            return []
-
-        self.res = []
-        self.dfs(root, [], targetSum)
-        return self.res
-
-    def dfs(self, node, curPath, remSum):
-        newPath = curPath + [node.val]
-        newRemSum = remSum - node.val
-
-        if not node.left and not node.right and newRemSum == 0:
-            self.res.append(newPath)
-            return
-            
-        if node.right:
-            self.dfs(node.right, newPath, newRemSum)
-        if node.left:
-            self.dfs(node.left, newPath, newRemSum)
-
-        # res = []
-        # if not root:
-        #     return res
+            return res
         
-        # stack = [(root, [], targetSum)]
-        # while stack:
-        #     cur, path, remSum = stack.pop()
-        #     if cur:
-        #         path.append(cur.val)
-        #         newRemSum = remSum - cur.val
-        #         if not cur.left and not cur.right and newRemSum == 0:
-        #             res.append(path)
-        #             continue
-        #         if cur.right:
-        #             stack.append((cur.right, path[:], newRemSum))
-        #         if cur.left:
-        #             stack.append((cur.left, path[:], newRemSum))
-        # return res
+        stack = [(root, [], targetSum)]
+        while stack:
+            cur, path, remSum = stack.pop()
+            if cur:
+                path.append(cur.val)
+                newRemSum = remSum - cur.val
+                if not cur.left and not cur.right and newRemSum == 0:
+                    res.append(path)
+                    continue
+                if cur.right:
+                    stack.append((cur.right, path[:], newRemSum))
+                if cur.left:
+                    stack.append((cur.left, path[:], newRemSum))
+        return res
 
         # target = 22
 
