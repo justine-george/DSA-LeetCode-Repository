c78f4ce41c6b040a05574af50380a7832226cf8a
Justine George <justinegeo96@gmail.com>
2024-03-03 19:40:36 -0600

    Time: 391 ms (51.30%) | Memory: 61.5 MB (59.41%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
@@ -6,19 +6,8 @@
 #         self.right = right
 class Solution:
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
-        def create_graph(node):
-            if node.left:
-                graph[node.val].append(node.left.val)
-                graph[node.left.val].append(node.val)
-                create_graph(node.left)
-
-            if node.right:
-                graph[node.val].append(node.right.val)
-                graph[node.right.val].append(node.val)
-                create_graph(node.right)
-
-        graph = collections.defaultdict(list)
-        create_graph(root)
+        self.graph = collections.defaultdict(list)
+        self.create_graph(root)
 
         q = collections.deque([(start, 0)]) # (node, no of minutes)
         time_taken = 0
@@ -27,8 +16,23 @@ class Solution:
             cur_val, time_elapsed = q.popleft()
             visited.add(cur_val)
             time_taken = max(time_taken, time_elapsed)
-            for neighbor in graph[cur_val]:
+            for neighbor in self.graph[cur_val]:
                 if neighbor not in visited:
                     q.append((neighbor, time_elapsed + 1))
 
         return time_taken
+    
+    def create_graph(self, node):
+        if not node:
+            return
+
+        if node.left:
+            self.graph[node.val].append(node.left.val)
+            self.graph[node.left.val].append(node.val)
+            
+        if node.right:
+            self.graph[node.val].append(node.right.val)
+            self.graph[node.right.val].append(node.val)
+        
+        self.create_graph(node.left)
+        self.create_graph(node.right)
