56852794a4069029f6e1be82b6b3b47406fb8db8
Justine George <justinegeo96@gmail.com>
2024-03-04 00:44:31 -0600

    Time: 366 ms (94.84%) | Memory: 16.8 MB (93.27%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -6,7 +6,7 @@ class Solution:
             return -1
 
         q = collections.deque([(0, 0, 1)])
-        visited = set([(0, 0)])
+        grid[0][0] = -1 # mark as visited on the grid itself
 
         directions = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1)]
         while q:
@@ -17,8 +17,31 @@ class Solution:
 
             for dr, dc in directions:
                 new_r, new_c = r + dr, c + dc
-                if 0 <= new_r < n and 0 <= new_c < n and grid[new_r][new_c] == 0 and (new_r, new_c) not in visited:
+                if 0 <= new_r < n and 0 <= new_c < n and grid[new_r][new_c] == 0:
                     q.append((new_r, new_c, length + 1))
-                    visited.add((new_r, new_c))
+                    grid[new_r][new_c] = -1
         
         return -1
+
+        # n = len(grid)
+
+        # if grid[0][0] == 1 or grid[n - 1][n -1] == 1:
+        #     return -1
+
+        # q = collections.deque([(0, 0, 1)])
+        # visited = set([(0, 0)])
+
+        # directions = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1)]
+        # while q:
+        #     r, c, length = q.popleft()
+            
+        #     if r == n - 1 and c == n - 1:
+        #         return length
+
+        #     for dr, dc in directions:
+        #         new_r, new_c = r + dr, c + dc
+        #         if 0 <= new_r < n and 0 <= new_c < n and grid[new_r][new_c] == 0 and (new_r, new_c) not in visited:
+        #             q.append((new_r, new_c, length + 1))
+        #             visited.add((new_r, new_c))
+        
+        # return -1
