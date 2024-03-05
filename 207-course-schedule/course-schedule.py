feace5619bd9eea6cf16e6523fd06f051e08eef5
Justine George <justinegeo96@gmail.com>
2024-03-04 20:15:56 -0600

    Time: 81 ms (94.24%) | Memory: 18.4 MB (49.66%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # prereq = [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]]
+        # [0, 1] means do 1 then 0
+        # 0: 1, 2 ie. 1 and 2 are prereqs of 0
+        # 1: 3, 4
+        # 2: 
+        # 3: 4
+        # 4: 
+        # prereq_map = collections.defaultdict(list)
+        prereq_map = { i: [] for i in range(numCourses) }
+        for course, prereq in prerequisites:
+            prereq_map[course].append(prereq)
+
+        # stores courses along the curr DFS path
+        visitSet = set()
+        def dfs(course):
+            if course in visitSet: return False
+            if prereq_map[course] == []: return True
+            
+            visitSet.add(course)
+            for prereq in prereq_map[course]:
+                if not dfs(prereq): return False
+            
+            visitSet.remove(course)
+            prereq_map[course] = []
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
+        
