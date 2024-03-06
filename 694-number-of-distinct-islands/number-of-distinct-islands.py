0dfcacdebe6a763cccd8eec4884a1ce0cd2782c9
Justine George <justinegeo96@gmail.com>
2024-03-06 02:06:32 -0600

    Time: 148 ms (79.29%) | Memory: 17.3 MB (67.25%) - LeetSync

--- 694-number-of-distinct-islands/number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/number-of-distinct-islands.py
@@ -3,7 +3,7 @@ class Solution:
         m, n = len(grid), len(grid[0])
         def getShape(grid, r, c, char):
             if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == 0:
-                return "."
+                return ""
             
             grid[r][c] = 0
 
@@ -12,7 +12,7 @@ class Solution:
             directions = [(1, 0, 'd'), (-1, 0, 'u'), (0, 1, 'r'), (0, -1, 'l')]
             for dr, dc, direc in directions:
                 res += getShape(grid, r + dr, c + dc, direc)
-
+            res += '.'
             return res
 
         shapes = set()
