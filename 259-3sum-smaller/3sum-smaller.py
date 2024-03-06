8a532dbdb89f15935158de3d9501916013bd06e9
Justine George <justinegeo96@gmail.com>
2024-03-05 19:10:49 -0600

    Time: 616 ms (41.27%) | Memory: 16.8 MB (43.64%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.py
+++ 259-3sum-smaller/3sum-smaller.py
@@ -6,18 +6,16 @@ class Solution:
         count = 0
         i = 0
         while i < n - 2:
-            # while 0 < i < n - 2 and nums[i] == nums[i - 1]:
-            #     i += 1
-
             j = i + 1
             k = n - 1
             
             while j < k:
                 cur_sum = nums[i] + nums[j] + nums[k]
                 
-                if cur_sum < target:  # If the current sum is less than the target, all elements from j to k are valid
-                    count += k - j  # Add all possible third numbers with the current first and second numbers
-                    j += 1  # Move the second number pointer forward
+                if cur_sum < target:
+                    # no need to k -= 1, since numbers below nums[k] and above nums[j] will be less than nums[k], thus satisfying this condition, so k - j numbers.
+                    count += k - j
+                    j += 1
                 else:
                     k -= 1
         
