64d1be2ec6faa97aab6c22886502e7fca441185f
Justine George <justinegeo96@gmail.com>
2024-03-05 22:35:29 -0600

    Time: 104 ms (76.69%) | Memory: 17.3 MB (55.92%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         maxArea = 0
-        self.visited = set()
+        # self.visited = set()
         self.m, self.n = len(grid), len(grid[0])
         for i in range(self.m):
             for j in range(self.n):
@@ -10,8 +10,8 @@ class Solution:
         return maxArea
 
     def dfs(self, grid, i, j):
-        if (i, j) in self.visited:
-            return 0
+        # if (i, j) in self.visited:
+        #     return 0
         
         if i < 0 or i >= self.m or j < 0 or j >= self.n:
             return 0
@@ -19,8 +19,13 @@ class Solution:
         if grid[i][j] == 0:
             return 0
         
-        # # grid is 1, mark as visited
-        # grid[i][j] = 0
-        self.visited.add((i, j))
+        # grid is 1, mark as visited
+        # self.visited.add((i, j))
+        grid[i][j] = 0
 
-        return 1 + self.dfs(grid, i + 1, j) + self.dfs(grid, i - 1, j) + self.dfs(grid, i, j + 1) + self.dfs(grid, i, j - 1)
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        area = 1
+        for dr, dc in directions:
+            area += self.dfs(grid, i + dr, j + dc)
+        return area
+        # return 1 + self.dfs(grid, i + 1, j) + self.dfs(grid, i - 1, j) + self.dfs(grid, i, j + 1) + self.dfs(grid, i, j - 1)
