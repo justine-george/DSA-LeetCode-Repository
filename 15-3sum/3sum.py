49bc5ab95eae21b04c9435d2701644522f607cf6
Justine George <justinegeo96@gmail.com>
2024-03-05 18:32:19 -0600

    Time: 750 ms (43.50%) | Memory: 20.6 MB (82.73%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -20,18 +20,13 @@ class Solution:
                     while j < k and nums[j] == nums[j - 1]:
                         j +=1
                     
-                    k -= 1
-                    while k >= j and nums[k] == nums[k + 1]:
-                        k -=1
+                    # k -= 1
+                    # while k >= j and nums[]k
 
                 elif cur_sum > 0:
                     k -= 1
-                    while k >= j and nums[k] == nums[k + 1]:
-                        k -=1
                 else:
                     j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j +=1
             
             i += 1
         
