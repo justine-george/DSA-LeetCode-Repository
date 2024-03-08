018222727a3671be00daa9d4df74ec39d113a9be
Justine George <justinegeo96@gmail.com>
2024-03-08 01:58:53 -0600

    Time: 37 ms (98.58%) | Memory: 16.5 MB (82.95%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -7,17 +7,20 @@ class Solution:
         
         def backtrack(index, arr, total):
             if total == target:
-                res.append(arr)
+                res.append(arr.copy())
                 return
             
             for i in range(index, n):
                 # skip duplicates
                 if i > index and candidates[i] == candidates[i - 1]:
                     continue
+                # early stop since the array is sorted
                 if candidates[i] + total > target:
                     break
-            
-                backtrack(i + 1, arr + [candidates[i]], total + candidates[i])
+
+                arr.append(candidates[i])
+                backtrack(i + 1, arr, total + candidates[i])
+                arr.pop()
             
         backtrack(0, [], 0)
         return res
