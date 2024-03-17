faf6a3867b1c9347fe1c8d4bbfd3642245a6afda
Justine George <justinegeo96@gmail.com>
2024-03-17 09:48:43 -0500

    Time: 64 ms (57.12%) | Memory: 16.7 MB (80.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,34 +2,33 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
 
-        s1_count, s2_count = [0] * 26, [0] * 26
+        s1_count = {chr(ord('a') + i): 0 for i in range(26)}
+        s2_count = {chr(ord('a') + i): 0 for i in range(26)}
 
         # first len(s1) characters of s1 and s2, fill the dict
         for i in range(len(s1)):
-            s1_count[ord(s1[i]) - ord('a')] += 1
-            s2_count[ord(s2[i]) - ord('a')] += 1
+            s1_count[s1[i]] += 1
+            s2_count[s2[i]] += 1
         
         matches = 0
-        for i in range(26):
-            matches += (1 if s1_count[i] == s2_count[i] else 0)
+        for key in s1_count.keys():
+            matches += (1 if s1_count[key] == s2_count[key] else 0)
 
         l = 0
         for r in range(len(s1), len(s2)):
             if matches == 26: return True
 
-            added_index = ord(s2[r]) - ord('a')
-            s2_count[added_index] += 1
+            s2_count[s2[r]] += 1
             # now equal
-            if s1_count[added_index] == s2_count[added_index]: matches += 1
+            if s1_count[s2[r]] == s2_count[s2[r]]: matches += 1
             # were equal
-            elif s1_count[added_index] + 1 == s2_count[added_index]: matches -= 1
+            elif s1_count[s2[r]] + 1 == s2_count[s2[r]]: matches -= 1
 
-            removed_index = ord(s2[l]) - ord('a')
-            s2_count[removed_index] -= 1
+            s2_count[s2[l]] -= 1
             # now equal
-            if s1_count[removed_index] == s2_count[removed_index]: matches += 1
+            if s1_count[s2[l]] == s2_count[s2[l]]: matches += 1
             # were equal
-            elif s1_count[removed_index] - 1 == s2_count[removed_index]: matches -= 1
+            elif s1_count[s2[l]] - 1 == s2_count[s2[l]]: matches -= 1
             l += 1
         
         return matches == 26
