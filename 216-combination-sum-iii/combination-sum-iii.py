3de73b177f1486e5454a623988fb523f6895c4a8
Justine George <justinegeo96@gmail.com>
2024-03-17 01:14:54 -0500

    Time: 31 ms (84.52%) | Memory: 16.4 MB (79.81%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,7 +1,5 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        # candidates = [i for i in range(1, 10)]
-
         res = []
 
         def dfs(cur_val, cur_arr, k_till_now, total):
