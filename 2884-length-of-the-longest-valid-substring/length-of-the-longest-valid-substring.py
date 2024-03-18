28e1a3745a54dc2b2d0a927bd4c01d7b08f99516
Justine George <justinegeo96@gmail.com>
2024-03-18 02:40:12 -0500

    Time: 1302 ms (58.93%) | Memory: 36.2 MB (35.89%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.py
@@ -2,13 +2,15 @@ class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
         invalid = set(forbidden)
         max_len = 0
-
         i = r_wall = len(word) - 1
+
         while i >= 0:
+
             for j in range(i, min(r_wall, i + 9) + 1):
                 if word[i:j + 1] in invalid:
                     r_wall = j - 1
                     break
+                    
             max_len = max(max_len, r_wall - i + 1)
             i -= 1
         
