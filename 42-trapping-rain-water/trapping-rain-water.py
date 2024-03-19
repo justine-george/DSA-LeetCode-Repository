82bdcdba8a15bab3adf8e299d079c299b3a69c4b
Justine George <justinegeo96@gmail.com>
2024-03-19 11:26:46 -0500

    Time: 115 ms (23.85%) | Memory: 18.5 MB (40.20%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,48 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        tallest_to_left = [0] * n
-        tallest_till_now = height[0]
-        for i in range(1, n):
-            tallest_till_now = max(tallest_till_now, height[i - 1])
-            tallest_to_left[i] = tallest_till_now
         
-        tallest_to_right = [0] * n
-        tallest_till_now = height[n - 1]
-        for i in range(n - 2, -1, -1):
-            tallest_till_now = max(tallest_till_now, height[i + 1])
-            tallest_to_right[i] = tallest_till_now
-        
-        res = 0
-        for i in range(n):
-            amount_at_i = min(tallest_to_left[i], tallest_to_right[i]) - height[i]
-            res += (amount_at_i if amount_at_i > 0 else 0)
+        def get_trapped_water(height):
+            n = len(height)
+            tallest_to_left = [0] * n
+            tallest_till_now = height[0]
+            for i in range(1, n):
+                tallest_till_now = max(tallest_till_now, height[i - 1])
+                tallest_to_left[i] = tallest_till_now
+            
+            tallest_to_right = [0] * n
+            tallest_till_now = height[n - 1]
+            for i in range(n - 2, -1, -1):
+                tallest_till_now = max(tallest_till_now, height[i + 1])
+                tallest_to_right[i] = tallest_till_now
+            
+            res = 0
+            for i in range(n):
+                amount_at_i = min(tallest_to_left[i], tallest_to_right[i]) - height[i]
+                res += (amount_at_i if amount_at_i > 0 else 0)
 
-        return res
+            return res
+        
+        return get_trapped_water(height)
+
+        # Follow up qn: what if all 0 elevations are holes which leak water?
+        # Solution: Split input arr by 0 borders
+        # height_list = []
+        # temp = []
+        # for h in height:
+        #     if h == 0:
+        #         if temp:
+        #             height_list.append(temp)
+        #             temp = []
+        #     else:
+        #         temp.append(h)
+        
+        # if temp:
+        #     height_list.append(temp)
+        
+        # print(height_list)
+
+        # res = 0
+        # for height in height_list:
+        #     res += get_trapped_water(height)
+
+        # return res
