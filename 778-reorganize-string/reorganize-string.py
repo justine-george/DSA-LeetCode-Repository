60c178dc7876ef0b15037d63da95c60e3677bea1
Justine George <justinegeo96@gmail.com>
2024-03-19 01:35:02 -0500

    Time: 27 ms (97.89%) | Memory: 16.7 MB (34.21%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,26 +1,24 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # Count the frequency of each character
-        char_counts = Counter(s)
-        # Create a max heap from char counts
-        max_heap = [(-cnt, char) for char, cnt in char_counts.items()]
-        heapq.heapify(max_heap)
+        count = Counter(s)
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
+        heapq.heapify(maxHeap)
 
-        result = []
-        # Keep track of the last character added to the result to avoid consecutive duplicates
-        prev_cnt, prev_char = 0, ''
-        
-        while max_heap:
-            cnt, char = heapq.heappop(max_heap)
-            # Add the current character to the result
-            result.append(char)
-            # Since we are using a max heap, we add 1 (because counts are negative)
-            if prev_cnt < 0:
-                # If there's a previous character to add back, do it now
-                heapq.heappush(max_heap, (prev_cnt, prev_char))
+        prev = None
+        res = ""
+        while maxHeap or prev:
+            if not maxHeap and prev:
+                return ""
+
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
             
-            # Update prev_char and decrease its count for the next round
-            prev_cnt, prev_char = cnt + 1, char
+            if cnt != 0:
+                prev = [cnt, char]
         
-        # If the last character count is less than 0, we couldn't reorganize successfully
-        return ''.join(result) if len(result) == len(s) else ""
+        return res
