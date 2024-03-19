90a8c5a69e5fadee64606fb339aa5995b33a00b2
Justine George <justinegeo96@gmail.com>
2024-03-19 12:46:19 -0500

    Time: 117 ms (20.44%) | Memory: 18.6 MB (18.28%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,49 +1,45 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # S: O(1) solution using 2 pointers
-        if not height: return 0
+        # # S: O(1) solution using 2 pointers
+        # if not height: return 0
 
-        n = len(height)
-        l, r = 0, n - 1
-        left_max, right_max = height[l], height[r]
-        res = 0
+        # n = len(height)
+        # l, r = 0, n - 1
+        # left_max, right_max = height[l], height[r]
+        # res = 0
 
-        while l < r:
-            if left_max < right_max:
-                l += 1
-                amount = left_max - height[l]
-                res += (amount if amount > 0 else 0)
-                left_max = max(left_max, height[l])
-            else:
-                r -= 1
-                amount = right_max - height[r]
-                res += (amount if amount > 0 else 0)
-                right_max = max(right_max, height[r])
+        # while l < r:
+        #     if left_max < right_max:
+        #         l += 1
+        #         amount = left_max - height[l]
+        #         res += (amount if amount > 0 else 0)
+        #         left_max = max(left_max, height[l])
+        #     else:
+        #         r -= 1
+        #         amount = right_max - height[r]
+        #         res += (amount if amount > 0 else 0)
+        #         right_max = max(right_max, height[r])
 
-        return res
+        # return res
 
-        # def get_trapped_water(height):
-        #     n = len(height)
-        #     tallest_to_left = [0] * n
-        #     tallest_till_now = height[0]
-        #     for i in range(1, n):
-        #         tallest_till_now = max(tallest_till_now, height[i - 1])
-        #         tallest_to_left[i] = tallest_till_now
+        def get_trapped_water(height):
+            n = len(height)
+            tallest_to_left, tallest_to_right = [0] * n, [0] * n
+
+            for i in range(1, n):
+                tallest_to_left[i] = max(tallest_to_left[i - 1], height[i - 1])
             
-        #     tallest_to_right = [0] * n
-        #     tallest_till_now = height[n - 1]
-        #     for i in range(n - 2, -1, -1):
-        #         tallest_till_now = max(tallest_till_now, height[i + 1])
-        #         tallest_to_right[i] = tallest_till_now
+            for i in range(n - 2, -1, -1):
+                tallest_to_right[i] = max(tallest_to_right[i + 1], height[i + 1])
             
-        #     res = 0
-        #     for i in range(n):
-        #         amount_at_i = min(tallest_to_left[i], tallest_to_right[i]) - height[i]
-        #         res += (amount_at_i if amount_at_i > 0 else 0)
+            res = 0
+            for i in range(n):
+                amount_at_i = min(tallest_to_left[i], tallest_to_right[i]) - height[i]
+                res += (amount_at_i if amount_at_i > 0 else 0)
 
-        #     return res
+            return res
         
-        # return get_trapped_water(height)
+        return get_trapped_water(height)
 
         # Follow up qn: what if all 0 elevations are holes which leak water?
         # Solution: Split input arr by 0 borders
