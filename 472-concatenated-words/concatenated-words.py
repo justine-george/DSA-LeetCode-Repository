d523b33570d9d846184089289342c12d1fc52ee1
Justine George <justinegeo96@gmail.com>
2024-03-19 17:47:14 -0500

    Time: 301 ms (81.74%) | Memory: 29.2 MB (42.86%) - LeetSync

--- 472-concatenated-words/concatenated-words.py
+++ 472-concatenated-words/concatenated-words.py
@@ -1,17 +1,18 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        words_set = set(words)
+        word_set = set(words)
 
         dp = {}
         def dfs(word):
             if word in dp:
                 return dp[word]
 
+            # go through all prefixes of this word
             for i in range(1, len(word)):
                 prefix = word[:i]
                 suffix = word[i:]
 
-                if (prefix in words_set and suffix in words_set) or (prefix in words_set and dfs(suffix)):
+                if (prefix in word_set and suffix in word_set) or (prefix in word_set and dfs(suffix)):
                     dp[word] = True
                     return True
 
@@ -19,7 +20,9 @@ class Solution:
             return False
 
         res = []
+
         for w in words:
             if dfs(w):
                 res.append(w)
+
         return res
