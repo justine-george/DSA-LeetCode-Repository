fdc577c8015c0b9a6d9c0eb7a01f1dbc384b878f
Justine George <justinegeo96@gmail.com>
2024-03-19 13:16:00 -0500

    Time: 364 ms (62.54%) | Memory: 30.2 MB (83.92%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -1,18 +1,12 @@
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
         n = len(nums)
-        largest_elem_index = 0
-        smallest_elem_index = 0
+        largest_elem_index, smallest_elem_index = 0, 0
         for i in range(1, n):
             if nums[i] >= nums[largest_elem_index]:
                 largest_elem_index = i
             if nums[i] < nums[smallest_elem_index]:
                 smallest_elem_index = i
-
-        # smallest_elem_index = n - 1
-        # for i in range(n - 2, -1, -1):
-        #     if nums[i] <= nums[smallest_elem_index]:
-        #         smallest_elem_index = i
 
         # 3 -> 5
         # 0 <- 5
