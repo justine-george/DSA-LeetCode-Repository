fa57a7ebfdf07ccf91b495a62ce7715f4e13476e
Justine George <justinegeo96@gmail.com>
2024-03-19 06:54:10 -0500

    Time: 59 ms (50.85%) | Memory: 17.7 MB (50.93%) - LeetSync

--- 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.py
+++ 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.py
@@ -1,19 +1,21 @@
 class Solution:
     def minimumKeypresses(self, s: str) -> int:
-        char_count = Counter(s)
+        # char_count = Counter(s)
 
-        res = 0
-        multiplier = 0
+        # res = 0
+        # multiplier = 0
 
-        for i, freq in enumerate(sorted(char_count.values(), reverse=True)):
-            # 0 -> 8   = 1 is the multiplier
-            # 9 -> 17  = 2 is the multiplier
-            # 18 -> 25 = 3 is the multiplier
+        # for i, freq in enumerate(sorted(char_count.values(), reverse=True)):
+        #     # 0 -> 8   = 1 is the multiplier
+        #     # 9 -> 17  = 2 is the multiplier
+        #     # 18 -> 25 = 3 is the multiplier
 
-            # time to change multiplier
-            if i % 9 == 0:
-                multiplier += 1
+        #     # time to change multiplier
+        #     if i % 9 == 0:
+        #         multiplier += 1
 
-            res += (freq * multiplier)
+        #     res += (freq * multiplier)
 
-        return res
+        # return res
+
+        return sum((i // 9 + 1) * freq for i, freq in enumerate(sorted(Counter(s).values(), reverse=True)))
