ba59039f21d04d947600d539a31761b01f9a06a9
Justine George <justinegeo96@gmail.com>
2024-03-18 20:57:48 -0500

    Time: 1887 ms (8.82%) | Memory: 55.8 MB (59.98%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.py
+++ 2165-plates-between-candles/plates-between-candles.py
@@ -1,33 +1,41 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        # Helper function to find the index of the leftmost candle to the right of a given position
         def get_left_candle_boundary(start, candle_pos):
+            # get left boundary
+            # ie. find left such that start <= left
+
             l, r = 0, len(candle_pos) - 1
             while l <= r:
-                m = (l + r) // 2
-                if candle_pos[m] < start:
+                m = (r + l) // 2
+                if candle_pos[m] == start:
+                    return m
+                elif candle_pos[m] < start:
                     l = m + 1
                 else:
                     r = m - 1
-            return l if l < len(candle_pos) else -1
-
-        # Helper function to find the index of the rightmost candle to the left of a given position
+            return l
+        
         def get_right_candle_boundary(end, candle_pos):
+            # get right boundary
+            # ie. find right such that right <= end
+
             l, r = 0, len(candle_pos) - 1
             while l <= r:
-                m = (l + r) // 2
-                if candle_pos[m] <= end:
+                m = (r + l) // 2
+                if candle_pos[m] == end:
+                    return m
+                elif candle_pos[m] < end:
                     l = m + 1
                 else:
                     r = m - 1
-            return r if r >= 0 else -1
+            return r
 
-        # Calculate prefix count of plates
+        # calculate prefix count of plates
         pre = [0] * (len(s) + 1)
         for i in range(1, len(s) + 1):
             pre[i] = pre[i - 1] + (1 if s[i - 1] == '*' else 0)
 
-        # Make a list of candle positions
+        # make a sorted list of candle positions to enable binary search
         candle_pos = [i for i, c in enumerate(s) if c == '|']
 
         res = []
@@ -37,6 +45,6 @@ class Solution:
             if left_index == -1 or right_index == -1 or left_index >= right_index:
                 res.append(0)
             else:
-                # Calculate the number of plates between the leftmost and rightmost candles in the query range
                 res.append(pre[candle_pos[right_index] + 1] - pre[candle_pos[left_index]])
+        
         return res
