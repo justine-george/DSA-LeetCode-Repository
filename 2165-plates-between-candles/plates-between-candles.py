ff65f141ae127703fa4d56a40e14340927cf5e32
Justine George <justinegeo96@gmail.com>
2024-03-18 23:22:31 -0500

    Time: 1453 ms (92.52%) | Memory: 55.7 MB (68.97%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.py
+++ 2165-plates-between-candles/plates-between-candles.py
@@ -23,3 +23,49 @@ class Solution:
             res.append(0 if left >= right else plate_count[right] - plate_count[left])
 
         return res
+
+
+        # def get_left_candle_boundary(start, candle_pos):
+        #     # get left boundary
+        #     # ie. find left such that start <= left
+
+        #     l, r = 0, len(candle_pos) - 1
+        #     while l <= r:
+        #         m = (r + l) // 2
+        #         if candle_pos[m] < start:
+        #             l = m + 1
+        #         else:
+        #             r = m - 1
+        #     return l if l < len(candle_pos) else -1
+        
+        # def get_right_candle_boundary(end, candle_pos):
+        #     # get right boundary
+        #     # ie. find right such that right <= end
+
+        #     l, r = 0, len(candle_pos) - 1
+        #     while l <= r:
+        #         m = (r + l) // 2
+        #         if candle_pos[m] <= end:
+        #             l = m + 1
+        #         else:
+        #             r = m - 1
+        #     return r if r >= 0 else -1
+
+        # # calculate prefix count of plates
+        # pre = [0] * (len(s) + 1)
+        # for i in range(1, len(s) + 1):
+        #     pre[i] = pre[i - 1] + (1 if s[i - 1] == '*' else 0)
+
+        # # make a sorted list of candle positions to enable binary search
+        # candle_pos = [i for i, c in enumerate(s) if c == '|']
+
+        # res = []
+        # for start, end in queries:
+        #     left_index = get_left_candle_boundary(start, candle_pos)
+        #     right_index = get_right_candle_boundary(end, candle_pos)
+        #     if left_index == -1 or right_index == -1 or left_index >= right_index:
+        #         res.append(0)
+        #     else:
+        #         res.append(pre[candle_pos[right_index] + 1] - pre[candle_pos[left_index]])
+        
+        # return res
