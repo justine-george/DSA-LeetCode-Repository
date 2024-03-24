25dcd1675c08c781c317f2865fbddfc671109688
Justine George <justinegeo96@gmail.com>
2024-03-24 00:57:02 -0500

    Time: 194 ms (55.13%) | Memory: 23.2 MB (40.12%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,38 +1,28 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        #    4,   10,  15,        24,26
-        # 0,    9,   12,    20
-        #     5,         18,   22,     30
+        pq = []
 
-        # sliding window of size k, then bump up min and max (min using priority queue), calculate min(range) and ranges themselves
-
-        pq = [] # (small number, list index, nums index)
-        n = len(nums)
+        k = len(nums)
         max_val = float('-inf')
-        
-        # populate initial state
-        for i in range(n):
-            heapq.heappush(pq, (nums[i][0], i, 0)) # first value from k lists
+
+        for i in range(k):
+            heapq.heappush(pq, (nums[i][0], i, 0))
             max_val = max(max_val, nums[i][0])
-        
-        # initialize ans with first interval
-        ans = [pq[0][0], max_val]
+
+        res = [pq[0][0], max_val]
 
         while True:
-            _, list_index, num_index = heapq.heappop(pq)
+            value, list_index, num_index = heapq.heappop(pq)
 
-            # if current smallest number is the last item in its list
             if num_index == len(nums[list_index]) - 1:
                 break
-            
-            next_number = nums[list_index][num_index + 1]
-            max_val = max(max_val, next_number)
-            heapq.heappush(pq, (next_number, list_index, num_index + 1))
 
-            if max_val - pq[0][0] < ans[1] - ans[0]:
-                ans = [pq[0][0], max_val]
+            next_num = nums[list_index][num_index + 1]
+            heapq.heappush(pq, (next_num, list_index, num_index + 1))
+            max_val = max(max_val, next_num)
 
-        return ans
-            
+            if max_val - pq[0][0] < res[1] - res[0]:
+                res = [pq[0][0], max_val]
+        
 
-
+        return res
