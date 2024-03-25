6d16c90380c4ba77ae2db3d91c7d06a9de9e47ee
Justine George <justinegeo96@gmail.com>
2024-03-25 00:25:18 -0500

    Time: 201 ms (39.21%) | Memory: 23.2 MB (40.93%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,28 +1,26 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         pq = []
-
-        k = len(nums)
         max_val = float('-inf')
 
-        for i in range(k):
+        for i in range(len(nums)):
             heapq.heappush(pq, (nums[i][0], i, 0))
             max_val = max(max_val, nums[i][0])
-
-        res = [pq[0][0], max_val]
+        
+        ans = [pq[0][0], max_val]
 
         while True:
-            value, list_index, num_index = heapq.heappop(pq)
+            val, list_index, num_index = heapq.heappop(pq)
 
             if num_index == len(nums[list_index]) - 1:
                 break
-
+            
             next_num = nums[list_index][num_index + 1]
             heapq.heappush(pq, (next_num, list_index, num_index + 1))
+
             max_val = max(max_val, next_num)
-
-            if max_val - pq[0][0] < res[1] - res[0]:
-                res = [pq[0][0], max_val]
+            
+            if ans[1] - ans[0] > max_val - pq[0][0]:
+                ans = [pq[0][0], max_val]
         
-
-        return res
+        return ans
