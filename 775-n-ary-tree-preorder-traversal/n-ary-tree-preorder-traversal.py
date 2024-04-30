c73f76e5b3abf54cb884055a86a0695f7163f270
Justine George <justinegeo96@gmail.com>
2024-04-29 21:21:33 -0500

    Time: 36 ms (97.01%) | Memory: 18.1 MB (94.26%) - LeetSync

--- 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -8,16 +8,29 @@ class Node:
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
-        if not root:
-            return []
+        # if not root:
+        #     return []
 
+        # res = []
+
+        # def preorder_dfs(root):
+        #     res.append(root.val)
+        #     for child in root.children:
+        #         if child:
+        #             preorder_dfs(child)
+
+        # preorder_dfs(root)
+        # return res
+
+        stack = [root] if root else None
         res = []
+        while stack:
+            node = stack.pop()
 
-        def preorder_dfs(root):
-            res.append(root.val)
-            for child in root.children:
+            res.append(node.val)
+
+            for child in node.children[::-1]:
                 if child:
-                    preorder_dfs(child)
+                    stack.append(child)
 
-        preorder_dfs(root)
         return res
