0c50644ad84315895abdd2ad281ea3fea6908097
Justine George <justinegeo96@gmail.com>
2024-05-02 18:55:21 -0500

    Time: 123 ms (80.98%) | Memory: 31.5 MB (27.24%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,45 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        '''
-        iterative/recursive dfs
 
-        for each dfs(root, max_so_far) iteration:
-            if node.val >= max_so_far, increment count and update max_so_far
-            increment counts from dfs(left, newmax) and dfs(right, newmax)
-            return count
-
-        return dfs(root, initial = float('-inf'))
-        '''
-
-        # def dfs(root, max_so_far):
-        #     if not root:
-        #         return 0
-            
-        #     count = 0
-        #     if root.val >= max_so_far:
-        #         max_so_far = root.val
-        #         count += 1
-            
-        #     count += dfs(root.left, max_so_far)
-        #     count += dfs(root.right, max_so_far)
-        #     return count
-        
-        # return dfs(root, float('-inf'))
-
-        stack = [(root, float('-inf'))]
-        count = 0
-        while stack:
-            node, max_so_far = stack.pop()
-
+        def dfs(node, max_so_far):
             if not node:
-                continue
+                return 0
             
+            count = 0
             if node.val >= max_so_far:
-                count += 1
                 max_so_far = node.val
-            
-            stack.append((node.left, max_so_far))
-            stack.append((node.right, max_so_far))
+                count += 1
 
-        return count
+            count += dfs(node.left, max_so_far)
+            count += dfs(node.right, max_so_far)
+
+            return count
+        
+
+        return dfs(root, float('-inf'))
