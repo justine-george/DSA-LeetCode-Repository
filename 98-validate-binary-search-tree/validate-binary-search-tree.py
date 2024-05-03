12e66561413b173a411fcc3555b44b06ffec8e6a
Justine George <justinegeo96@gmail.com>
2024-05-02 20:30:56 -0500

    Time: 29 ms (98.54%) | Memory: 18.3 MB (83.14%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -10,47 +10,47 @@ class Solution:
         '''
         observation: inorder traversal (left, root, right) of a BST is always a strictly-increasing array.
         '''
-        # prev_node = None
-
-        # # Inorder traversal
-        # def inorder(node):
-        #     nonlocal prev_node
-            
-        #     if not node:
-        #         return True
-            
-        #     # left
-        #     if not inorder(node.left):
-        #         return False
-
-        #     # current
-        #     if prev_node and prev_node.val >= node.val:
-        #         return False
-        #     prev_node = node
-
-        #     # right
-        #     if not inorder(node.right):
-        #         return False
-
-        #     return True
-
-        # return inorder(root)
-
-        st = []
-        node = root
         prev_node = None
-        while st or node:
-            # reach leftmost node
-            while node:
-                st.append(node)
-                node = node.left
 
-            node = st.pop()
+        # Inorder traversal - recursive
+        def inorder(node):
+            nonlocal prev_node
             
+            if not node:
+                return True
+            
+            # left
+            if not inorder(node.left):
+                return False
+
+            # current
             if prev_node and prev_node.val >= node.val:
                 return False
             prev_node = node
 
-            node = node.right
+            # right
+            if not inorder(node.right):
+                return False
 
-        return True
+            return True
+
+        return inorder(root)
+
+        # st = []
+        # node = root
+        # prev_node = None
+        # while st or node:
+        #     # reach leftmost node
+        #     while node:
+        #         st.append(node)
+        #         node = node.left
+
+        #     node = st.pop()
+            
+        #     if prev_node and prev_node.val >= node.val:
+        #         return False
+        #     prev_node = node
+
+        #     node = node.right
+
+        # return True
