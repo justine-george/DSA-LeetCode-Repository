623156d977327fe5f7e4f36a0904c6dd1dadeb73
Justine George <justinegeo96@gmail.com>
2024-05-02 19:57:39 -0500

    Time: 39 ms (73.56%) | Memory: 18.3 MB (48.53%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        '''
+        observation: inorder traversal (left, root, right) of a BST is always a strictly-increasing array.
+        '''
+
+        prev_node = None
+        def inorder(node):
+            if not node:
+                return True
+            
+            if not inorder(node.left):
+                return False
+            nonlocal prev_node
+            if prev_node and prev_node.val >= node.val:
+                return False
+            prev_node = node
+            
+            return inorder(node.right)
+
+        return inorder(root)
+
+
+        # st = [(root)] if root else None
+        # prev_node = None
+        # while st:
+        #     node = st.pop()
+
+        #     if not node:
+        #         continue
+
+        #     st.append(node.left)
+
+        #     if prev_node and prev_node.val >= node.val:
+        #         return False
+            
+        #     st.append(node.right)
+
+        #     prev_node = node
+
+        # return True
