0694bc546b1dfe1171c1a420c61dbd9dcb6ec25d
Justine George <justinegeo96@gmail.com>
2024-05-29 22:25:02 -0500

    Time: 34 ms (90.98%) | Memory: 16.5 MB (74.14%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -5,4 +5,4 @@ class Comparator(str):
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         largest_num = "".join(sorted(map(str, nums), key=Comparator, reverse=True))
-        return "0" if largest_num[0] == "0" else largest_num 
+        return "0" if largest_num[0] == "0" else largest_num
