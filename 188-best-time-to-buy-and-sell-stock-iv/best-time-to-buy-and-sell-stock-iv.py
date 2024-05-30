d87334a645ed72fea2aca993427c4f21e6b3fc9e
Justine George <justinegeo96@gmail.com>
2024-05-30 09:06:01 -0500

    Time: 320 ms (5.89%) | Memory: 80.2 MB (18.45%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        # return max profit possible starting at index i
+        cache = defaultdict(int)
+        def dp(i, k, isHolding):
+            if i == len(prices) or k == 0:
+                return 0
+            
+            if (i, k, isHolding) in cache:
+                return cache[(i, k, isHolding)]
+            
+            # do nothing
+            profit_doNothing = dp(i + 1, k, isHolding)
+
+            # do something
+            profit_doSomething = 0
+            if isHolding:
+                profit_doSomething = prices[i] + dp(i + 1, k - 1, False)
+            else:
+                profit_doSomething = -prices[i] + dp(i + 1, k, True)
+            
+            cache[(i, k, isHolding)] = max(profit_doNothing, profit_doSomething)
+            return cache[(i, k, isHolding)]
+
+        return dp(0, k, False)
