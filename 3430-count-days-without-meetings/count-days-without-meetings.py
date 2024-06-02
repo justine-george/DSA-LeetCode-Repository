69b5cf99382d7200b51a205c20238f74a636d474
Justine George <justinegeo96@gmail.com>
2024-06-02 08:18:38 -0500

    Time: 1261 ms (100.00%) | Memory: 66.7 MB (100.00%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -1,26 +1,22 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         # merge intervals first
-
         meetings.sort(key=lambda x: x[0])
-        
-        intervals = [meetings[0]]
-        
+        new_list = [meetings[0]]
         for cur_start, cur_end in meetings:
-            prev_start,prev_end = intervals[-1][0], intervals[-1][1]
+            prev_start,prev_end = new_list[-1][0], new_list[-1][1]
 
-            # cur comes after (no overlap)
+            # current interval comes after prev interval
             if cur_start > prev_end:
-                intervals.append([cur_start, cur_end])
-
+                new_list.append([cur_start, cur_end])
             # overlap
             else:
                 new_end = max(cur_end, prev_end)
-                intervals[-1][1] = new_end
+                new_list[-1][1] = new_end
 
         # iterate over the new non overlapping intervals
         res = days
-        for start, end in intervals:
+        for start, end in new_list:
             res -= (end - start + 1)
         
         return res
