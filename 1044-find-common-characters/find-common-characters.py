81a7ed1aad35b9124691b2016f6e6e749fbd2f49
Justine George <justinegeo96@gmail.com>
2024-06-05 16:13:42 -0500

    Time: 54 ms (32.24%) | Memory: 16.6 MB (93.58%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -1,13 +1,8 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        
-        
         prevWord = words[0]
         for i in range(1, len(words)):
             res = []
-            print(prevWord)
-            print(words[i])
-            print("--")
             count_prev = Counter(prevWord)
             count_cur = Counter(words[i])
 
