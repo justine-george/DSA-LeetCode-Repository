5dfbc3f51bb7a73b37cfaef0cfcea37f0f9be6df
Justine George <justinegeo96@gmail.com>
2024-06-08 18:15:34 -0500

    Time: 1024 ms (5.05%) | Memory: 36.2 MB (64.77%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if k < 1:
+            return False
+        
         cum_sum_map = {0: -1}
 
         s = 0
         for i, n in enumerate(nums):
-            s += n
-            if k != 0:
-                s = s % k
+            s = (s + n) % k
 
             if s in cum_sum_map:
                 if i - cum_sum_map[s] > 1:
