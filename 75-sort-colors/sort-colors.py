927edcb9d1e0e922a23581e54f16e384548d6ab8
Justine George <justinegeo96@gmail.com>
2024-06-11 19:55:09 -0500

    Time: 37 ms (57.63%) | Memory: 16.7 MB (7.83%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -8,24 +8,26 @@ class Solution:
         # b 2
 
         # # counting sort since we know max range
-        # count_map = Counter(nums)
-        # order = [0, 1, 2]
-        # i = 0
-        # for o in order:
-        #     while count_map[o] > 0:
-        #         nums[i] = o
-        #         i += 1
-        #         count_map[o] -= 1
+        count_map = [0] * 3
+        for n in nums:
+            count_map[n] += 1
+        order = [0, 1, 2]
+        i = 0
+        for o in order:
+            while count_map[o] > 0:
+                nums[i] = o
+                i += 1
+                count_map[o] -= 1
 
-        # single pass and S: O(1) - dutch flag sort
-        zero, it, two = 0, 0, len(nums) - 1
-        while it <= two:
-            if nums[it] == 0:
-                nums[it], nums[zero] = nums[zero], nums[it]
-                it += 1
-                zero += 1
-            elif nums[it] == 2:
-                nums[it], nums[two] = nums[two], nums[it]
-                two -= 1
-            else:
-                it += 1
+        # single pass T: O(n) and S: O(1) - dutch flag sort
+        # zero, it, two = 0, 0, len(nums) - 1
+        # while it <= two:
+        #     if nums[it] == 0:
+        #         nums[it], nums[zero] = nums[zero], nums[it]
+        #         it += 1
+        #         zero += 1
+        #     elif nums[it] == 2:
+        #         nums[it], nums[two] = nums[two], nums[it]
+        #         two -= 1
+        #     else:
+        #         it += 1
