eadb1fbc3b4f3a6882b33d647aa10e97685801c5
Justine George <justinegeo96@gmail.com>
2024-06-11 19:46:57 -0500

    Time: 39 ms (45.53%) | Memory: 16.5 MB (79.02%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -7,26 +7,25 @@ class Solution:
         # w 1
         # b 2
 
-        # counting sort since we know max range
-        count_map = Counter(nums)
-        
-        order = [0, 1, 2]
-        i = 0
-        for o in order:
-            while count_map[o] > 0:
-                nums[i] = o
-                i += 1
-                count_map[o] -= 1
+        # # counting sort since we know max range
+        # count_map = Counter(nums)
+        # order = [0, 1, 2]
+        # i = 0
+        # for o in order:
+        #     while count_map[o] > 0:
+        #         nums[i] = o
+        #         i += 1
+        #         count_map[o] -= 1
 
         # single pass and S: O(1) - dutch flag sort
-        # zero, it, two = 0, 0, len(nums) - 1
-        # while it <= two:
-        #     if nums[it] == 0:
-        #         nums[it], nums[zero] = nums[zero], nums[it]
-        #         it += 1
-        #         zero += 1
-        #     elif nums[it] == 2:
-        #         nums[it], nums[two] = nums[two], nums[it]
-        #         two -= 1
-        #     else:
-        #         it += 1
+        zero, it, two = 0, 0, len(nums) - 1
+        while it <= two:
+            if nums[it] == 0:
+                nums[it], nums[zero] = nums[zero], nums[it]
+                it += 1
+                zero += 1
+            elif nums[it] == 2:
+                nums[it], nums[two] = nums[two], nums[it]
+                two -= 1
+            else:
+                it += 1
