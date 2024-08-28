46e1fc3e7bc1be6cb6b2ea52eba98dea1b056ef7
Justine George <justinegeo96@gmail.com>
2024-08-27 23:48:37 -0700

    Time: 0 ms (100.00%) | Memory: 12 MB (94.19%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        string res = "";
-        for (int i = 0; i < size(nums[0]); i++) {
-            if (nums[i][i] == '0') {
-                res += '1';
-            } else {
-                res += '0';
-            }
+        string res;
+        for (int i = 0; i < nums[0].size(); i++) {
+            res += (nums[i][i] == '0') ? '1' : '0';
         }
         return res;
     }
