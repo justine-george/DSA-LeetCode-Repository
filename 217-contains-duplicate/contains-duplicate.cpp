ddd017ee6d7e670ece379a8121d004cac4d7dee2
Justine George <justinegeo96@gmail.com>
2024-09-21 04:52:50 -0500

    Time: 97 ms (44.71%) | Memory: 73.3 MB (70.85%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> visited;
-        for (const auto& num: nums) {
-            if (visited.count(num)) {
-                return true;
-            }
-            visited.insert(num);
+        // unordered_set<int> visited;
+        // for (const auto& num: nums) {
+        //     if (visited.count(num)) {
+        //         return true;
+        //     }
+        //     visited.insert(num);
+        // }
+        // return false;
+
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) return true;
+
+            s.insert(nums[i]);
         }
+
         return false;
 
         // sort(nums.begin(), nums.end());
