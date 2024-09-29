ec0523411cb5e4e8c04f92ee4250db75e374fe44
Justine George <justinegeo96@gmail.com>
2024-09-28 23:19:08 -0700

    Time: 31 ms (80.07%) | Memory: 16.9 MB (5.22%) - LeetSync

--- 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.py
@@ -3,40 +3,30 @@ class Solution:
         if numerator == 0:
             return "0"
 
-        res = ""
+        res = []
         if numerator * denominator < 0:
-            res += '-'
+            res.append('-')
         
         numerator = abs(numerator)
         denominator = abs(denominator)
 
-        res += str(numerator // denominator)
-        if numerator % denominator == 0:
-            return res
+        res.append(str(numerator // denominator))
+        remainder = numerator % denominator
+        if remainder == 0:
+            return ''.join(res)
 
-        res += "."
+        res.append('.')
 
-        found_num_quotients = []
-        while True:
-            remainder = numerator % denominator
+        remainder_map = {}
+        while remainder != 0:
+            if remainder in remainder_map:
+                res.insert(remainder_map[remainder], '(')
+                res.append(')')
+                break
 
-            if remainder == 0:
-                for num_quotient in found_num_quotients:
-                    res += str(num_quotient[1])
-                return res
-
-            numerator = remainder * 10
-            new_quotient = numerator // denominator
-            
-            if [numerator, new_quotient] not in found_num_quotients:
-                found_num_quotients.append([numerator, new_quotient])
-            else:
-                index_found = found_num_quotients.index([numerator, new_quotient])
-                for num_quotient in found_num_quotients[:index_found]:
-                    res += str(num_quotient[1])
-
-                res += "("
-                for num_quotient in found_num_quotients[index_found:]:
-                    res += str(num_quotient[1])
-                res += ")"
-                return res
+            remainder_map[remainder] = len(res)
+            remainder *= 10
+            res.append(str(remainder // denominator))
+            remainder %= denominator
+        
+        return ''.join(res)
