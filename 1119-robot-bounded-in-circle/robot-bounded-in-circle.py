ac3559e938abd6ef3eebe0fc01b8e318736cfe2c
Justine George <justinegeo96@gmail.com>
2024-09-30 00:08:22 -0700

    Time: 34 ms (63.87%) | Memory: 16.6 MB (14.17%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -1,23 +1,28 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        x, y = 0, 0
-        # 0: N
-        # 1: E
-        # 2: S
-        # 3: W
-        direction = 0
-        # corresponding to N E S W
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        '''
+        direction = 0, 1, 2, 3: N, E, S, W
+        '''
         
-        # simulate one instruction set
+        x, y, direction = 0, 0, 0
+        directions = [
+            (0,1),
+            (1,0),
+            (0,-1),
+            (-1,0)
+        ]
+
         for i in instructions:
             match i:
                 case 'G':
                     x += directions[direction][0]
                     y += directions[direction][1]
+
                 case 'L':
                     direction = (direction - 1) % 4
+
                 case 'R':
                     direction = (direction + 1) % 4
 
-        return (x == 0 and y == 0) or direction != 0
+        
+        return x == y == 0 or direction != 0
