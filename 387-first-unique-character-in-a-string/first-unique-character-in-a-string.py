6d906f411721d7608a3e98c52a4b56c40551f8ee
Justine George <justinegeo96@gmail.com>
2024-09-30 00:37:08 -0700

    Time: 146 ms (10.30%) | Memory: 17.3 MB (13.47%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # char: latest_index, freq
+        map = {}
+        for i, c in enumerate(s):
+            if c not in map:
+                map[c] = [i, 0]
+            map[c] = [i, map[c][1] + 1]
+        
+        for char, [latest_index, freq] in map.items():
+            if freq == 1:
+                return latest_index
+
+        return -1
