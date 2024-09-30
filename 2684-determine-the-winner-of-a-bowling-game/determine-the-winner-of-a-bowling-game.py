4007f98c4780d59806e122e71d7cfd3d53397ac4
Justine George <justinegeo96@gmail.com>
2024-09-30 02:28:29 -0700

    Time: 215 ms (33.48%) | Memory: 16.8 MB (16.74%) - LeetSync

--- 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.py
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.py
@@ -2,18 +2,19 @@ class Solution:
     def isWinner(self, player1: List[int], player2: List[int]) -> int:
         def get_score(scores):
             total_score = 0
-
             for i, score in enumerate(scores):
                 if (i >= 2 and (scores[i - 2] == 10 or scores[i - 1] == 10)) or (i == 1 and scores[i - 1] == 10):
                     total_score += (2 * score)
                 else:
                     total_score += score
-
             return total_score
 
         player_1_score = get_score(player1)
         player_2_score = get_score(player2)
 
-        if player_1_score == player_2_score:
+        if player_1_score > player_2_score:
+            return 1
+        elif player_1_score < player_2_score:
+            return 2
+        else:
             return 0
-        return 1 if player_1_score > player_2_score else 2
