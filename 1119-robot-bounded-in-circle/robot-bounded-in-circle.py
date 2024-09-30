5f87fd414489eaeb49a13ce641290171bd1ac270
Justine George <justinegeo96@gmail.com>
2024-09-29 23:38:57 -0700

    Time: 34 ms (63.87%) | Memory: 16.5 MB (79.95%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -31,35 +31,44 @@ class Solution:
 
         coord = [0,0,'N']
 
+        def move_robot(coord):
+            if coord[2] == 'N':
+                coord[1] += 1
+            elif coord[2] == 'E':
+                coord[0] += 1
+            elif coord[2] == 'W':
+                coord[0] -= 1
+            else:
+                coord[1] -= 1
+            
+        def turn_robot(coord, turn_direction):
+            if turn_direction == 'L':
+                if coord[2] == 'N':
+                    coord[2] = 'W'
+                elif coord[2] == 'E':
+                    coord[2] = 'N'
+                elif coord[2] == 'W':
+                    coord[2] = 'S'
+                else:
+                    coord[2] = 'E'
+            elif turn_direction == 'R':
+                if coord[2] == 'N':
+                    coord[2] = 'E'
+                elif coord[2] == 'E':
+                    coord[2] = 'S'
+                elif coord[2] == 'W':
+                    coord[2] = 'N'
+                else:
+                    coord[2] = 'W'
+
         # simulate one instruction set
         for i in instructions:
             if i == 'G':
-                if coord[2] == 'N':
-                    coord[1] += 1
-                elif coord[2] == 'E':
-                    coord[0] += 1
-                elif coord[2] == 'W':
-                    coord[0] -= 1
-                else:
-                    coord[1] -= 1
+                move_robot(coord)
             elif i == 'L':
-                if coord[2] == 'N':
-                    coord[2] = 'W'
-                elif coord[2] == 'E':
-                    coord[2] = 'N'
-                elif coord[2] == 'W':
-                    coord[2] = 'S'
-                else:
-                    coord[2] = 'E'
+                turn_robot(coord, 'L')
             else:
-                if coord[2] == 'N':
-                    coord[2] = 'E'
-                elif coord[2] == 'E':
-                    coord[2] = 'S'
-                elif coord[2] == 'W':
-                    coord[2] = 'N'
-                else:
-                    coord[2] = 'W'
+                turn_robot(coord, 'R')
 
         if (coord[0] == 0 and coord[1] == 0) or coord[2] != 'N':
             return True
