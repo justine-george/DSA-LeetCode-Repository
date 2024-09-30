745bfb0fbc3cdef9afc5d04a83ffeba37a9f2e62
Justine George <justinegeo96@gmail.com>
2024-09-30 02:43:44 -0700

    Time: 163 ms (39.53%) | Memory: 16.9 MB (72.87%) - LeetSync

--- 3474-better-compression-of-string/better-compression-of-string.py
+++ 3474-better-compression-of-string/better-compression-of-string.py
@@ -4,15 +4,15 @@ class Solution:
         i = 0
         while i < len(compressed):
             char = compressed[i]
-
             i += 1
+
             count = ''
             while i < len(compressed) and (compressed[i] < 'a' or compressed[i] > 'z'):
                 count += compressed[i]
                 i += 1
             i -= 1
 
-            map[char] += int(count) if count != '' else 0
+            map[char] += int(count)
             i += 1
         
         res = []
