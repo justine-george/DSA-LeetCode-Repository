9faa51e74639d5bb4d2033eb132bd2c2cfe40103
Justine George <justinegeo96@gmail.com>
2024-09-30 01:10:31 -0700

    Time: 75 ms (25.51%) | Memory: 16.9 MB (44.36%) - LeetSync

--- 1074-high-five/high-five.py
+++ 1074-high-five/high-five.py
@@ -2,8 +2,7 @@ class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
         id_marks_map = defaultdict(list)
         
-        for item in items:
-            id, score = item
+        for id, score in items:
             if len(id_marks_map[id]) < 5:
                 heapq.heappush(id_marks_map[id], score)
             elif score > id_marks_map[id][0]:
@@ -12,10 +11,8 @@ class Solution:
         
         res = []
         for id in id_marks_map.keys():
-            total = 0
-            while id_marks_map[id]:
-                total += heapq.heappop(id_marks_map[id])
+            total = sum(id_marks_map[id])
             avg = total // 5
             res.append([id, avg])
         
-        return sorted(res)
+        return sorted(res, key=lambda x: x[0])
