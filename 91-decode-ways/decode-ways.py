2c0f09d1c0c04b836711847f7e3381cef4a005c4
Justine George <justinegeo96@gmail.com>
2024-09-29 21:49:51 -0700

    Time: 43 ms (13.27%) | Memory: 16.4 MB (82.15%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -13,39 +13,31 @@ class Solution:
         '''
         
         # # dp approach
-        # # T: O(n), S: O(n)
-        # n = len(s)
-        # if n == 0 or s[0] == '0':
-        #     return 0
+        '''
+        Method to calculate the number of ways to decode a given encoded string 's'.
 
-        # dp = {n: 1}
-        # for i in range(n - 1, -1, -1):
-        #     if s[i] == "0":
-        #         dp[i] = 0
-        #     else:
-        #         dp[i] = dp[i + 1]
+        Args:
+        - s (str): The encoded string containing digits
 
-        #     if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
-        #         dp[i] += dp[i + 2]
-        
-        # return dp[0]
+        Returns:
+        - int: The number of possible decoding ways.        
+        '''
 
-        # more efficient space, S:O(1)
         n = len(s)
         if n == 0 or s[0] == "0":
             return 0
         
-        next1, next2 = 1, 1 # base cases
+        dp = {n: 1} # base case: one way to decode an empty string
         for i in range(n - 1, -1, -1):
-            current = 0
-
-            if s[i] != "0":
-                current = next1
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                # single digit decode
+                dp[i] = dp[i + 1]
             
+            # double digit decode
             if i + 1 < n and 10 <= int(s[i: i + 2]) <= 26:
-                current += next2
-            
-            next2 = next1
-            next1 = current
+                dp[i] += dp[i + 2]
         
-        return next1
+        # returns number of ways to decode starting from 0th index.
+        return dp[0]
