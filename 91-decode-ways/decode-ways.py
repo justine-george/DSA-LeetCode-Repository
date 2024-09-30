59ff47d1df55c176acf9b63055d8ed7f260df49e
Justine George <justinegeo96@gmail.com>
2024-09-29 21:43:43 -0700

    Time: 21 ms (99.63%) | Memory: 16.6 MB (30.76%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -12,20 +12,40 @@ class Solution:
 
         '''
         
-        n = len(s)
-        if n == 0 or s[0] == '0':
-            return 0
+        # # dp approach
+        # # T: O(n), S: O(n)
+        # n = len(s)
+        # if n == 0 or s[0] == '0':
+        #     return 0
 
-        dp = {n: 1}
-        for i in range(n - 1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
+        # dp = {n: 1}
+        # for i in range(n - 1, -1, -1):
+        #     if s[i] == "0":
+        #         dp[i] = 0
+        #     else:
+        #         dp[i] = dp[i + 1]
 
-            if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
-                dp[i] += dp[i + 2]
+        #     if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+        #         dp[i] += dp[i + 2]
         
+        # return dp[0]
 
-        return dp[0]
+        # more efficient space, S:O(1)
+        n = len(s)
+        if n == 0 or s[0] == "0":
+            return 0
+        
+        next1, next2 = 1, 1 # base cases
+        for i in range(n - 1, -1, -1):
+            current = 0
 
+            if s[i] != "0":
+                current = next1
+            
+            if i + 1 < n and 10 <= int(s[i: i + 2]) <= 26:
+                current += next2
+            
+            next2 = next1
+            next1 = current
+        
+        return next1
