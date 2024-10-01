6b00d86213089f16901f27e221474436f8f8f4a8
Justine George <justinegeo96@gmail.com>
2024-09-30 23:47:46 -0700

    Time: 134 ms (13.51%) | Memory: 16.5 MB (89.62%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        q = deque()
+
+        for i in range(1, n + 1):
+            q.append(i)
+
+        if n == 1 and k == 1:
+            return 1
+
+        while True:
+            # shift k - 1 times
+            for i in range(k - 1):
+                val = q.popleft()
+                q.append(val)
+            
+            # remove kth
+            q.popleft()
+
+            if len(q) == 1:
+                break
+
+        return q.popleft()
