504658a60ad6f6f8221d42cdf4851fd47446581d
Justine George <justinegeo96@gmail.com>
2024-09-30 23:51:51 -0700

    Time: 119 ms (19.98%) | Memory: 16.5 MB (61.25%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(1, n + 1):
             q.append(i)
 
-        for _ in range(n - 1):
+        while len(q) > 1:
             # shift k - 1 times
             for i in range(k - 1):
                 val = q.popleft()
