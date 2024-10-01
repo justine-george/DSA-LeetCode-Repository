6573ab5bebd1bbadd669b3cf66cfd4ba6a5d39fa
Justine George <justinegeo96@gmail.com>
2024-09-30 23:50:56 -0700

    Time: 126 ms (16.36%) | Memory: 16.5 MB (61.25%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,15 +1,14 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         # T: O(nk), S: O(n)
-        if n == 1:
-            return 1
-            
+        # if n == 1:
+        #     return 1
+
         q = deque()
         for i in range(1, n + 1):
             q.append(i)
 
-
-        while True:
+        for _ in range(n - 1):
             # shift k - 1 times
             for i in range(k - 1):
                 val = q.popleft()
@@ -18,7 +17,4 @@ class Solution:
             # remove kth
             q.popleft()
 
-            if len(q) == 1:
-                return q.popleft()
-
-        # return q.popleft()
+        return q.popleft()
