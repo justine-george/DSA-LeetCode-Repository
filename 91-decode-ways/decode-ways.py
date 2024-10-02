0b15a14ced137cea3a610002f1132c1e052df43c
Justine George <justinegeo96@gmail.com>
2024-10-02 00:37:11 -0700

    Time: 38 ms (43.65%) | Memory: 16.6 MB (31.12%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,67 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        '''
-        121
-
-        starting from 0:
-            # single digit
-            (if not 0) + starting from 1
-
-            # 2 digit
-            (if between 10 and 26 incl.) + starting from 2
-
-        '''
-        
-        # # dp approach
-        '''
-        Method to calculate the number of ways to decode a given encoded string 's'.
-
-        Args:
-        - s (str): The encoded string containing digits
-
-        Returns:
-        - int: The number of possible decoding ways.        
-        '''
-
-        n = len(s)
-        if n == 0 or s[0] == "0":
+        if len(s) == 0 or s[0] == "0":
             return 0
-        
-        # dp = {n: 1} # base case: one way to decode an empty string
-        # for i in range(n - 1, -1, -1):
-        #     if s[i] == "0":
-        #         dp[i] = 0
-        #     else:
-        #         # single digit decode
-        #         dp[i] = dp[i + 1]
-            
-        #     # double digit decode
-        #     if i + 1 < n and 10 <= int(s[i: i + 2]) <= 26:
-        #         dp[i] += dp[i + 2]
-        
-        # # returns number of ways to decode starting from 0th index.
-        # return dp[0]
 
-        # space optimization
-        next1, next2 = 1, 1 # base case: one way to decode an empty string
+        dp = {len(s): 1}
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
 
-        for i in range(n - 1, -1, -1):
-            current = 0
-            if s[i] != "0":
-                # single digit decode
-                current = next1
-            
-            # double digit decode
-            if i + 1 < n and 10 <= int(s[i: i + 2]) <= 26:
-                current += next2
-            
-            # move variables for next iteration
-            # 1234
-            #      next2
-            #     next1
-            #    current
-            next2 = next1
-            next1 = current
+            if i + 2 <= len(s) and 10 <= int(s[i:i+2]) <= 26:
+                dp[i] += dp[i + 2]
         
-        return next1
+        return dp[0]
 
+
+        # 1 2
+
+        # [0 1]
