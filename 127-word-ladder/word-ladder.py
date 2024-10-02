00c4b872abc490cb0cb24ea57e79d74c491fe907
Justine George <justinegeo96@gmail.com>
2024-10-01 19:46:09 -0700

    Time: 429 ms (29.08%) | Memory: 18.1 MB (70.17%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -7,12 +7,15 @@ class Solution:
         while q:
             word, count = q.popleft()
 
-            if word == endWord:
-                return count
+            # if word == endWord:
+            #     return count
 
             for i in range(len(word)):
                 for c in "abcdefghijklmnopqrstuvwxyz":
                     newWord = word[:i] + c + word[i+1:]
+
+                    if newWord == endWord and newWord in wordListSet:
+                        return count + 1
 
                     if newWord not in visited and newWord != word and newWord in wordListSet:
                         q.append((newWord, count + 1))
