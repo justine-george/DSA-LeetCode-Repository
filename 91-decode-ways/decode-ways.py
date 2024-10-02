e653689d758260ba7d6cada693ac9d17b05bb7d9
Justine George <justinegeo96@gmail.com>
2024-10-02 00:39:03 -0700

    Time: 33 ms (76.01%) | Memory: 16.5 MB (54.89%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -3,19 +3,15 @@ class Solution:
         if len(s) == 0 or s[0] == "0":
             return 0
 
-        dp = {len(s): 1}
+        # dp = {len(s): 1}
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
         for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
-                dp[i] = 0
+                dp[i] += 0
             else:
-                dp[i] = dp[i + 1]
-
+                dp[i] += dp[i + 1]
             if i + 2 <= len(s) and 10 <= int(s[i:i+2]) <= 26:
                 dp[i] += dp[i + 2]
         
         return dp[0]
-
-
-        # 1 2
-
-        # [0 1]
