c7140321ba2f639abaecad9c3c709bffce4ec95c
Justine George <justinegeo96@gmail.com>
2024-10-01 17:33:06 -0700

    Time: 415 ms (30.89%) | Memory: 18.1 MB (70.17%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,37 +1,22 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
+        q = deque([(beginWord, 1)])
+        visited = set([beginWord])
 
-        # make adjacency list {wildcard_pattern: [words]}, eg: {'*ot': ['hot', 'dot', 'bot']}
-        adjlist = collections.defaultdict(list)
-        wordList.append(beginWord)
-        for word in wordList:
-            for j in range(len(word)):
-                # for each position, replace with wild card char
-                pattern = word[ : j] + '*' + word[j + 1 : ]
-                adjlist[pattern].append(word)
+        wordListSet = set(wordList)
 
-        # bfs
-        visit = set([beginWord])
-        q = collections.deque([beginWord])
-        res = 1
         while q:
-            # go layer by layer
-            for i in range(len(q)):
-                word = q.popleft()
-                if word == endWord:
-                    return res
-                
-                # get neighbors
-                # first find all pattern this word falls into, then get from adjlist
-                for j in range(len(word)):
-                    pattern = word[ : j] + '*' + word[j + 1 : ]
-                    for neighborWord in adjlist[pattern]:
-                        if neighborWord not in visit:
-                            visit.add(neighborWord)
-                            q.append(neighborWord)
+            word, count = q.popleft()
 
-            res += 1
+            if word == endWord:
+                return count
+
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newWord = word[:i] + c + word[i+1:]
+
+                    if newWord not in visited and newWord != word and newWord in wordListSet:
+                        q.append((newWord, count + 1))
+                        visited.add(newWord)
 
         return 0
