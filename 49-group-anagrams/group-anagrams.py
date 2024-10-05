8b1795aebf5a7ca7f6eb49633d3223b6820c211a
Justine George <justinegeo96@gmail.com>
2024-10-05 04:06:58 -0700

    Time: 91 ms (51.30%) | Memory: 22.1 MB (19.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,6 +3,8 @@ class Solution:
         map = defaultdict(list)
         for s in strs:
             # key = ''.join(sorted(s))
-            key = sorted(s)
-            map[tuple(key)].append(s)
+            charmap = [0] * 26
+            for c in s:
+                charmap[ord(c) - ord('a')] += 1
+            map[tuple(charmap)].append(s)
         return map.values()
