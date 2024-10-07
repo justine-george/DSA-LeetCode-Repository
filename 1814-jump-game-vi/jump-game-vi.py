4fa4a40c98accfa4c26c4284526ae3f06f9c755b
Justine George <justinegeo96@gmail.com>
2024-10-07 14:33:06 -0700

    Time: 682 ms (48.34%) | Memory: 30.7 MB (24.95%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.py
+++ 1814-jump-game-vi/jump-game-vi.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         N = len(nums)
+        # Initialize the dp array with the same value of nums for last element
         dp = [0] * N
         dp[-1] = nums[-1]
 
-        # store indices of the dp array
+        # Deque to store indices, starting with the last index
         dq = deque([N - 1])
 
         '''
@@ -12,14 +13,19 @@ class Solution:
         [7    6    5   7  -4  3]
         '''
         for i in range(N - 2, -1, -1):
+            # Remove indices from deque which are out of current range [i + 1, i + k]
             while dq and dq[0] > i +  k:
                 dq.popleft()
             
+            # Compute the maximum score for current position
             dp[i] = nums[i] + dp[dq[0]]
 
+            # Maintain the deque so that it stores indices in decreasing order of dp values
+            # Remove elements from the back if they are less than or equal to the current score
             while dq and dp[i] >= dp[dq[-1]]:
                 dq.pop()
             
             dq.append(i)
 
+        # dp[0] holds the maximum score from the first to the last index
         return dp[0]
