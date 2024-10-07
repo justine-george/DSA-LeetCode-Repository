d852ff30a65026b752a4d5fa6a253f20f7fadd6a
Justine George <justinegeo96@gmail.com>
2024-10-07 10:33:00 -0700

    Time: 238 ms (14.67%) | Memory: 23.3 MB (57.72%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -4,14 +4,13 @@ class Solution:
         q = deque([start])
         visited = set([start])
         while q:
-            for _ in range(len(q)):
-                st_index = q.popleft()
-                if arr[st_index] == 0:
-                    return True
-                # jump to neighbors
-                for next_index in [st_index + arr[st_index], st_index - arr[st_index]]:
-                    if 0 <= next_index < len(arr) and next_index not in visited:
-                        q.append(next_index)
-                        visited.add(next_index)
+            st_index = q.popleft()
+            if arr[st_index] == 0:
+                return True
+            # jump to neighbors
+            for next_index in [st_index + arr[st_index], st_index - arr[st_index]]:
+                if 0 <= next_index < len(arr) and next_index not in visited:
+                    q.append(next_index)
+                    visited.add(next_index)
             
         return False
