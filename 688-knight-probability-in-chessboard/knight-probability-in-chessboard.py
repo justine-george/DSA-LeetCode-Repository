5ca38196d6808865d91a853c9c1ba4ccca90f2f2
Justine George <justinegeo96@gmail.com>
2024-10-07 02:29:48 -0700

    Time: 205 ms (34.41%) | Memory: 16.8 MB (78.20%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -11,26 +11,49 @@ class Solution:
             (2,1),
         ]
 
-        # bottoms up approach
-        # initialize 3d dp with all zeros
-        dp = [[[0.0 for _ in range(n)] for _ in range(n)] for _ in range(k + 1)]
+        # bottoms up approach - space optimized
+        # initialize 2 2d dps
+        prev_dp = [[0.0 for _ in range(n)] for _ in range(n)]
+        cur_dp = [[0.0 for _ in range(n)] for _ in range(n)]
 
         # base case: 0 moves -> prob = 1 if starting on the board
         for i in range(n):
             for j in range(n):
-                dp[0][i][j] = 1.0
+                prev_dp[i][j] = 1.0
         
         # fill dp table
         for move in range(1, k + 1):
             for x in range(n):
                 for y in range(n):
+                    cur_dp[x][y] = 0.0
                     for dx, dy in directions:
                         nx, ny = x + dx, y + dy
                         if 0 <= nx < n and 0 <= ny < n:
-                            dp[move][x][y] += dp[move - 1][nx][ny] / 8.0
+                            cur_dp[x][y] += prev_dp[nx][ny] / 8.0
+            prev_dp, cur_dp = cur_dp, prev_dp
 
-        print(dp)
-        return dp[k][row][column]
+        return prev_dp[row][column]
+
+        # # bottoms up approach
+        # # initialize 3d dp with all zeros
+        # dp = [[[0.0 for _ in range(n)] for _ in range(n)] for _ in range(k + 1)]
+
+        # # base case: 0 moves -> prob = 1 if starting on the board
+        # for i in range(n):
+        #     for j in range(n):
+        #         dp[0][i][j] = 1.0
+        
+        # # fill dp table
+        # for move in range(1, k + 1):
+        #     for x in range(n):
+        #         for y in range(n):
+        #             for dx, dy in directions:
+        #                 nx, ny = x + dx, y + dy
+        #                 if 0 <= nx < n and 0 <= ny < n:
+        #                     dp[move][x][y] += dp[move - 1][nx][ny] / 8.0
+
+        # print(dp)
+        # return dp[k][row][column]
 
         # recursive dp + memoization
         # dp = {}
