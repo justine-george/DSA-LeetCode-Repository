ef8d9ce36422f742a09ad4ef153a1445c01d6ac8
Justine George <justinegeo96@gmail.com>
2024-10-07 02:11:16 -0700

    Time: 149 ms (80.03%) | Memory: 25.8 MB (25.48%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -1,14 +1,14 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
         directions = [
-            (-2, 1),
-            (-1, 2),
-            (-1, -2),
-            (-2, -1),
-            (2, 1),
-            (1, 2),
-            (1, -2),
-            (2, -1)
+            (-2,-1),
+            (-2,1),
+            (-1,-2),
+            (-1,2),
+            (1,-2),
+            (1,2),
+            (2,-1),
+            (2,1),
         ]
 
         @cache
