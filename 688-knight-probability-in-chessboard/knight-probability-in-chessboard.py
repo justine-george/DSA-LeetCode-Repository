c69319d636b86a5c8c8713d251b7ebc5f7ab4646
Justine George <justinegeo96@gmail.com>
2024-10-07 02:23:55 -0700

    Time: 293 ms (5.25%) | Memory: 22.9 MB (35.38%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -11,19 +11,45 @@ class Solution:
             (2,1),
         ]
 
-        @cache
-        def calculate(move, x, y):
-            if move == k:
-                return 1.0
+        # bottoms up approach
+        # initialize 3d dp with all zeros
+        dp = [[[0.0 for _ in range(n)] for _ in range(n)] for _ in range(k + 1)]
 
-            p = 0.0
-
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-
-                if 0 <= nx < n and 0 <= ny < n:
-                    p += calculate(move + 1, nx, ny) / 8.0
-
-            return p
+        # base case: 0 moves -> prob = 1 if starting on the board
+        for i in range(n):
+            for j in range(n):
+                dp[0][i][j] = 1.0
         
-        return calculate(0, row, column)
+        # fill dp table
+        for move in range(1, k + 1):
+            for x in range(n):
+                for y in range(n):
+                    for dx, dy in directions:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < n and 0 <= ny < n:
+                            dp[move][x][y] += dp[move - 1][nx][ny] / 8.0
+
+        print(dp)
+        return dp[k][row][column]
+
+        # recursive dp + memoization
+        # dp = {}
+        # def calculate(move, x, y):
+        #     if (move, x, y) in dp:
+        #         return dp[(move, x, y)]
+
+        #     if move == k:
+        #         return 1.0
+
+        #     p = 0.0
+
+        #     for dx, dy in directions:
+        #         nx, ny = x + dx, y + dy
+
+        #         if 0 <= nx < n and 0 <= ny < n:
+        #             p += calculate(move + 1, nx, ny) / 8.0
+
+        #     dp[(move, x, y)] = p
+        #     return p
+        
+        # return calculate(0, row, column)
