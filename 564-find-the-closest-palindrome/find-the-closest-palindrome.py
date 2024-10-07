60f8ca8507cdca7c40eb8d10744b162751704069
Justine George <justinegeo96@gmail.com>
2024-10-07 01:33:38 -0700

    Time: 35 ms (63.29%) | Memory: 16.6 MB (28.12%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.py
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.py
@@ -6,8 +6,8 @@ class Solution:
             12321
             12[2]21
             12[4]21
-            9999
-            100001
+            9999 -> int(1 + (len(n) - 1) zeros) - 1
+            100001 -> int(1 + len(n) zeros) + 1
 
         '''
 
@@ -18,17 +18,24 @@ class Solution:
 
         candidates = []
 
-        def half_to_palindrome(firstHalf, is_even):
-            strFirst = str(firstHalf)
-            strSecond = ''
-            if is_even:
-                strSecond = strFirst[::-1]
-            else:
-                strSecond = strFirst[:-1][::-1]
+        def half_to_palindrome(num, is_even):
+            # strFirst = str(firstHalf)
+            # strSecond = ''
+            # if is_even:
+            #     strSecond = strFirst[::-1]
+            # else:
+            #     strSecond = strFirst[:-1][::-1]
+            # return int(strFirst+strSecond)
+            res = num
+
+            if not is_even:
+                num = num // 10
             
-            return int(strFirst+strSecond)
-
-
+            while num > 0:
+                res = res * 10 + num % 10
+                num //= 10
+            
+            return res
 
         candidates.append(half_to_palindrome(firstHalf, is_even))
         candidates.append(half_to_palindrome(firstHalf + 1, is_even))
