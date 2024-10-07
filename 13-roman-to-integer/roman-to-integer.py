d9c59aa3a71062e50a7cd50e672047b7fdde7714
Justine George <justinegeo96@gmail.com>
2024-10-07 06:16:52 -0700

    Time: 41 ms (82.82%) | Memory: 16.5 MB (91.09%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -9,36 +9,11 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-        res = roman_map[s[len(s) - 1]]
-        for i in range(len(s) - 2, -1, -1):
-            if roman_map[s[i]] < roman_map[s[i + 1]]:
+
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
                 res -= roman_map[s[i]]
             else:
                 res += roman_map[s[i]]
         return res
-
-        # roman_map = {
-        #     "I": 1,
-        #     "IV": 4,
-        #     "V": 5,
-        #     "IX": 9,
-        #     "X": 10,
-        #     "XL": 40,
-        #     "L": 50,
-        #     "XC": 90,
-        #     "C": 100,
-        #     "CD": 400,
-        #     "D": 500,
-        #     "CM": 900,
-        #     "M": 1000
-        # }
-        # i = 0
-        # res = 0
-        # while i < len(s):
-        #     if i + 1 < len(s) and s[i:i+2] in roman_map:
-        #         res += roman_map[s[i:i+2]]
-        #         i += 2
-        #     else:
-        #         res += roman_map[s[i]]
-        #         i += 1
-        # return res
