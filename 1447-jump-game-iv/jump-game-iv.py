733a9c68ce5ba6186837e6e6daeb12b81b0a9aee
Justine George <justinegeo96@gmail.com>
2024-10-07 11:34:14 -0700

    Time: 542 ms (57.32%) | Memory: 31.7 MB (40.69%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        # Group indices by value.
+        same_val_indices_map = defaultdict(list)
+        for i, a in enumerate(arr):
+            same_val_indices_map[a].append(i)
+        
+        res = 0
+        q = deque([0])
+        visited = set([0])
+        n = len(arr)
+
+        while q:
+            for _ in range(len(q)):
+                cur_index = q.popleft()
+
+                # Check if we've reached the last index
+                if cur_index == n - 1:
+                    return res
+                
+                # Check neighbours on the left and right
+                neighbors = []
+                if cur_index - 1 >= 0:
+                    neighbors.append(cur_index - 1)
+                if cur_index + 1 < n:
+                    neighbors.append(cur_index + 1)
+
+                # Add all indices with the same value as potential jumps
+                # We only consider these neighbors once to avoid redundant traversals
+                neighbors.extend(same_val_indices_map[arr[cur_index]])
+
+                # Clear the list to prevent future unnecessary check for these indices
+                same_val_indices_map[arr[cur_index]].clear()
+                
+                # Visit all neighbors
+                for neighbor in neighbors:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        q.append(neighbor)
+                
+            res += 1
+        
+        return res
