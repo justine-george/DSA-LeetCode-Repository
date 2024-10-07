5c28d39570616371de54557179bb0a518b744285
Justine George <justinegeo96@gmail.com>
2024-10-06 20:14:41 -0700

    Time: 54 ms (80.03%) | Memory: 16.7 MB (16.73%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,25 +1,46 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        count = 1
         n = len(chars)
-        res = []
-        i = 0
+        if n <= 1:
+            return n
+
+        i, j = 0, 0
         while i < n:
-            prev = chars[i]
-            res.append(chars[i])
-            i += 1
-            while i < n and chars[i] == prev:
+            count = 1
+            while i < n - 1 and chars[i] == chars[i + 1]:
                 count += 1
                 i += 1
+            
+            chars[j] = chars[i]
+            j += 1
             if count > 1:
                 for digit in str(count):
-                    res.append(digit)
-                count = 1
+                    chars[j] = digit
+                    j += 1
             
-        print(res)
+            i += 1
+
+        return j
+
+        # count = 1
+        # res = []
+        # i = 0
+        # while i < n:
+        #     prev = chars[i]
+        #     res.append(chars[i])
+        #     i += 1
+        #     while i < n and chars[i] == prev:
+        #         count += 1
+        #         i += 1
+        #     if count > 1:
+        #         for digit in str(count):
+        #             res.append(digit)
+        #         count = 1
+            
+        # print(res)
         
-        for i in range(len(res)):
-            chars[i] = res[i]
+        # for i in range(len(res)):
+        #     chars[i] = res[i]
         
-        chars = chars[:len(res)]
-        return len(chars)
+        # chars = chars[:len(res)]
+        # return len(chars)
