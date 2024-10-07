9dbd9934b17ee731d37f54ec4ed36911804febc9
Justine George <justinegeo96@gmail.com>
2024-10-07 06:43:59 -0700

    Time: 39 ms (26.94%) | Memory: 16.6 MB (57.25%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -7,6 +7,8 @@ class Solution:
             x = 1/x
             n = -n
 
+        # T: O(logn)
+        # S: O(1)
         res = 1
         while n != 0:
             # power is an odd number
@@ -15,6 +17,7 @@ class Solution:
                 n -= 1
             
             # now power is even
+            # x**n = (x**2)**(n/2)
             x *= x
             n //= 2
 
