ddd6732fc3b7c729f7689626ede70aab45cdbc2d
Justine George <justinegeo96@gmail.com>
2024-10-07 16:38:20 -0700

    Time: 186 ms (80.28%) | Memory: 20.5 MB (34.64%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if s[-1] != '0':
+            return False
+
+        N = len(s)
+        q = deque([0])
+        farthest = 0
+
+        while q:
+            i = q.popleft()
+
+            start = max(i + minJump, farthest + 1)
+            end = min(i + maxJump, N - 1)
+
+            for j in range(start, end + 1):
+                if s[j] == "0":
+                    if j == N - 1:
+                        return True
+                    q.append(j)
+            
+            farthest = end
+        
+        return False
