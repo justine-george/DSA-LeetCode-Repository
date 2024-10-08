86f9745375d6b7605648ecf5b0d91762c16ace9f
Justine George <justinegeo96@gmail.com>
2024-10-07 18:12:43 -0700

    Time: 40 ms (16.73%) | Memory: 16.7 MB (20.71%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        2
+
+        @cache
+        def dfs(i):
+            if i == n:
+                return 1
+            
+            ways = dfs(i + 1) if i + 1 <= n else 0
+            ways += dfs(i + 2) if i + 2 <= n else 0
+            return ways
+
+        return dfs(1) + dfs(2)
