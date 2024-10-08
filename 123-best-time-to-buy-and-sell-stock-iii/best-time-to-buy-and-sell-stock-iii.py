551db2381233bbd17722ff7893d746567303839d
Justine George <justinegeo96@gmail.com>
2024-10-08 14:37:47 -0700

    Time: 777 ms (66.53%) | Memory: 30.6 MB (49.91%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,40 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = {}
-        def iterate(i, canBuy, rem):
-            if (i, canBuy, rem) in memo:
-                return memo[(i, canBuy, rem)]
-                
-            if i >= len(prices) or rem == 0:
-                return 0
+        # state variables
+        if not prices:
+            return 0
+        
+        A = -prices[0]
+        B = float('-inf')
+        C = float('-inf')
+        D = float('-inf')
 
-            if canBuy:
-                buy = iterate(i + 1, False, rem) - prices[i]
-                skip = iterate(i + 1, True, rem)
-                res = max(buy, skip)
-            else:
-                sell = iterate(i + 1, True, rem - 1) + prices[i]
-                skip = iterate(i + 1, False, rem)
-                res = max(sell, skip)
+        for price in prices:
+            A = max(A, -price)
+            B = max(B, A + price)
+            C = max(C, B - price)
+            D = max(D, C + price)
             
-            memo[(i, canBuy, rem)] = res
-            return res
+        return D
 
-        return iterate(0, True, 2)
+        # memo = {}
+        # def iterate(i, canBuy, rem):
+        #     if (i, canBuy, rem) in memo:
+        #         return memo[(i, canBuy, rem)]
+                
+        #     if i >= len(prices) or rem == 0:
+        #         return 0
+
+        #     if canBuy:
+        #         buy = iterate(i + 1, False, rem) - prices[i]
+        #         skip = iterate(i + 1, True, rem)
+        #         res = max(buy, skip)
+        #     else:
+        #         sell = iterate(i + 1, True, rem - 1) + prices[i]
+        #         skip = iterate(i + 1, False, rem)
+        #         res = max(sell, skip)
+            
+        #     memo[(i, canBuy, rem)] = res
+        #     return res
+
+        # return iterate(0, True, 2)
