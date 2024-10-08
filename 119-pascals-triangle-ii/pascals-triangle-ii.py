b0616bb5136585ec27199f6b7c063f3108ccb84f
Justine George <justinegeo96@gmail.com>
2024-10-07 18:48:25 -0700

    Time: 34 ms (63.94%) | Memory: 16.4 MB (69.51%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        elif rowIndex == 1:
+            return [1, 1]
+
+        '''
+        11
+        121
+        '''
+        prev_row = [1, 1]
+        for i in range(2, rowIndex + 1):
+            cur_row = [0] * (i + 1)
+
+            cur_row[0] = 1
+            for j in range(1, i):
+                cur_row[j] = prev_row[j-1] + prev_row[j]
+            cur_row[-1] = 1
+            prev_row, cur_row = cur_row, prev_row
+        
+        return prev_row
+
+
