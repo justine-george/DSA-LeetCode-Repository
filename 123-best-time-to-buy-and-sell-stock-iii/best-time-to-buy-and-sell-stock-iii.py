239ba48651ef8d8cdc3dcce7e69ff28a78df2285
Justine George <justinegeo96@gmail.com>
2024-10-08 15:42:08 -0700

    Time: 2213 ms (9.11%) | Memory: 448.6 MB (8.45%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,40 +1,33 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # state variables
-        if not prices:
-            return 0
-        
-        A = -prices[0]
-        B = float('-inf')
-        C = float('-inf')
-        D = float('-inf')
-
-        for price in prices:
-            A = max(A, -price)
-            B = max(B, A + price)
-            C = max(C, B - price)
-            D = max(D, C + price)
-            
-        return D
-        
-        # memo = {}
-        # def iterate(i, canBuy, rem):
-        #     if (i, canBuy, rem) in memo:
-        #         return memo[(i, canBuy, rem)]
+        memo = {}
+        def iterate(i, canBuy, rem):
+            if i >= len(prices) or rem == 0:
+                return 0
+            if (i, canBuy, rem) in memo:
+                return memo[(i, canBuy, rem)]
                 
-        #     if i >= len(prices) or rem == 0:
-        #         return 0
-
-        #     if canBuy:
-        #         buy = iterate(i + 1, False, rem) - prices[i]
-        #         skip = iterate(i + 1, True, rem)
-        #         res = max(buy, skip)
-        #     else:
-        #         sell = iterate(i + 1, True, rem - 1) + prices[i]
-        #         skip = iterate(i + 1, False, rem)
-        #         res = max(sell, skip)
+            skipProfit = iterate(i + 1, canBuy, rem)
+            doProfit = iterate(i + 1, False, rem) - prices[i] if canBuy else iterate(i + 1, True, rem - 1) + prices[i]
+            memo[(i, canBuy, rem)] = max(skipProfit, doProfit)
             
-        #     memo[(i, canBuy, rem)] = res
-        #     return res
+            return memo[(i, canBuy, rem)]
 
-        # return iterate(0, True, 2)
+        return iterate(0, True, 2)
+
+        # # state variables
+        # if not prices:
+        #     return 0
+        
+        # A = -prices[0]
+        # B = float('-inf')
+        # C = float('-inf')
+        # D = float('-inf')
+
+        # for price in prices:
+        #     A = max(A, -price)
+        #     B = max(B, A + price)
+        #     C = max(C, B - price)
+        #     D = max(D, C + price)
+            
+        # return D
