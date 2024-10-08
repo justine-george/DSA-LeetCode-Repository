853991cb8deb76b80412b792cfb5c0e11d9165af
Justine George <justinegeo96@gmail.com>
2024-10-07 18:37:56 -0700

    Time: 31 ms (88.40%) | Memory: 16.6 MB (38.94%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -9,12 +9,10 @@ class Solution:
             if i < 3:
                 res.append([1 for _ in range(i)])
                 continue
-            
             temp = [1]
             for j in range(1, i - 1):
                 temp.append(res[-1][j - 1] + res[-1][j])
             temp += [1]
             res.append(temp)
-
         return res
 
