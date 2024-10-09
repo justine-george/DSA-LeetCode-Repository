a2dbf9d62947227728cce1219a6a3aedfa9f436a
Justine George <justinegeo96@gmail.com>
2024-10-09 03:20:54 -0700

    Time: 74 ms (30.22%) | Memory: 17 MB (11.77%) - LeetSync

--- 1074-high-five/high-five.py
+++ 1074-high-five/high-five.py
@@ -3,16 +3,18 @@ class Solution:
         id_marks_map = defaultdict(list)
         
         for id, score in items:
-            if len(id_marks_map[id]) < 5:
-                heapq.heappush(id_marks_map[id], score)
-            elif score > id_marks_map[id][0]:
+            heapq.heappush(id_marks_map[id], score)
+            if len(id_marks_map[id]) > 5:
                 heapq.heappop(id_marks_map[id])
-                heapq.heappush(id_marks_map[id], score)
         
         res = []
-        for id in id_marks_map.keys():
-            total = sum(id_marks_map[id])
-            avg = total // 5
+        for id in id_marks_map:
+            avg = sum(id_marks_map[id])//len(id_marks_map[id])
             res.append([id, avg])
         
-        return sorted(res, key=lambda x: x[0])
+        self.sort_arr = res
+        # return sorted(res, key=lambda x: x[0])
+        return sorted(res, key=self.custom_comparator)
+    
+    def custom_comparator(self, n):
+        return (n[0])
