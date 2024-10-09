fb76c96bde46f227a023631c5116bd74c0a4bc44
Justine George <justinegeo96@gmail.com>
2024-10-08 22:54:06 -0700

    Time: 45 ms (83.91%) | Memory: 16.6 MB (64.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,16 +1,8 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        q = deque([])
-        visited = set()
-        fresh_count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 2:
-                    q.append((i, j))
-                    visited.add((i, j))
-                elif grid[i][j] == 1:
-                    fresh_count += 1
+        q = deque([(i, j) for i in range(m) for j in range(n) if grid[i][j] == 2])
+        fresh_count = sum(grid[i][j] == 1 for i in range(m) for j in range(n))
         
         if fresh_count == 0:
             return 0
@@ -22,14 +14,12 @@ class Solution:
                 i, j = q.popleft()
                 for dx, dy in directions:
                     nx, ny = i + dx, j + dy
-                    if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited:
-                        if grid[nx][ny] == 1:
-                            q.append((nx, ny))
-                            visited.add((nx, ny))
-                            fresh_count -= 1
-                            if fresh_count == 0:
-                                return time_max + 1
-
+                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        q.append((nx, ny))
+                        fresh_count -= 1
+                        if fresh_count == 0:
+                            return time_max + 1
             time_max += 1
 
         return -1
