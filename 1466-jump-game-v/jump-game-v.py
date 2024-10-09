9d9259145ee1fef625f85f5aca8633e58693cd63
Justine George <justinegeo96@gmail.com>
2024-10-08 19:31:21 -0700

    Time: 347 ms (69.06%) | Memory: 16.8 MB (88.27%) - LeetSync

--- 1466-jump-game-v/jump-game-v.py
+++ 1466-jump-game-v/jump-game-v.py
@@ -4,7 +4,8 @@ class Solution:
         # recognize base case -> by default, dp[i] = 1
         N = len(arr)
         dp = [1] * N
-        sorted_indeces = sorted(range(N), key=lambda i: arr[i])
+        self.arr = arr
+        sorted_indeces = sorted(range(N), key=self.custom_comparator)
         
         for i in sorted_indeces:
             # forwards
@@ -21,44 +22,6 @@ class Solution:
                     break
             
         return max(dp)
-
-        '''
-        # graph
-        # map: index -> places it can jump to
-        # DFS memoize
-
-        graph = defaultdict(set)
-        memo = {}
-
-        N = len(arr)
-
-        # build adjacency list
-        for i in range(N):
-            for j in range(i + 1, N):
-                if arr[j] < arr[i] and abs(j - i) <= d:
-                    graph[i].add(j)
-                else:
-                    break
-            for j in range(i - 1, -1, -1):
-                if arr[j] < arr[i] and abs(j - i) <= d:
-                    graph[i].add(j)
-                else:
-                    break
-        
-        def dfs(cur_idx):
-            if cur_idx in memo:
-                return memo[cur_idx]
-            
-            max_path = 1
-            for neighbor in graph[cur_idx]:
-                max_path = max(max_path, 1 + dfs(neighbor))
-            
-            memo[cur_idx] = max_path
-            return max_path
-        
-        res = 0
-        for i in range(N):
-            res = max(res, dfs(i))
-
-        return res
-        '''
+    
+    def custom_comparator(self, n):
+            return (self.arr[n])
