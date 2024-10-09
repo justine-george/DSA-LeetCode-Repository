dcd736ca343ec4be13a5b2d97a1fc2fb61e27d42
Justine George <justinegeo96@gmail.com>
2024-10-08 19:06:19 -0700

    Time: 672 ms (32.43%) | Memory: 25.8 MB (35.27%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,17 +3,31 @@ class Solution:
         N = len(prices)
         if N <= 1:
             return 0
+        
+        dp = [[0] * 2 for _ in range(N + 1)]
 
-        @cache
-        def iterate(i, canBuy):
-            if i >= N:
-                return 0
+        for cur in range(N - 1, -1, -1):
+            for canBuy in [0, 1]:
+                skip = dp[cur + 1][canBuy]
+                if canBuy:
+                    do = dp[cur + 1][0] - prices[cur]
+                else:
+                    do = dp[cur + 1][1] + prices[cur] - fee
+                dp[cur][canBuy] = max(do, skip)
+
+        return dp[0][1]
+        
+
+        # @cache
+        # def iterate(i, canBuy):
+        #     if i >= N:
+        #         return 0
             
-            skip = iterate(i + 1, canBuy)
-            if canBuy:
-                do = iterate(i + 1, False) - prices[i]
-            else:
-                do = iterate(i + 1, True) + prices[i] - fee
-            return max(skip, do)
+        #     skip = iterate(i + 1, canBuy)
+        #     if canBuy:
+        #         do = iterate(i + 1, False) - prices[i]
+        #     else:
+        #         do = iterate(i + 1, True) + prices[i] - fee
+        #     return max(skip, do)
 
-        return iterate(0, True)
+        # return iterate(0, True)
