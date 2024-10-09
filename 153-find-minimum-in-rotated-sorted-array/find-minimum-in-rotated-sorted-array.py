799b6259277459e090aa9e38fe959d278202244a
Justine George <justinegeo96@gmail.com>
2024-10-09 00:43:23 -0700

    Time: 40 ms (74.02%) | Memory: 17 MB (11.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -3,15 +3,15 @@ class Solution:
         l, r = 0, len(nums) - 1
         global_min = nums[0]
         while l <= r:
-            if nums[l] < nums[r]:
+            if nums[l] <= nums[r]:
                 global_min = min(global_min, nums[l])
                 break
             
             mid = l + (r - l) // 2
-            global_min = min(global_min, nums[mid])
+            # global_min = min(global_min, nums[mid])
             if nums[l] <= nums[mid]:
                 l = mid + 1
             else:
-                r = mid - 1
+                r = mid
         
         return global_min
