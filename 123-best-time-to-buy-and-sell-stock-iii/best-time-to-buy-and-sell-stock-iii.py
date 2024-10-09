acd50974c32a4c203a7721ef3e722ef742561b4b
Justine George <justinegeo96@gmail.com>
2024-10-08 18:09:35 -0700

    Time: 1664 ms (22.76%) | Memory: 56.8 MB (23.50%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
         if not prices:
             return 0
-    
+        
         n = len(prices)
         transactions = 2
 
