e3acd428e88c9b997e66ab73cda459b0eef4cb10
Justine George <justinegeo96@gmail.com>
2024-10-08 19:52:14 -0700

    Time: 3377 ms (11.06%) | Memory: 18 MB (9.39%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,35 +1,19 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # dp solution: T: O(n**2), S: O(n)
-        # dp = [False] * len(nums)
-        # dp[0] = True
-        # for i in range(len(nums)):
-        #     if dp[i] and nums[i] > 0:
-        #         for j in range(1, nums[i] + 1):
-        #             if i + j < len(nums):
-        #                 dp[i + j] = True
-        #                 if i + j == len(nums) - 1:
-        #                     return True
-        # return dp[len(nums) - 1]
+        dp = [False] * len(nums)
+        dp[0] = True
+        for i in range(len(nums)):
+            if dp[i] and nums[i] > 0:
+                for j in range(1, min(nums[i] + 1, len(nums))):
+                    dp[i + j] = True
+                    if i + j == len(nums) - 1:
+                        return True
+        return dp[len(nums) - 1]
 
         # # greedy solution: T: O(n), S: O(1)
         # goalpost = len(nums) - 1
-        # while goalpost >= 0:
-        #     cur = goalpost - 1
-        #     while cur >= 0 and nums[cur] < goalpost - cur:
-        #         cur -= 1
-        #         if cur == -1:
-        #             return False
-        #     goalpost = cur
-        # return True
-
-        # greedy solution: T: O(n), S: O(1)
-        goalpost = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= goalpost:
-                goalpost = i
-        return goalpost == 0
-
-
-
-
+        # for i in range(len(nums) - 2, -1, -1):
+        #     if i + nums[i] >= goalpost:
+        #         goalpost = i
+        # return goalpost == 0
