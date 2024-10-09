7e9c09568267a39586025ef717260aef5b909a96
Justine George <justinegeo96@gmail.com>
2024-10-09 03:29:58 -0700

    Time: 44 ms (6.46%) | Memory: 16.5 MB (33.68%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,16 +1,20 @@
 class Solution:
     def __init__(self):
         self.nums = []
+        self.memo = {}
     
-    @cache
     def dfs(self, curIdx):
         if curIdx >= len(self.nums):
             return 0
+
+        if curIdx in self.memo:
+            return self.memo[curIdx]
         
         skip = self.dfs(curIdx + 1)
         take = self.nums[curIdx] + self.dfs(curIdx + 2)
         
-        return max(skip, take)
+        self.memo[curIdx] = max(skip, take)
+        return self.memo[curIdx]
     
     def rob(self, nums: List[int]) -> int:
         self.nums = nums
