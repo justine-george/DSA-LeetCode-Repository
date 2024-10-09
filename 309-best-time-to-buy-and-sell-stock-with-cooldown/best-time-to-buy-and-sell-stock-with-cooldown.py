e488cb0ade0a66459b670c6cefc3b10286f6f289
Justine George <justinegeo96@gmail.com>
2024-10-08 18:54:36 -0700

    Time: 39 ms (84.64%) | Memory: 17.1 MB (61.87%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,17 +3,31 @@ class Solution:
         if len(prices) <= 1:
             return 0
 
-        @cache
-        def iterate(i, canBuy):
-            if i >= len(prices):
-                return 0
-            
-            skip = iterate(i + 1, canBuy)
-            if canBuy:
-                do = iterate(i + 1, False) - prices[i]
-            else:
-                do = iterate(i + 2, True) + prices[i]
-            
-            return max(skip, do)
+        # bottoms up dp
+        dp = [[0, 0] for _ in range(len(prices) + 2)]
 
-        return iterate(0, True)
+        for cur in range(len(prices) - 1, -1, -1):
+            for canBuy in [0, 1]:
+                skip = dp[cur + 1][canBuy]
+                if canBuy:
+                    do = dp[cur + 1][0] - prices[cur]
+                else:
+                    do = dp[cur + 2][1] + prices[cur]
+                dp[cur][canBuy] = max(skip, do)
+        
+        return dp[0][1]
+
+        # @cache
+        # def iterate(i, canBuy):
+        #     if i >= len(prices):
+        #         return 0
+            
+        #     skip = iterate(i + 1, canBuy)
+        #     if canBuy:
+        #         do = iterate(i + 1, False) - prices[i]
+        #     else:
+        #         do = iterate(i + 2, True) + prices[i]
+            
+        #     return max(skip, do)
+
+        # return iterate(0, True)
