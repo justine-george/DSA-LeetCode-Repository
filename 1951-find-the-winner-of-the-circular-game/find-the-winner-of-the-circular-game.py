0036513b7923b0435d34eb3deac2036046a71291
Justine George <justinegeo96@gmail.com>
2024-10-09 03:21:37 -0700

    Time: 112 ms (21.62%) | Memory: 16.6 MB (62.91%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -8,25 +8,25 @@ class Solution:
         #     return (helper(n - 1, k) + k) % n
         # return helper(n, k) + 1
 
-        # Iterative
-        # T: O(n), S: O(1)
-        res = 0
-        for people in range(2, n + 1):
-            res = (res + k) % people
-        return res + 1
+        # # Iterative
+        # # T: O(n), S: O(1)
+        # res = 0
+        # for people in range(2, n + 1):
+        #     res = (res + k) % people
+        # return res + 1
         
-        # # T: O(nk), S: O(n)
-        # q = deque()
-        # for i in range(1, n + 1):
-        #     q.append(i)
+        # T: O(nk), S: O(n)
+        q = deque([])
+        for i in range(1, n + 1):
+            q.append(i)
 
-        # while len(q) > 1:
-        #     # shift k - 1 times
-        #     for i in range(k - 1):
-        #         q.append(q.popleft())
+        while len(q) > 1:
+            # shift k - 1 times
+            for i in range(k - 1):
+                q.append(q.popleft())
             
-        #     # remove kth
-        #     q.popleft()
+            # remove kth
+            q.popleft()
 
-        # # last one remains is the winner
-        # return q[0]
+        # last one remains is the winner
+        return q[0]
