d344d8ca97f7af1795496e7a136f3f93b1f1f7df
Justine George <justinegeo96@gmail.com>
2024-10-09 00:10:49 -0700

    Time: 78 ms (21.51%) | Memory: 17.1 MB (40.45%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -6,9 +6,7 @@ class Solution:
         globalMax = curMin = curMax = nums[0]
 
         for num in nums[1:]:
-            choices = (num, curMin * num, curMax * num)
-            curMin = min(choices)
-            curMax = max(choices)
+            curMin, curMax = min(num, curMin * num, curMax * num), max(num, curMin * num, curMax * num)
             globalMax = max(globalMax, curMax)
         
         return globalMax
