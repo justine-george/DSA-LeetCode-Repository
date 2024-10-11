6feda598d38bb532e32679825a476d7b6f5ce9d0
Justine George <justinegeo96@gmail.com>
2024-10-10 21:25:09 -0700

    Time: 110 ms (55.34%) | Memory: 16.6 MB (72.71%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -3,18 +3,39 @@ class Solution:
         if n <= 5:
             return n
 
-        ugly = [1]
-        i2, i3, i5 = 0, 0, 0
-
-        for _ in range(1, n):
-            next_ugly = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
-            ugly.append(next_ugly)
-
-            if next_ugly == ugly[i2] * 2:
-                i2 += 1
-            if next_ugly == ugly[i3] * 3:
-                i3 += 1
-            if next_ugly == ugly[i5] * 5:
-                i5 += 1
+        ugly_heap = [1]
+        visited = set([1])
+        factors = [2, 3, 5]
         
-        return ugly[-1]
+        for i in range(n):
+            cur = heappop(ugly_heap)
+
+            for f in factors:
+                candidate = cur * f
+                if candidate not in visited:
+                    heappush(ugly_heap, candidate)
+                    visited.add(candidate)
+
+        return cur
+
+
+        # # dp solution
+        # if n <= 5:
+        #     return n
+
+        # ugly = [1]
+        
+        # i2, i3, i5 = 0, 0, 0
+
+        # for _ in range(1, n):
+        #     next_ugly = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
+        #     ugly.append(next_ugly)
+
+        #     if next_ugly == ugly[i2] * 2:
+        #         i2 += 1
+        #     if next_ugly == ugly[i3] * 3:
+        #         i3 += 1
+        #     if next_ugly == ugly[i5] * 5:
+        #         i5 += 1
+        
+        # return ugly[-1]
