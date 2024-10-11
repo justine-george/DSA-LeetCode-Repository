4b2263e27cb0cd3da44ca8f3e0a2d9c793db8bac
Justine George <justinegeo96@gmail.com>
2024-10-11 16:18:42 -0700

    Time: 87 ms (73.44%) | Memory: 16.6 MB (36.14%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,8 +1,22 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
+        # T: O(n), S: O(1) solution
+        ugly_list = [1]
+        i2 = i3 = i5 = 0
+        for i in range(n - 1):
+            next_ugly = min(ugly_list[i2] * 2, ugly_list[i3] * 3, ugly_list[i5] * 5)
+            ugly_list.append(next_ugly)
+
+            if next_ugly == ugly_list[i2] * 2:
+                i2 += 1
+            if next_ugly == ugly_list[i3] * 3:
+                i3 += 1
+            if next_ugly == ugly_list[i5] * 5:
+                i5 += 1
+        
+        return ugly_list[-1]
         '''
-        heap solution
-        '''
+        # heap solution
         min_heap = [1]
         visited = set([1])
         factors = [2, 3, 5]
@@ -16,3 +30,4 @@ class Solution:
                     heapq.heappush(min_heap, candidate)
 
         return last_popped
+        '''
