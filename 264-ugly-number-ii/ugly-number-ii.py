7ba9622c2e631981aa8e1385bf43d084299de227
Justine George <justinegeo96@gmail.com>
2024-10-10 21:31:29 -0700

    Time: 87 ms (73.43%) | Memory: 16.6 MB (36.12%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,42 +1,38 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        if n <= 5:
-            return n
-
-        ugly_heap = [1]
-        visited = set([1])
-        factors = [2, 3, 5]
+        # ugly_heap = [1]
+        # visited = set([1])
+        # factors = [2, 3, 5]
         
-        last_popped = 1
-        for i in range(n):
-            last_popped = heappop(ugly_heap)
+        # last_popped = 1
+        # for i in range(n):
+        #     last_popped = heappop(ugly_heap)
 
-            for f in factors:
-                candidate = last_popped * f
-                if candidate not in visited:
-                    heappush(ugly_heap, candidate)
-                    visited.add(candidate)
+        #     for f in factors:
+        #         candidate = last_popped * f
+        #         if candidate not in visited:
+        #             heappush(ugly_heap, candidate)
+        #             visited.add(candidate)
 
-        return last_popped
+        # return last_popped
 
-
-        # # dp solution
+        # dp solution
         # if n <= 5:
         #     return n
 
-        # ugly = [1]
+        ugly = [1]
         
-        # i2, i3, i5 = 0, 0, 0
+        i2, i3, i5 = 0, 0, 0
 
-        # for _ in range(1, n):
-        #     next_ugly = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
-        #     ugly.append(next_ugly)
+        for _ in range(1, n):
+            next_ugly = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
+            ugly.append(next_ugly)
 
-        #     if next_ugly == ugly[i2] * 2:
-        #         i2 += 1
-        #     if next_ugly == ugly[i3] * 3:
-        #         i3 += 1
-        #     if next_ugly == ugly[i5] * 5:
-        #         i5 += 1
+            if next_ugly == ugly[i2] * 2:
+                i2 += 1
+            if next_ugly == ugly[i3] * 3:
+                i3 += 1
+            if next_ugly == ugly[i5] * 5:
+                i5 += 1
         
-        # return ugly[-1]
+        return ugly[-1]
