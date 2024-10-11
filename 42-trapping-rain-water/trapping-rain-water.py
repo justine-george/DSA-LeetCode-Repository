c29c55b6e965852b4a62a58e7793dd8644d36daf
Justine George <justinegeo96@gmail.com>
2024-10-11 16:09:34 -0700

    Time: 136 ms (19.42%) | Memory: 20.6 MB (5.94%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,23 +1,46 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height) < 3:
+        N = len(height)
+        if N <= 2:
             return 0
 
-        l, r = 0, len(height) - 1
-        max_left, max_right = height[l], height[r]
-        total_water = 0
-        while l < r:
-            if max_left < max_right:
-                l += 1
-                water_collected = max_left - height[l]
-                if water_collected > 0:
-                    total_water += water_collected
-                max_left = max(max_left, height[l])
-            else:
-                r -= 1
-                water_collected = max_right - height[r]
-                if water_collected > 0:
-                    total_water += water_collected
-                max_right = max(max_right, height[r])
+        # calculate max height to the left and right
+        max_height_to_left_of = {0: -1}
+        max_height_to_right_of = {N - 1: -1}
 
-        return total_water
+        for i in range(1, N):
+            max_height_to_left_of[i] = max(max_height_to_left_of[i - 1], height[i - 1])
+        
+        for i in range(N - 2, -1, -1):
+            max_height_to_right_of[i] = max(max_height_to_right_of[i + 1], height[i + 1])
+
+        total_water_collected = 0
+        for i in range(1, N - 1):
+            cur_height = min(max_height_to_left_of[i], max_height_to_right_of[i])
+            cur_water = cur_height - height[i]
+            if cur_water > 0:
+                total_water_collected += cur_water
+
+        return total_water_collected
+        
+        # if len(height) < 3:
+        #     return 0
+
+        # l, r = 0, len(height) - 1
+        # max_left, max_right = height[l], height[r]
+        # total_water = 0
+        # while l < r:
+        #     if max_left < max_right:
+        #         l += 1
+        #         water_collected = max_left - height[l]
+        #         if water_collected > 0:
+        #             total_water += water_collected
+        #         max_left = max(max_left, height[l])
+        #     else:
+        #         r -= 1
+        #         water_collected = max_right - height[r]
+        #         if water_collected > 0:
+        #             total_water += water_collected
+        #         max_right = max(max_right, height[r])
+
+        # return total_water
