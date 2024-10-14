52598d0aa0dc64c901f01d73d4c356e653ef8f5a
Justine George <justinegeo96@gmail.com>
2024-10-14 01:24:18 -0700

    Time: 124 ms (53.01%) | Memory: 17.4 MB (33.82%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,33 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t or len(s) < len(t):
+        if len(s) < len(t):
             return ""
 
-        t_count = Counter(t)
-        window_count = Counter()
-        
-        matches = 0
-        required = len(t_count)
-        
+        t_freq_map = Counter(t)
+        window_freq_map = Counter()
+
         l = 0
         min_l, min_r = 0, float('inf')
+        have, need = 0, len(t_freq_map)
         
         for r in range(len(s)):
-            char = s[r]
-            if char in t_count:
-                window_count[char] += 1
-                if window_count[char] == t_count[char]:
-                    matches += 1
+            r_char = s[r]
+            window_freq_map[r_char] += 1
+            if r_char in t_freq_map:
+                if window_freq_map[r_char] == t_freq_map[r_char]:
+                    have += 1
             
-            while matches == required:
+            while have == need:
                 if r - l < min_r - min_l:
                     min_l, min_r = l, r
                 
-                left_char = s[l]
-                if left_char in t_count:
-                    window_count[left_char] -= 1
-                    if window_count[left_char] < t_count[left_char]:
-                        matches -= 1
+                l_char = s[l]
+                window_freq_map[l_char] -= 1
+                if l_char in t_freq_map:
+                    if window_freq_map[l_char] < t_freq_map[l_char]:
+                        have -= 1
                 l += 1
         
-        return s[min_l:min_r+1] if min_r != float('inf') else ""
+        return s[min_l:min_r + 1] if min_r != float('inf') else ""
