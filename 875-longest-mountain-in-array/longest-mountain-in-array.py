fc5261148680b344371afaf0e16e3e82fc286d1a
Justine George <justinegeo96@gmail.com>
2024-10-14 03:01:40 -0700

    Time: 126 ms (80.08%) | Memory: 18.1 MB (9.85%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.py
+++ 875-longest-mountain-in-array/longest-mountain-in-array.py
@@ -24,29 +24,3 @@ class Solution:
                 i += 1
         
         return max_length
-
-        '''
-        if len(arr) < 3:
-            return 0
-
-        peaks = []
-        for i in range(1, len(arr) - 1):
-            if arr[i - 1] < arr[i] and arr[i + 1] < arr[i]:
-                peaks.append(i)
-
-        max_length = 0
-        for peak_idx in peaks:
-            l = peak_idx - 1
-            while l >= 0 and arr[l] < arr[l + 1]:
-                l -= 1
-            l += 1
-            
-            r = peak_idx + 1
-            while r < len(arr) and arr[r] < arr[r - 1]:
-                r += 1
-            r -= 1
-
-            max_length = max(max_length, r - l + 1)
-
-        return max_length
-        '''
