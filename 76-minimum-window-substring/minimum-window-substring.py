080ad17c3f942808fc5ac8456b81d87e24af7a3a
Justine George <justinegeo96@gmail.com>
2024-10-14 01:25:22 -0700

    Time: 132 ms (49.09%) | Memory: 17.4 MB (8.09%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -13,9 +13,8 @@ class Solution:
         for r in range(len(s)):
             r_char = s[r]
             window_freq_map[r_char] += 1
-            if r_char in t_freq_map:
-                if window_freq_map[r_char] == t_freq_map[r_char]:
-                    have += 1
+            if r_char in t_freq_map and window_freq_map[r_char] == t_freq_map[r_char]:
+                have += 1
             
             while have == need:
                 if r - l < min_r - min_l:
@@ -23,9 +22,8 @@ class Solution:
                 
                 l_char = s[l]
                 window_freq_map[l_char] -= 1
-                if l_char in t_freq_map:
-                    if window_freq_map[l_char] < t_freq_map[l_char]:
-                        have -= 1
+                if l_char in t_freq_map and window_freq_map[l_char] < t_freq_map[l_char]:
+                    have -= 1
                 l += 1
         
         return s[min_l:min_r + 1] if min_r != float('inf') else ""
