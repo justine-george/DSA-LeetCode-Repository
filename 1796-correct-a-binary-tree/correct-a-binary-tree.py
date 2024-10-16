e0b469342056d256ecd1b0a9fec34c287a7c025c
Justine George <justinegeo96@gmail.com>
2024-10-16 06:28:37 -0700

    Time: 97 ms (67.90%) | Memory: 22.3 MB (38.27%) - LeetSync

--- 1796-correct-a-binary-tree/correct-a-binary-tree.py
+++ 1796-correct-a-binary-tree/correct-a-binary-tree.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def correctBinaryTree(self, root: TreeNode) -> TreeNode:
+        '''
         # BFS
         if not root:
             return None
         
         # (node, parent)
         q = deque([(root, None)])
+        visited = set()
         while q:
-            visited = set()
             for _ in range(len(q)):
                 node, parent = q.popleft()
                 
@@ -32,8 +33,8 @@ class Solution:
                     q.append((node.left, node))
         
         return root
-
         '''
+
         # DFS
         if not root:
             return None
@@ -43,21 +44,20 @@ class Solution:
         while stack:
             node, parent = stack.pop()
 
-            if node:
-                # Check if node's right child is in visited set
-                if node.right and node.right.val in visited:
-                    if parent:
-                        if parent.left == node:
-                            parent.left = None
-                        else:
-                            parent.right = None
-                    continue
-                
-                visited.add(node.val)
+            if node.right and node.right.val in visited:
+                if parent:
+                    if parent.left == node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                continue
+            
+            visited.add(node.val)
 
-                # Push left child first to the stack (postorder: right-left-root)
+            # Push left child first to the stack (postorder: right-left-root)
+            if node.left:
                 stack.append((node.left, node))
+            if node.right:
                 stack.append((node.right, node))
 
         return root
-        '''
