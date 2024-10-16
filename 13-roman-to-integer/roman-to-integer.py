bad1d7232a4b64593d46a81ccce7ca63150f60bd
Justine George <justinegeo96@gmail.com>
2024-10-15 22:54:52 -0700

    Time: 46 ms (59.37%) | Memory: 16.7 MB (15.59%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_map = {
+        roman_int_map = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -12,8 +12,9 @@ class Solution:
 
         res = 0
         for i in range(len(s)):
-            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
-                res -= roman_map[s[i]]
+            if i < len(s) - 1 and roman_int_map[s[i]] < roman_int_map[s[i + 1]]:
+                res -= roman_int_map[s[i]]
             else:
-                res += roman_map[s[i]]
+                res += roman_int_map[s[i]]
+        
         return res
