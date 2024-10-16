1158bd82f23b052fc63d3bb6cc3d12d836f371a1
Justine George <justinegeo96@gmail.com>
2024-10-16 02:30:37 -0700

    Time: 50 ms (30.60%) | Memory: 16.4 MB (80.00%) - LeetSync

--- 1083-two-sum-less-than-k/two-sum-less-than-k.py
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.py
@@ -1,20 +1,22 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        def binary_search(arr, i, l, r):
+        def binary_search_highest(arr, l, r, i, k):
             while l < r:
                 mid = l + (r - l + 1) // 2
-                if arr[mid] < k - arr[i]:
+                if arr[mid] + arr[i] < k:
                     l = mid
                 else:
                     r = mid - 1
-            return l if arr[l] < k - arr[i] else l - 1
+            return l if arr[l] + arr[i] < k else l - 1
 
         nums.sort()
         max_so_far = -1
         for i in range(len(nums) - 1):
-            idx = binary_search(nums, i, i + 1, len(nums) - 1)
+            if nums[i] >= k:
+                break
 
-            if idx > i:
-                max_so_far = max(max_so_far, nums[i] + nums[idx])
+            j = binary_search_highest(nums, i + 1, len(nums) - 1, i, k)
+            if j > i:
+                max_so_far = max(max_so_far, nums[i] + nums[j])
 
         return max_so_far
