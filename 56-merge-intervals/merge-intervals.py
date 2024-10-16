0fe42a466b96ae679f417d5270723d9ca32c4164
Justine George <justinegeo96@gmail.com>
2024-10-15 23:56:39 -0700

    Time: 126 ms (43.94%) | Memory: 21.2 MB (30.07%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,14 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0])
+        merged_list = [intervals[0]]
+        for i in range(1, len(intervals)):
+            prev_start, prev_end = merged_list[-1]
+            new_start, new_end = intervals[i]
 
-        new_list = [intervals[0]]
-        for cur_start, cur_end in intervals[1:]:
-            # if cur interval lies after prev interval
-            if cur_start > new_list[-1][1]:
-                new_list.append([cur_start, cur_end])
-            else:
-                new_list[-1][1] = max(new_list[-1][1], cur_end)
+            # check if overlap:
+            if prev_end >= new_start:
+                merged_list.pop()
+                new_start = min(prev_start, new_start)
+                new_end = max(prev_end, new_end)
+            
+            merged_list.append([new_start, new_end])
 
-        
-        return new_list
+        return merged_list
