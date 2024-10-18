b058afbf47c51c5c1d74ef395c2be22afb7e185b
Justine George <justinegeo96@gmail.com>
2024-10-17 18:53:39 -0700

    Time: 27 ms (100.00%) | Memory: 16.7 MB (57.90%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -3,31 +3,7 @@ class Solution:
         def custom_comparator(x):
             return (-x[0], -x[1])
         
-        N = len(nums)
-
-        counter_map = defaultdict(int)
-        for i in range(k):
-            counter_map[nums[i]] += 1
-        
-        res = []
-        
-        freq_list = [(freq, key) for key, freq in counter_map.items()]
-        freq_list.sort(key=custom_comparator)
-        sum = 0
-        for i in range(x):
-            if i < len(freq_list):
-                sum += (freq_list[i][0] * freq_list[i][1])
-        res.append(sum)
-
-        l = 0
-        r = k
-        print(f"{counter_map}, {res[-1]}")
-        while r < N:
-            counter_map[nums[r]] += 1
-            counter_map[nums[l]] -= 1
-            r += 1
-            l += 1
-
+        def compute_next_res(counter_map, res):
             freq_list = [(freq, key) for key, freq in counter_map.items()]
             freq_list.sort(key=custom_comparator)
             sum = 0
@@ -36,8 +12,22 @@ class Solution:
                     sum += (freq_list[i][0] * freq_list[i][1])
             res.append(sum)
 
-            print(f"{counter_map}, {res[-1]}")
+        counter_map = defaultdict(int)
+        for i in range(k):
+            counter_map[nums[i]] += 1
+        
+        res = []        
+        compute_next_res(counter_map, res)
 
+        l, r = 0, k
+        print(f"{counter_map}, {res[-1]}")
+        while r < len(nums):
+            counter_map[nums[r]] += 1
+            counter_map[nums[l]] -= 1
+            compute_next_res(counter_map, res)
+
+            r += 1
+            l += 1
         
         return res
                 
