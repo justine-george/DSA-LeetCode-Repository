b9fb24cb8eb7bb57b4f2b82e42b3ce8dfd2e99ca
Justine George <justinegeo96@gmail.com>
2024-10-17 19:56:30 -0700

    Time: 32 ms (100.00%) | Memory: 16.6 MB (89.28%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,28 +1,8 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        def custom_comparator(x):
-            return (-x[0], -x[1])
-        
-        def compute_next_res(counter_map, res):
-            freq_list = [(freq, key) for key, freq in counter_map.items()]
-            freq_list.sort(key=custom_comparator)
-            x_sum= sum(freq_list[i][0] * freq_list[i][1] for i in range(x) if i < len(freq_list))
-            res.append(x_sum)
-
-        counter_map = defaultdict(int)
-        for i in range(k):
-            counter_map[nums[i]] += 1
-        
-        res = []
-        compute_next_res(counter_map, res)
-
-        l, r = 0, k
-        while r < len(nums):
-            counter_map[nums[r]] += 1
-            counter_map[nums[l]] -= 1
-            compute_next_res(counter_map, res)
-
-            l, r = l + 1, r + 1
-        
-        return res
-                
+        def do_sum(i):
+            ctr = Counter(nums[i:i + k])
+            most_freq = heapq.nlargest(x, ctr, key=lambda y: (ctr[y], y))
+            return sum(y * ctr[y] for y in most_freq)
+            
+        return list(map(do_sum, range(len(nums) - k + 1)))
