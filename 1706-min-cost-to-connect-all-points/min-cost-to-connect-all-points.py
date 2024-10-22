ac9b9e74567c99faa60ee243767408814fa6bef7
Justine George <justinegeo96@gmail.com>
2024-10-21 23:12:58 -0700

    Time: 1379 ms (56.18%) | Memory: 154.5 MB (30.91%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,23 +1,32 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # cost, points index
-        min_heap = [(0, 0)]
+        N = len(points)
+        # completely connected graph, all other points are neighbors
+        # { i: [(dist, j)]} means i->j costs dist
+        adj = { i: [] for i in range(N)}
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                manh_dist = abs(x2-x1) + abs(y2-y1)
+                adj[i].append((manh_dist, j))
+                adj[j].append((manh_dist, i))
+        
         visited = set()
         total_cost = 0
+        # cost, points index
+        min_heap = [(0, 0)]
 
-        while min_heap:
-            cost, index = heapq.heappop(min_heap)
-            if index in visited:
+        while len(visited) < N:
+            cost, i = heapq.heappop(min_heap)
+            if i in visited:
                 continue
             
-            visited.add(index)
             total_cost += cost
-            cur_x, cur_y = points[index]
+            visited.add(i)
 
-            # completely connected graph, all other points are neighbors
-            for i, (x, y) in enumerate(points):
-                if i not in visited:
-                    manh_dist = abs(cur_x - x) + abs(cur_y - y)
-                    heapq.heappush(min_heap, (manh_dist, i))
+            for neiCost, nei in adj[i]:
+                if nei not in visited:
+                    heapq.heappush(min_heap, (neiCost, nei))
         
         return total_cost
