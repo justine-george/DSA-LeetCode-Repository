75af797b523f5b6bd3c144551753534b0256d9b9
Justine George <justinegeo96@gmail.com>
2024-10-22 01:24:39 -0700

    Time: 31 ms (94.30%) | Memory: 18.3 MB (5.00%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,26 +1,26 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # DP solution: T: O(n^2)
+        # DP solution: T: O(n^2), S: O(n^2)
         cache = {}
         def dfs(i, k1, k2):
             if (k1, k2) in cache:
                 return cache[(k1, k2)]
             
-            if i == len(costs):
+            if i < 0:
                 return 0
             
             l = float('inf')
             r = float('inf')
 
             if k1 < len(costs) // 2:
-                l = costs[i][0] + dfs(i + 1, k1 + 1, k2)
+                l = costs[i][0] + dfs(i - 1, k1 + 1, k2)
             if k2 < len(costs) // 2:
-                r = costs[i][1] + dfs(i + 1, k1, k2 + 1)
+                r = costs[i][1] + dfs(i - 1, k1, k2 + 1)
             
             cache[(k1, k2)] = min(l, r)
             return cache[(k1, k2)]
         
-        return dfs(0, 0, 0)
+        return dfs(len(costs) - 1, 0, 0)
 
         # # Greedy solution, T: O(nlogn), S: O(n)
         # # how much extra to send each of them to city_a
