cafed61d1a452bae40c36c302366e54bcdea8a79
Justine George <justinegeo96@gmail.com>
2024-10-22 01:47:03 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (12.32%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,23 +1,36 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # 2d dp bottom up
-        n = len(costs) // 2
-        # min cost to send i people to A and j people to B
-        dp = [[0] * (n + 1) for i in range(n + 1)]
+        # Greedy solution, T: O(nlogn), S: O(n)
+        # how much extra to send each of them to city_a
+        diff = [(cost_a - cost_b, cost_a, cost_b) for cost_a, cost_b in costs]
+        diff.sort()
+    
+        total_cost = 0
+        for i in range(len(diff)):
+            if i < len(diff) // 2:
+                total_cost += diff[i][1]
+            else:
+                total_cost += diff[i][2]
+        return total_cost
+        
+        # # 2d dp bottom up
+        # n = len(costs) // 2
+        # # min cost to send i people to A and j people to B
+        # dp = [[0] * (n + 1) for i in range(n + 1)]
 
-        for i in range(1, n + 1):
-            # Send i people to city A (when city B has no people
-            dp[i][0] = dp[i - 1][0] + costs[i - 1][0]
-            # Send i people to city B (when city A has no people
-            dp[0][i] = dp[0][i - 1] + costs[i - 1][1]
+        # for i in range(1, n + 1):
+        #     # Send i people to city A (when city B has no people
+        #     dp[i][0] = dp[i - 1][0] + costs[i - 1][0]
+        #     # Send i people to city B (when city A has no people
+        #     dp[0][i] = dp[0][i - 1] + costs[i - 1][1]
 
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                fromA = dp[i - 1][j] + costs[i+j-1][0]
-                fromB = dp[i][j - 1] + costs[i+j-1][1]
-                dp[i][j] = min(fromA, fromB)
+        # for i in range(1, n + 1):
+        #     for j in range(1, n + 1):
+        #         fromA = dp[i - 1][j] + costs[i+j-1][0]
+        #         fromB = dp[i][j - 1] + costs[i+j-1][1]
+        #         dp[i][j] = min(fromA, fromB)
 
-        return dp[n][n]
+        # return dp[n][n]
         
         # # DP solution: T: O(n^2), S: O(n^2)
         # cache = {}
@@ -40,16 +53,3 @@ class Solution:
         #     return cache[(k1, k2)]
         
         # return dfs(0, len(costs) // 2, len(costs) // 2)
-
-        # # Greedy solution, T: O(nlogn), S: O(n)
-        # # how much extra to send each of them to city_a
-        # diff = [(cost_a - cost_b, cost_a, cost_b) for cost_a, cost_b in costs]
-        # diff.sort()
-    
-        # total_cost = 0
-        # for i in range(len(diff)):
-        #     if i < len(diff) // 2:
-        #         total_cost += diff[i][1]
-        #     else:
-        #         total_cost += diff[i][2]
-        # return total_cost
