8f58972dc60f2b09406e0815d8aa3dde4920ab97
Justine George <justinegeo96@gmail.com>
2024-10-21 23:33:18 -0700

    Time: 680 ms (83.51%) | Memory: 80.3 MB (83.65%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,32 +1,23 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         N = len(points)
-        min_cost = [float('inf')] * N
-        min_cost[0] = 0 # starting from 1st point
-        connected = set()
+        min_cost_heap = [(0, 0)]  # (cost, point_index)
+        in_mst = set()
         total_cost = 0
 
-        for _ in range(N):
-            cur_min_cost = float('inf')
-            cur_min_point = -1
-
-            # Find the next point to connect to MST
-            for i in range(N):
-                if i not in connected and min_cost[i] < cur_min_cost:
-                    cur_min_cost = min_cost[i]
-                    cur_min_point = i
+        while len(in_mst) < N:
+            cost, i = heapq.heappop(min_cost_heap)
+            if i in in_mst:
+                continue
             
-            # Add the selected point to MST
-            total_cost += cur_min_cost
-            connected.add(cur_min_point)
+            total_cost += cost
+            in_mst.add(i)
 
-            # Update the cost array with paths from the current point
-            cur_x, cur_y = points[cur_min_point]
             for j in range(N):
-                if j not in connected:
-                    x, y = points[j]
-                    manh_dist = abs(x - cur_x) + abs(y - cur_y)
-                    if manh_dist < min_cost[j]:
-                        min_cost[j] = manh_dist
+                if j not in in_mst:
+                    x1, y1 = points[i]
+                    x2, y2 = points[j]
+                    manh_dist = abs(x1 - x2) + abs(y1 - y2)
+                    heapq.heappush(min_cost_heap, (manh_dist, j))
 
         return total_cost
