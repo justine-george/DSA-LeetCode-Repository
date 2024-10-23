5c79e375a5b5cc5c956148cb1ab16e2204073543
Justine George <justinegeo96@gmail.com>
2024-10-23 14:58:30 -0700

    Time: 62 ms (33.07%) | Memory: 19.8 MB (84.65%) - LeetSync

---
+++ 1634-clone-n-ary-tree/clone-n-ary-tree.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        # clone kids
+        # clone this node, and add kids to children
+        # return new node
+
+        newChildren = []
+        for child in root.children:
+            newChildren.append(self.cloneTree(child))
+        newRoot = Node(root.val, newChildren)
+        return newRoot
