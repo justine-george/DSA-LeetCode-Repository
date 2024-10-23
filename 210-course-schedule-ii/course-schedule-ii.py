1cb06499d849e3604c26ab3fc464030f4145de45
Justine George <justinegeo96@gmail.com>
2024-10-23 02:15:17 -0700

    Time: 0 ms (100.00%) | Memory: 18.4 MB (66.78%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,52 +1,52 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # Both approaches have same time and space complexity
-        # T: O(n), S: O(n)
+        # # Both approaches have same time and space complexity
+        # # T: O(n), S: O(n)
 
-        next_map = {i: [] for i in range(numCourses)}
-
-        for crs, pre in prerequisites:
-            next_map[pre].append(crs)
-        
-        # {i: False (visited) | True (in the current path) | no key (not visited)}
-        visit = {}
-
-        # post-order dfs, topological sort
-        def dfs(i):
-            if i in visit:
-                return visit[i]
-            
-            visit[i] = True
-            for next_crs in next_map[i]:
-                if dfs(next_crs): # cycle detected
-                    return True
-            res.append(i)
-            visit[i] = False
-
-        res = []
-        for i in next_map:
-            if dfs(i): # cycle detected
-                return []
-        
-        res.reverse()
-        return res
-
-        # next_map = {i: set() for i in range(numCourses)}
-        # indegree = [0] * numCourses
+        # next_map = {i: [] for i in range(numCourses)}
 
         # for crs, pre in prerequisites:
-        #     next_map[pre].add(crs)
-        #     indegree[crs] += 1
+        #     next_map[pre].append(crs)
         
-        # q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        # # {i: False (visited) | True (in the current path) | no key (not visited)}
+        # visit = {}
+
+        # # post-order dfs, topological sort
+        # def dfs(i):
+        #     if i in visit:
+        #         return visit[i]
+            
+        #     visit[i] = True
+        #     for next_crs in next_map[i]:
+        #         if dfs(next_crs): # cycle detected
+        #             return True
+        #     res.append(i)
+        #     visit[i] = False
+
         # res = []
-        # while q:
-        #     crs = q.popleft()
-        #     res.append(crs)
+        # for i in next_map:
+        #     if dfs(i): # cycle detected
+        #         return []
+        
+        # res.reverse()
+        # return res
 
-        #     for next_crs in next_map[crs]:
-        #         indegree[next_crs] -= 1
-        #         if indegree[next_crs] == 0:
-        #             q.append(next_crs)
+        next_map = {i: set() for i in range(numCourses)}
+        indegree = [0] * numCourses
 
-        # return res if len(res) == numCourses else []
+        for crs, pre in prerequisites:
+            next_map[pre].add(crs)
+            indegree[crs] += 1
+        
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        res = []
+        while q:
+            crs = q.popleft()
+            res.append(crs)
+
+            for next_crs in next_map[crs]:
+                indegree[next_crs] -= 1
+                if indegree[next_crs] == 0:
+                    q.append(next_crs)
+
+        return res if len(res) == numCourses else []
