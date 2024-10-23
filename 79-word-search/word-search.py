d7b82d1227b1c56233e04552213078d7419cf8ea
Justine George <justinegeo96@gmail.com>
2024-10-22 19:54:19 -0700

    Time: 8208 ms (5.01%) | Memory: 16.7 MB (43.81%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,39 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        visit = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def backtrack(r, c, i):
+            if i == len(word):
+                return True
+
+            if r not in range(m) or c not in range(n) or board[r][c] != word[i] or (r, c) in visit:
+                return False
+
+            visit.add((r, c))
+            
+            res = False
+            for dr, dc in directions:
+                res |= backtrack(r + dr, c + dc, i + 1)
+
+            # for dr, dc in directions:
+            #     if backtrack(r + dr, c + dc, i + 1):
+            #         res = True
+            #         break
+
+            # res = (backtrack(r + 1, c, i + 1) or
+            #     backtrack(r - 1, c, i + 1) or
+            #     backtrack(r, c + 1, i + 1) or
+            #     backtrack(r, c - 1, i + 1))
+
+            visit.remove((r, c))
+
+            return res
+        
+        for i in range(m):
+            for j in range(n):
+                if backtrack(i, j, 0):
+                    return True
+        
+        return False
