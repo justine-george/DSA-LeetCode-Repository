aa7124d3af70201d6679c4e9ab092b0069fb54b3
Justine George <justinegeo96@gmail.com>
2024-10-24 08:59:53 -0700

    Time: 303 ms (12.61%) | Memory: 16.5 MB (50.65%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,27 +1,26 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        N = len(pattern)
-        res = []
-        stack = []
-        for i in range(N + 1):
-            stack.append(i + 1)
-
-            if i == N or pattern[i] == 'I':
-                while stack:
-                    res.append(str(stack.pop()))
-        
-        return "".join(res)
-
-        
-        
         # N = len(pattern)
-        # for t in permutations(range(1, N + 2)):
-        #     for i in range(N):
-        #         if t[i] > t[i + 1]:
-        #             if pattern[i] == "I":
-        #                 break
-        #         else:
-        #             if pattern[i] == "D":
-        #                 break
-        #     else:
-        #         return "".join(map(str, t))
+        # res = []
+        # stack = []
+        # for i in range(N + 1):
+        #     stack.append(i + 1)
+
+        #     if i == N or pattern[i] == 'I':
+        #         while stack:
+        #             res.append(str(stack.pop()))
+        
+        # return "".join(res)
+
+        
+        N = len(pattern)
+        for t in permutations(range(1, N + 2)):
+            for i in range(N):
+                if t[i] > t[i + 1]:
+                    if pattern[i] == "I":
+                        break
+                else:
+                    if pattern[i] == "D":
+                        break
+            else:
+                return "".join([str(n) for n in t])
