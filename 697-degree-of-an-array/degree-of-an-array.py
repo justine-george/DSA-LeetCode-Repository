de5d5afb1fcd00a9d877d55a66093dc573c2dab3
Justine George <justinegeo96@gmail.com>
2024-10-23 21:25:56 -0700

    Time: 968 ms (10.80%) | Memory: 18.3 MB (31.46%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.py
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -1,31 +1,31 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        count = defaultdict(int)
-        first_occur_map = {}
+        # count = defaultdict(int)
+        # first_occur_map = {}
 
-        min_len = float('inf')
-        max_freq = 0
-        for i in range(len(nums)):
-            # first_occur_map.setdefault(nums[i], i)
-            if nums[i] not in first_occur_map:
-                first_occur_map[nums[i]] = i
-            count[nums[i]] += 1
+        # min_len = float('inf')
+        # max_freq = 0
+        # for i in range(len(nums)):
+        #     # first_occur_map.setdefault(nums[i], i)
+        #     if nums[i] not in first_occur_map:
+        #         first_occur_map[nums[i]] = i
+        #     count[nums[i]] += 1
 
-            if count[nums[i]] > max_freq:
-                max_freq = count[nums[i]]
-                min_len = i - first_occur_map[nums[i]] + 1
-            elif count[nums[i]] == max_freq:
-                min_len = min(min_len, i - first_occur_map[nums[i]] + 1)
+        #     if count[nums[i]] > max_freq:
+        #         max_freq = count[nums[i]]
+        #         min_len = i - first_occur_map[nums[i]] + 1
+        #     elif count[nums[i]] == max_freq:
+        #         min_len = min(min_len, i - first_occur_map[nums[i]] + 1)
         
-        return min_len
+        # return min_len
         
-        '''
+
         # naive solution
         freq_map = Counter(nums)
         
         max_nums, max_count = [], max(freq_map.values())
         for num in freq_map:
-            if freq_map[num] >= max_count:
+            if freq_map[num] == max_count:
                 max_count = freq_map[num]
                 max_nums.append(num)
 
@@ -46,4 +46,3 @@ class Solution:
             min_size = min(min_size, last_index - first_index + 1)
         
         return min_size
-        '''
