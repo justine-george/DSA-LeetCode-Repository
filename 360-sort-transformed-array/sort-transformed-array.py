a290fd3a527db7214f63a356ca39f2fb4f718135
Justine George <justinegeo96@gmail.com>
2024-10-25 01:23:47 -0700

    Time: 3 ms (96.83%) | Memory: 16.7 MB (13.03%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -3,4 +3,31 @@ class Solution:
         def get_val(x, a, b, c):
             return (a * x * x) + (b * x) + c
         
-        return sorted([get_val(n, a, b, c) for n in nums])
+        res = []
+        l, r = 0, len(nums) - 1
+
+        if a < 0:
+            # downward parabola, we put smaller edge element first
+            while l <= r:
+                left_val = get_val(nums[l], a, b, c)
+                right_val = get_val(nums[r], a, b, c)
+                if left_val < right_val:
+                    res.append(left_val)
+                    l += 1
+                else:
+                    res.append(right_val)
+                    r -= 1
+        else:
+            # upward parabola/straight line, we put bigger edge element first
+            while l <= r:
+                left_val = get_val(nums[l], a, b, c)
+                right_val = get_val(nums[r], a, b, c)
+                if left_val > right_val:
+                    res.append(left_val)
+                    l += 1
+                else:
+                    res.append(right_val)
+                    r -= 1
+            res.reverse()
+
+        return res
