35d728695c61e854d039531923848446cc836fe0
Justine George <justinegeo96@gmail.com>
2024-10-27 17:33:13 -0700

    Time: 45 ms (84.48%) | Memory: 20.7 MB (10.58%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,34 +1,34 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        rank_count_map = Counter(ranks)
-        l = 1
-        r = min(rank_count_map) * cars * cars
-        while l < r:
-            m = l + (r - l) // 2
-            if sum((isqrt(m // rank) * rank_count_map[rank]) for rank in rank_count_map) >= cars:
-                r = m
-            else:
-                l = m + 1
-        return l
+        # rank_count_map = Counter(ranks)
+        # l = 1
+        # r = min(rank_count_map) * cars * cars
+        # while l < r:
+        #     m = l + (r - l) // 2
+        #     if sum((isqrt(m // rank) * rank_count_map[rank]) for rank in rank_count_map) >= cars:
+        #         r = m
+        #     else:
+        #         l = m + 1
+        # return l
+
+        rank_counter = Counter(ranks)
         
-        '''
         def check(threshold):
             # rank * n * n = time
             # see if sum of n's obtained from substituting time threshold in the equation above is greater than cars count
             count = 0
-            for r in ranks:
-                count += int(sqrt(threshold / r))
+            for rank, freq in rank_counter.items():
+                count += int(sqrt(threshold / rank)) * freq
             return count >= cars
         
 
         l, r = 1, int(100 * 1e6 * 1e6)
-        while l <= r:
+        while l < r:
             m = l + (r - l) // 2
             # if condition is satisfied, update res and check for an even smaller val
             if check(m):
-                r = m - 1
+                r = m
             else:
                 l = m + 1
 
         return l
-        '''
