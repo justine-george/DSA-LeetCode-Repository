d7af868fe02a68d8b568d44efb681aea845d20f8
Justine George <justinegeo96@gmail.com>
2024-10-27 17:36:43 -0700

    Time: 17 ms (94.83%) | Memory: 20.6 MB (21.16%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,34 +1,22 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        # rank_count_map = Counter(ranks)
-        # l = 1
-        # r = min(rank_count_map) * cars * cars
-        # while l < r:
-        #     m = l + (r - l) // 2
-        #     if sum((isqrt(m // rank) * rank_count_map[rank]) for rank in rank_count_map) >= cars:
-        #         r = m
-        #     else:
-        #         l = m + 1
-        # return l
-
-        rank_counter = Counter(ranks)
-        
-        def check(threshold):
-            # rank * n * n = time
-            # see if sum of n's obtained from substituting time threshold in the equation above is greater than cars count
-            count = 0
-            for rank, freq in rank_counter.items():
-                count += int(sqrt(threshold / rank)) * freq
-            return count >= cars
-        
-
+        self.rank_counter = Counter(ranks)
         l, r = 1, int(min(ranks) * cars * cars)
+        
         while l < r:
             m = l + (r - l) // 2
             # if condition is satisfied, update res and check for an even smaller val
-            if check(m):
+            if self.check(cars, m):
                 r = m
             else:
                 l = m + 1
 
         return l
+    
+    def check(self, cars, threshold):
+        # rank * n * n = time
+        # see if sum of n's obtained from substituting time threshold in the equation above is greater than cars count
+        count = 0
+        for rank, freq in self.rank_counter.items():
+            count += int(sqrt(threshold / rank)) * freq
+        return count >= cars
