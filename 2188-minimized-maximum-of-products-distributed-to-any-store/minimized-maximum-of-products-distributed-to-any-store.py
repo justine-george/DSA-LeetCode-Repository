29bcaf345d60df5e909457e205eb8303f12b214d
Justine George <justinegeo96@gmail.com>
2024-10-30 04:49:05 +0530

    Time: 485 ms (98.46%) | Memory: 28.6 MB (91.06%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        '''
+        n stores
+        m prod types
+        quantities[i] -> qty of prod type i
+
+        goal: all quantities should be distributed to all stores, minimizing max no. of prod 'x' given to any store.
+
+        intuition:
+            binary search, try all x from 1 to max(product), for each iteration, iterate over quantities, take count of how many stores can it serve. success is when the total count of stores it can serve should be >= n. then minimize 'x'
+        '''
+
+        def check(x):
+            store_count = 0
+            for q in quantities:
+                store_count += ceil(q/x)
+                # if store_count > n:
+                #     return False
+            # return True
+            return store_count <= n
+
+
+        l, r = 1, max(quantities)
+        while l < r:
+            m = l + (r - l) // 2
+            if check(m):
+                r = m
+            else:
+                l = m + 1
+
+        return l
