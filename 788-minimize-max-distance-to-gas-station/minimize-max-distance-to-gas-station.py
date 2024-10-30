53fc4558c58d4a5cacea33019a927d38db5b05f9
Justine George <justinegeo96@gmail.com>
2024-10-30 20:27:09 +0530

    Time: 233 ms (73.53%) | Memory: 17 MB (38.46%) - LeetSync

---
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minmaxGasDist(self, stations: List[int], k: int) -> float:
+        def check(delta):
+            new_station_count = 0
+            for i in range(len(stations) - 1):
+                gap = stations[i + 1] - stations[i]
+                # total number of new stations added
+                new_station_count += ceil(gap / delta) - 1
+                if new_station_count > k:
+                    return False
+            return new_station_count <= k
+
+
+        initialMaxDist = max([stations[i + 1] - stations[i] for i in range(len(stations) - 1)])
+        l, r = 1e-6, initialMaxDist
+        while r - l > 1e-6:
+            m = l + (r - l) / 2
+            if check(m):
+                r = m
+            else:
+                l = m
+        return l
