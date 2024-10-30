8fb731313391b53cef454cdecb0c820fbc9e16d1
Justine George <justinegeo96@gmail.com>
2024-10-30 05:38:48 +0530

    Time: 22 ms (88.57%) | Memory: 20.5 MB (29.69%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,12 +1,14 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
+        ranks_count = Counter(ranks)
+
         def check(time):
             cars_repaired = 0
-            for r in ranks:
-                cars_repaired += int(sqrt(time/r))
+            for rank, freq in ranks_count.items():
+                cars_repaired += freq * int(sqrt(time/rank))
             return cars_repaired >= cars
         
-        l, r = 1, min(ranks) * cars * cars
+        l, r = 1, max(ranks) * cars * cars
         while l < r:
             m = l + (r - l) // 2
             if check(m):
