d447180ba547e5bc322f3e16e9d4271438a6f53c
Justine George <justinegeo96@gmail.com>
2024-10-30 05:34:29 +0530

    Time: 887 ms (35.71%) | Memory: 20.7 MB (11.46%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,22 +1,16 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        self.rank_counter = Counter(ranks)
-        l, r = 1, int(min(ranks) * cars * cars)
+        def check(time):
+            cars_repaired = 0
+            for r in ranks:
+                cars_repaired += int(sqrt(time/r))
+            return cars_repaired >= cars
         
+        l, r = 1, int(1e14)
         while l < r:
             m = l + (r - l) // 2
-            # if condition is satisfied, update res and check for an even smaller val
-            if self.check(cars, m):
+            if check(m):
                 r = m
             else:
                 l = m + 1
-
         return l
-    
-    def check(self, cars, threshold):
-        # rank * n * n = time
-        # see if sum of n's obtained from substituting time threshold in the equation above is greater than cars count
-        count = 0
-        for rank, freq in self.rank_counter.items():
-            count += int(sqrt(threshold / rank)) * freq
-        return count >= cars
