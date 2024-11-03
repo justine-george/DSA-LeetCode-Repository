789eb0346eec627f5c8c11668a38fc25663c5e40
Justine George <justinegeo96@gmail.com>
2024-11-03 19:29:05 +0530

    Time: 184 ms (59.87%) | Memory: 16.8 MB (31.52%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -3,37 +3,38 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def is_move_valid(r, c, new_num):
-            # check current row and col
-            for i in range(9):
-                if board[r][i] == new_num or board[i][c] == new_num:
-                    return False
-
-            # check 3x3
-            start_row, start_col = 3 * (r // 3), 3 * (c // 3)
-            for i in range(3):
-                for j in range(3):
-                    if board[start_row + i][start_col + j] == new_num:
-                        return False
-            
-            return True
-
-        rset = {i: set() for i in range(9)}
-        cset = {i: set() for i in range(9)}
-        gset = {(r, c) for r in range(3) for c in range(3)}
         def solve():
             for r in range(9):
                 for c in range(9):
                     if board[r][c] == '.':
                         for new_num in map(str, range(1, 10)):
-                            if is_move_valid(r, c, new_num):
+                            if new_num not in rset[r] and new_num not in cset[c] and new_num not in gset[(r // 3, c // 3)]:
+                            # if is_move_valid(r, c, new_num):
                                 board[r][c] = new_num
+                                rset[r].add(new_num)
+                                cset[c].add(new_num)
+                                gset[(r // 3, c // 3)].add(new_num)
                                 
                                 if solve():
                                     return True
                                 
                                 board[r][c] = '.'
+                                rset[r].remove(new_num)
+                                cset[c].remove(new_num)
+                                gset[(r // 3, c // 3)].remove(new_num)
                         return False # if all tried and not solved
             return True # solved
+
+        # populate rset, cset, gset before solving the board
+        rset = {i: set() for i in range(9)}
+        cset = {i: set() for i in range(9)}
+        gset = {(r, c): set() for r in range(3) for c in range(3)}
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != '.':
+                    new_num = board[r][c]
+                    rset[r].add(new_num)
+                    cset[c].add(new_num)
+                    gset[(r // 3, c // 3)].add(new_num)
 
         solve()
