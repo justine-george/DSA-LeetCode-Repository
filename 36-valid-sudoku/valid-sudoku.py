dfed8a11f1939f72eac9aad0b3417d45c13e55b3
Justine George <justinegeo96@gmail.com>
2024-11-03 17:21:28 +0530

    Time: 7 ms (39.87%) | Memory: 16.6 MB (70.05%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,15 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rset = {i: [] for i in range(9)}
-        cset = {i: [] for i in range(9)}
-        gset = {(i, j): [] for i in range(3) for j in range(3)}
+        rset = {i: set() for i in range(9)}
+        cset = {i: set() for i in range(9)}
+        gset = {(i, j): set() for i in range(3) for j in range(3)}
         for r in range(9):
             for c in range(9):
                 cur = board[r][c]
                 if cur != '.':
                     if cur in rset[r] or cur in cset[c] or cur in gset[(r // 3, c // 3)]:
                        return False
-                    rset[r].append(cur)
-                    cset[c].append(cur)
-                    gset[(r // 3, c // 3)].append(cur)
+                    rset[r].add(cur)
+                    cset[c].add(cur)
+                    gset[(r // 3, c // 3)].add(cur)
         return True
