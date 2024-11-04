6a790cb633ef32009424098fa8673599ff0f96b7
Justine George <justinegeo96@gmail.com>
2024-11-04 13:10:07 +0530

    Time: 7 ms (6.03%) | Memory: 16.8 MB (13.52%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -6,15 +6,9 @@ class Solution:
         i = 0
         while i < len(a) or i < len(b):
             ans = (int(rev_a[i]) if i < len(a) else 0) + (int(rev_b[i]) if i < len(b) else 0) + carry
-            if ans == 2:
-                ans = 0
-                carry = 1
-            elif ans == 3:
-                ans = 1
-                carry = 1
-            else:
-                carry = 0
-
+            carry = 1 if ans in [2, 3] else 0
+            if ans == 2 or ans == 3:
+                ans = 1 if ans == 3 else 0
             res.append(ans)
             i += 1
         
