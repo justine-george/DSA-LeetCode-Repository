5961377541c1fcdb89aae7241ae015ab30c68e9b
Justine George <justinegeo96@gmail.com>
2024-11-05 17:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (13.37%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        min_prefix_sum = nums[0]
-        prefix_sum = nums[0]
-        for n in nums[1:]:
-            prefix_sum += n
-            min_prefix_sum = min(min_prefix_sum, prefix_sum)
-
-        return 1 - min_prefix_sum if min_prefix_sum < 0 else 1
+        def check(val):
+            temp = val
+            for n in nums:
+                temp += n
+                if temp < 1:
+                    return False
+            return True
+        
+        l, r = 1, int(1e5)
+        while l < r:
+            m = l + (r - l) // 2
+            if check(m):
+                r = m
+            else:
+                l = m + 1
+        return l
