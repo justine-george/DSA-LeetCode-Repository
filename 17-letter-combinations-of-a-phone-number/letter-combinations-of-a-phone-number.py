c126d166375ffcf3a98e52eca2d20744310f7000
Justine George <justinegeo96@gmail.com>
2024-11-06 16:20:12 +0530

    Time: 3 ms (5.60%) | Memory: 16.7 MB (29.11%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -4,14 +4,14 @@ class Solution:
             return []
 
         map = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', "z"]
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
 
         res = []
@@ -22,9 +22,9 @@ class Solution:
             
             current_digit = digits[i]
             for letter in map[current_digit]:
-                # path.append(letter)
-                backtrack(i + 1, path + [letter])
-                # path.pop()
+                path.append(letter)
+                backtrack(i + 1, path)
+                path.pop()
 
         backtrack(0, [])
         return res
