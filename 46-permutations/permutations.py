8429ce5d3193583126afeba58367952dcb7ef5f2
Justine George <justinegeo96@gmail.com>
2024-11-06 10:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (56.49%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,13 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for num in nums:
-            new_res = []
-            for perm in res:
-                for i in range(len(perm) + 1):
-                    # insert num at every possible position
-                    new_perm = perm[:i] + [num] + perm[i:]
-                    print(new_perm)
-                    new_res.append(new_perm)
-            res = new_res
+        if len(nums) == 0:
+            return [[]]
+        
+        res = []
+
+        perms = self.permute(nums[1:])
+        # insert nums[0] at every position
+        for p in perms:
+            for i in range(len(p) + 1):
+                new_p = p[:i] + [nums[0]] + p[i:]
+                res.append(new_p)
+
         return res
