378b97f529989f02092ce82d127ca8ba3b95a6f6
Justine George <justinegeo96@gmail.com>
2024-11-06 17:47:01 +0530

    Time: 4910 ms (29.37%) | Memory: 16.7 MB (53.40%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,36 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        visit = set()
+        rowRange, colRange = set(range(0, m)), set(range(0, n))
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        def backtrack(r, c, i):
+        visited = set()
+        def dfs(r, c, i):
             if i == len(word):
                 return True
-
-            if r not in range(m) or c not in range(n) or board[r][c] != word[i] or (r, c) in visit:
-                return False
-
-            visit.add((r, c))
             
-            # res = False
-            # for dr, dc in directions:
-            #     if backtrack(r + dr, c + dc, i + 1):
-            #         res = True
-            #         break
+            if r not in rowRange or c not in colRange or (r, c) in visited or board[r][c] != word[i]:
+                return False
+            
+            visited.add((r, c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if dfs(nr, nc, i + 1):
+                    return True
+            visited.remove((r, c))
+            return False
 
-            res = (backtrack(r + 1, c, i + 1) or
-                backtrack(r - 1, c, i + 1) or
-                backtrack(r, c + 1, i + 1) or
-                backtrack(r, c - 1, i + 1))
 
-            visit.remove((r, c))
-
-            return res
-        
         for i in range(m):
             for j in range(n):
-                if backtrack(i, j, 0):
+                if dfs(i, j, 0):
                     return True
-        
         return False
