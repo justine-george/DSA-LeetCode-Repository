a01b72a42762e36e0298f9744388985724074532
Justine George <justinegeo96@gmail.com>
2024-11-11 17:20:48 +0530

    Time: 1 ms (79.06%) | Memory: 16.9 MB (18.31%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -8,7 +8,6 @@ class Solution:
         for char, freq in balloon_freq_map.items():
             if char not in text_freq_map or text_freq_map[char] < balloon_freq_map[char]:
                 return 0
-
             no_of_balloons = min(no_of_balloons, text_freq_map[char] // freq)
         
         return no_of_balloons
