7bb806be11ef463771ab613ff93952c875dd512b
Justine George <justinegeo96@gmail.com>
2024-11-14 12:51:53 +0530

    Time: 48 ms (58.95%) | Memory: 34 MB (52.22%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def is_palindrome(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        def dfs(start):
+            if start == len(s):
+                res.append(part[:])
+                return
+            for i in range(start, len(s)):
+                if is_palindrome(s, start, i):
+                    part.append(s[start:i+1])
+                    dfs(i + 1)
+                    part.pop()
+
+
+        dfs(0)
+        return res
