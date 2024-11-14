b97a7cb6ac57b32312d7ffcedef321a484edf55c
Justine George <justinegeo96@gmail.com>
2024-11-14 12:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.24%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,23 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # find first i from right less than i + 1
         i = len(nums) - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
         if i == -1:
             nums[:] = nums[::-1]
             return
-        # found i
-
-        # find first number starting from end, greater than i
+        
         j = len(nums) - 1
-        while j > i and nums[i] >= nums[j]:
+        while j > i and nums[j] <= nums[i]:
             j -= 1
-        # found j
-
-        # swap i and j
+        
         nums[i], nums[j] = nums[j], nums[i]
 
-        # reverse i + 1 and beyond to get next smallest
-        nums[i+1:] = nums[i+1:][::-1]
+        nums[i+1:] = nums[i + 1:][::-1]
