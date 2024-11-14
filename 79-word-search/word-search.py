5039c7ae09fe84917c4296aad3f5ccf9bfca0d7d
Justine George <justinegeo96@gmail.com>
2024-11-14 13:25:49 +0530

    Time: 5110 ms (25.06%) | Memory: 16.9 MB (6.11%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,27 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        rowRange, colRange = set(range(0, m)), set(range(0, n))
+        rows, cols = set(range(m)), set(range(n))
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         visited = set()
-        def dfs(r, c, i):
+        def does_word_exist(r, c, i):
             if i == len(word):
                 return True
-            
-            if r not in rowRange or c not in colRange or (r, c) in visited or board[r][c] != word[i]:
+
+            if r not in rows or c not in cols or (r, c) in visited or board[r][c] != word[i]:
                 return False
             
             visited.add((r, c))
+            
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if dfs(nr, nc, i + 1):
+                if does_word_exist(nr, nc, i + 1):
                     return True
+
             visited.remove((r, c))
             return False
 
 
-        for i in range(m):
-            for j in range(n):
-                if dfs(i, j, 0):
+        for r in range(m):
+            for c in range(n):
+                if does_word_exist(r, c, 0):
                     return True
         return False
