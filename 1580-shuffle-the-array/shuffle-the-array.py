2b792075ab3617314816c81cefd0024843694240
Justine George <justinegeo96@gmail.com>
2024-11-18 16:58:54 +0530

    Time: 59 ms (26.59%) | Memory: 16.6 MB (95.80%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,12 +1,14 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        # fill 0, n - 1 elements with (x,y) values - y in the 10 LS bits, and x in the following bits
+        '''
+        [2, 5, 1, 3, 4, 7]
+
+        '''
         for i in range(n):
             nums[i] <<= 10
             nums[i] |= nums[i + n]
-        
-        # now use these values to fill x and y values from back to front
-        ones = ((2 ** 10) - 1)
+
+        ones = 2**10 - 1
         j = 2*n - 1
         for i in range(n - 1, -1, -1):
             y = nums[i] & ones
@@ -14,6 +16,6 @@ class Solution:
 
             nums[j] = y
             nums[j - 1] = x
-            j -= 2 
+            j -= 2
         
         return nums
